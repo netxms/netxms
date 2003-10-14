840a1a6a04a10a1f1905b568eaff15f7e42916f6
Victor Kirhenshtein <victor@netxms.org>
2003-10-14 04:07:30 +0000

    - Added definitions for interface types (compatible with SNMP definitions)

--- include/nms_common.h
+++ include/nms_common.h
@@ -37,4 +37,43 @@ typedef void * HMODULE;
 
 #endif
 
+
+//
+// Interface types
+//
+
+#define IFTYPE_OTHER                1
+#define IFTYPE_REGULAR1822          2
+#define IFTYPE_HDH1822              3
+#define IFTYPE_DDN_X25              4
+#define IFTYPE_RFC877_X25           5
+#define IFTYPE_ETHERNET_CSMACD      6
+#define IFTYPE_ISO88023_CSMACD      7
+#define IFTYPE_ISO88024_TOKENBUS    8
+#define IFTYPE_ISO88025_TOKENRING   9
+#define IFTYPE_ISO88026_MAN         10
+#define IFTYPE_STARLAN              11
+#define IFTYPE_PROTEON_10MBIT       12
+#define IFTYPE_PROTEON_80MBIT       13
+#define IFTYPE_HYPERCHANNEL         14
+#define IFTYPE_FDDI                 15
+#define IFTYPE_LAPB                 16
+#define IFTYPE_SDLC                 17
+#define IFTYPE_DS1                  18
+#define IFTYPE_E1                   19
+#define IFTYPE_BASIC_ISDN           20
+#define IFTYPE_PRIMARY_ISDN         21
+#define IFTYPE_PROP_PTP_SERIAL      22
+#define IFTYPE_PPP                  23
+#define IFTYPE_SOFTWARE_LOOPBACK    24
+#define IFTYPE_EON                  25
+#define IFTYPE_ETHERNET_3MBIT       26
+#define IFTYPE_NSIP                 27
+#define IFTYPE_SLIP                 28
+#define IFTYPE_ULTRA                29
+#define IFTYPE_DS3                  30
+#define IFTYPE_SMDS                 31
+#define IFTYPE_FRAME_RELAY          32
+
+
 #endif   /* _nms_common_h_ */
