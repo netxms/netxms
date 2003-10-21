a733196f497ad9486ae07ed5a1c1d9c1b5f8ca12
Victor Kirhenshtein <victor@netxms.org>
2003-10-21 15:04:52 +0000

    New parameter 'DiscoveryPollingInterval' added to setup script

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,3 @@
 INSERT INTO config (name,value) VALUES ('SyncInterval','60');
 INSERT INTO config (name,value) VALUES ('NewNodePollingInterval','60');
+INSERT INTO config (name,value) VALUES ('DiscoveryPollingInterval','3600');
