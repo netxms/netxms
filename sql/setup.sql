2404576158f553f357bdae6ac64fa2f5b0cdd752
Victor Kirhenshtein <victor@netxms.org>
2003-10-23 11:39:36 +0000

    - Changes in setup script

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,4 @@
 INSERT INTO config (name,value) VALUES ('SyncInterval','60');
 INSERT INTO config (name,value) VALUES ('NewNodePollingInterval','60');
-INSERT INTO config (name,value) VALUES ('DiscoveryPollingInterval','3600');
+INSERT INTO config (name,value) VALUES ('DiscoveryPollingInterval','900');
+INSERT INTO config (name,value) VALUES ('ResolveNodeNames','0');
