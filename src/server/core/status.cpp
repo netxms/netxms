517f5ec48b6ebadb864d80a7e92d82456d1420ee
Victor Kirhenshtein <victor@netxms.org>
2003-11-02 16:44:08 +0000

    Status poller code

---
+++ src/server/core/status.cpp
@@ -0,0 +1,47 @@
+/* 
+** Project X - Network Management System
+** Copyright (C) 2003 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** $module: status.cpp
+**
+**/
+
+#include "nms_core.h"
+
+
+//
+// Status poll thread
+//
+
+void StatusPoller(void *arg)
+{
+   Node *pNode;
+
+   while(!ShutdownInProgress())
+   {
+      if (SleepAndCheckForShutdown(5))
+         break;      // Shutdown has arrived
+
+      // Walk through nodes and do status poll
+      for(DWORD i = 0; i < g_dwNodeAddrIndexSize; i++)
+      {
+         pNode = (Node *)g_pNodeIndexByAddr[i].pObject;
+         if (pNode->ReadyForStatusPoll())
+            pNode->StatusPoll();
+      }
+   }
+}
