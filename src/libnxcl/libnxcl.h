aaa065f81e27b49809a9fc20a3e7f1b7daa0a68b
Victor Kirhenshtein <victor@netxms.org>
2004-01-27 16:06:45 +0000

    Work on client library started [Victor]

---
+++ src/libnxcl/libnxcl.h
@@ -0,0 +1,37 @@
+/* 
+** NetXMS - Network Management System
+** Client Library
+** Copyright (C) 2004 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** $module: libnxcl.h
+**
+**/
+
+#ifndef _libnxcl_h_
+#define _libnxcl_h_
+
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else    /* _WIN32 */
+#include <unistd.h>
+#endif   /* _WIN32 */
+
+#include <nms_common.h>
+
+#endif   /* _libnxcl_h_ */
---
+++ src/libnxcl/main.cpp
@@ -0,0 +1,48 @@
+/* 
+** NetXMS - Network Management System
+** Client Library
+** Copyright (C) 2004 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** $module: main.cpp
+**
+**/
+
+#include "libnxcl.h"
+
+
+//
+// Get library version
+//
+
+DWORD EXPORTABLE NXC_GetVersion(void)
+{
+   return (NETXMS_VERSION_MAJOR << 24) | (NETXMS_VERSION_MINOR << 16) | NETXMS_VERSION_RELEASE;
+}
+
+
+//
+// DLL entry point
+//
+
+#ifdef _WIN32
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+   return TRUE;
+}
+
+#endif   /* _WIN32 */
