ac317f02aca3cda1ed262fa619987c03f05f7a0c
Victor Kirhenshtein <victor@netxms.org>
2004-12-02 16:56:30 +0000

    Version number changed to 0.1.9

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -30,8 +30,8 @@
 
 #define NETXMS_VERSION_MAJOR      0
 #define NETXMS_VERSION_MINOR      1
-#define NETXMS_VERSION_BUILD      8
-#define NETXMS_VERSION_STRING     "0.1.8"
+#define NETXMS_VERSION_BUILD      9
+#define NETXMS_VERSION_STRING     "0.1.9"
 
 
 #endif
