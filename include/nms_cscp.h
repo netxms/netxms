13b78f74e376d8a79c84cf23d49094c8bd66e81a
Victor Kirhenshtein <victor@netxms.org>
2005-01-21 21:43:52 +0000

    Schema updated for NetworkService class

--- include/nms_cscp.h
+++ include/nms_cscp.h
@@ -422,6 +422,7 @@ typedef struct
 #define VID_SERVICE_REQUEST         ((DWORD)133)
 #define VID_SERVICE_RESPONCE        ((DWORD)134)
 #define VID_POLLER_NODE_ID          ((DWORD)135)
+#define VID_SERVICE_STATUS          ((DWORD)136)
 
 // Variable ranges for object's ACL
 #define VID_ACL_USER_BASE           ((DWORD)0x00001000)
