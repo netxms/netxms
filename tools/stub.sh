2dad1e4c10fb08a523fc67715146b186c139612e
Alex Kirhenshtein <alk@netxms.org>
2005-02-01 18:46:00 +0000

    works on linux

--- tools/stub.sh
+++ tools/stub.sh
@@ -7,32 +7,30 @@ command=__COMMAND__
 
 trap '
 	echo Cleanup...
-	#[ "x"$temp != "x" ] && rm -rf $temp
+	[ "x"$temp != "x" ] && rm -rf $temp
 	exit
 ' INT EXIT
 
-md5=`which md5`
+md5=`which md5 2>/dev/null`
 if [ $? != 0 ]; then
-	md5=`which md5sum`
+	md5=`which md5sum 2>/dev/null`
 	if [ $? != 0 ]; then
-		md5=`which openssl`
+		md5=`which openssl 2>/dev/null`
 		if [ $? != 0 ]; then
 			echo "Can't calculate MD5, exiting"
 			exit
 		else
 			md5="$md5 md5"
 		fi
-	else
-		md5="$md5 | cut -d' ' -f1"
 	fi
 fi
 
-if [ "X"`head -n$skip $0 | tail -n +5 | $md5 ` != "X"$hash1 ]; then
+if [ "X"`head -n$skip $0 | tail -n +5 | $md5 | cut -b1-32 | tr A-Z a-z` != "X"$hash1 ]; then
 	echo Script MD5 mismach
 	exit
 fi
-skip=`let "$skip+1"`
-if [ "X"`tail -n +$skip $0 | $md5 ` != "X"$hash2 ]; then
+let "skip=skip+1" >/dev/null
+if [ "X"`tail -n +$skip $0 | $md5 | cut -b1-32 | tr A-Z a-z` != "X"$hash2 ]; then
 	echo Payload MD5 mismach
 	exit
 fi
@@ -48,7 +46,11 @@ if [ $? != 0 ]; then
 	exit;
 else
 	cd $temp
-	chmod +x ./$command || echo "Can't chmod $command" && exit
+	chmod +x ./$command
+	if [ $? != 0 ]; then
+		echo "Can't chmod $command"
+		exit
+	fi
 	./$command
 fi
 
