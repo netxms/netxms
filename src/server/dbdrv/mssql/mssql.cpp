e187b2cbd4bf9790585b3c9eb34b2a58075b4a9c
Victor Kirhenshtein <victor@netxms.org>
2005-02-03 13:25:34 +0000

    Minor changes

--- src/server/dbdrv/mssql/mssql.cpp
+++ src/server/dbdrv/mssql/mssql.cpp
@@ -1,6 +1,6 @@
 /* 
 ** Microsoft SQL Server Database Driver
-** Copyright (C) 2004 Victor Kirhenshtein
+** Copyright (C) 2004, 2005 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
--- src/server/dbdrv/mysql/mysql.cpp
+++ src/server/dbdrv/mysql/mysql.cpp
@@ -1,6 +1,6 @@
 /* 
 ** MySQL Database Driver
-** Copyright (C) 2003, 2004 Victor Kirhenshtein
+** Copyright (C) 2003, 2004, 2005 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
