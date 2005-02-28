fd979678be9a83347334ad44bec2504b8aaf004a
Victor Kirhenshtein <victor@netxms.org>
2005-02-28 15:12:19 +0000

    config.h replacement for NetWare

---
+++ include/config-netware.h
@@ -0,0 +1,10 @@
+/* Replacement of config.h for NetWare */
+
+/* Define to 1 if you have the `strtoll' function. */
+#define HAVE_STRTOLL 1
+
+/* Define to 1 if you have the `strtoull' function. */
+#define HAVE_STRTOULL 1
+
+/* Define to 1 if you have the `wcslen' function and sizeof(wchar_t) == 2. */
+#define HAVE_WCSLEN 1
