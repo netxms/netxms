42005ac190deab03925f6a2b29dd27230bac266c
Alex Kirhenshtein <alk@netxms.org>
2005-05-30 23:24:09 +0000

    *** empty log message ***

--- www/cgi-bin/news.pl
+++ www/cgi-bin/news.pl
@@ -4,6 +4,7 @@ use strict;
 #use warnings;
 
 use CGI;
+use POSIX qw(strftime mktime);
 
 ###############################################################################
 # globals
@@ -15,13 +16,22 @@ my $newsFile = "../netxms.news";
 
 ###############################################################################
 # code
-print "Content-type: text/html\n\n";
 
 my $q = new CGI;
 my $cut = 0;
+my $rss = 0;
 if (defined $q->param("cut"))
 {
 	$cut = 1;
+}
+if (defined $q->param("rss"))
+{
+	print "Content-type: text/xml\n\n";
+	$rss = 1;
+}
+else
+{
+	print "Content-type: text/html\n\n";
 }
 
 open(F, $newsFile) || exit 0;
@@ -49,15 +59,81 @@ if (defined $q->param("count"))
 {
 	$count = $q->param("count") + 0;
 }
-
-for (my $i = 0; $i < $count; $i++)
+else
 {
-	my $date = $news[$i]->{date};
-	my $title = $news[$i]->{title};
-	my $summary = $news[$i]->{sum};
-	print <<"__END";
+	if ($rss == 1)
+	{
+		$count = 10;
+	}
+}
+
+if ($rss == 0)
+{
+	for (my $i = 0; $i < $count; $i++)
+	{
+		my $date = $news[$i]->{date};
+		my $title = $news[$i]->{title};
+		my $summary = $news[$i]->{sum};
+		print <<"__END";
 <a href="/news.shtml" class="newsHeading">$title</a>
 <p class="newsDate">$date</p>
 <p class="newsSummary">$summary</p>
+__END
+	}
+}
+else
+{
+	#my $buildDate = "Thu, 26 May 2005 12:22:00 EET";
+	my $buildDate = strftime("%a, %e %b %Y %H:%M:%S %Z", localtime());
+	print <<"__END";
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+	<channel>
+		<title>NetXMS News</title>
+		<link>http://netxms.org</link>
+		<description>NetMXS - Open source monitoring system</description>
+		<lastBuildDate>$buildDate</lastBuildDate>
+__END
+
+	my %m;
+	$m{Jan} = 0;
+	$m{Feb} = 1;
+	$m{Mar} = 2;
+	$m{Apr} = 3;
+	$m{May} = 4;
+	$m{Jun} = 5;
+	$m{Jul} = 6;
+	$m{Aug} = 7;
+	$m{Sep} = 8;
+	$m{Oct} = 9;
+	$m{Nov} = 10;
+	$m{Dec} = 11;
+
+	for (my $i = 0; $i < $count; $i++)
+	{
+		#my $pubDate = strftime("%a, %d %b %Y %H:%M:%S %Z", localtime(time()));
+		if ($news[$i]->{date} =~ /^([0-9]+)-([a-z]+)-([0-9]+)$/i)
+		{
+			my $pubDate = strftime("%a, %d %b %Y %H:%M:%S %Z",
+				localtime(mktime(0, 0, 11, $1, $m{$2}, $3 - 1900)));
+			my $title = $news[$i]->{title};
+			my $description = $news[$i]->{sum};
+			$title =~ s/</&lt;/g; $title =~ s/>/&gt;/g;
+			$description =~ s/</&lt;/g; $description =~ s/>/&gt;/g;
+			print <<"__END";
+		<item>
+			<pubDate>$pubDate</pubDate>
+			<link>http://netxms.org</link>
+			<title>$title</title>
+			<description>
+				$description
+			</description>
+		</item>
+__END
+		}
+	}
+	print <<"__END";
+	</channel>
+</rss>
 __END
 }
