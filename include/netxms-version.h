23c4001e0b0c1d7c4cde2b6b778549096d5df9b1
Victor Kirhenshtein <victor@netxms.org>
2005-06-01 13:20:14 +0000

    Version number changed to 0.2.1-dev

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -30,8 +30,8 @@
 
 #define NETXMS_VERSION_MAJOR        0
 #define NETXMS_VERSION_MINOR        2
-#define NETXMS_VERSION_BUILD        0
-#define NETXMS_VERSION_STRING       _T("0.2.0")
+#define NETXMS_VERSION_BUILD        1
+#define NETXMS_VERSION_STRING       _T("0.2.1-dev")
 
 
 //
