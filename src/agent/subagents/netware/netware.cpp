9039c003cb45b66f900f1cb92fcbdcef0a5887b4
Victor Kirhenshtein <victor@netxms.org>
2005-06-08 14:05:35 +0000

    NetWare platform subagent improved: - Disk.xxx parameters implemented correctly - System.Memory.Physical.xxx implemented

--- src/agent/subagents/netware/netware.cpp
+++ src/agent/subagents/netware/netware.cpp
@@ -20,20 +20,7 @@
 **
 **/
 
-#include <nms_common.h>
-#include <nms_agent.h>
-#include <nms_threads.h>
-#include <netdb.h>
-#include <monitor.h>
-#include <nks/plat.h>
-
-
-//
-// Constants
-//
-
-#define CPU_HISTORY_SIZE   1200
-#define MAX_CPU            32
+#include "nwagent.h"
 
 
 //
@@ -45,6 +32,37 @@ static int m_iCpuHPos = 0;
 
 
 //
+// Memory information
+//
+
+static LONG H_MemoryInfo(char *pszParam, char *pArg, char *pValue)
+{
+   struct memory_info info;
+
+   memset(&info, 0, sizeof(struct memory_info));
+   if (netware_mem_info(&info) != 0)
+      return SYSINFO_RC_ERROR;
+
+   switch((int)pArg)
+   {
+      case MEMINFO_PHYSICAL_TOTAL:
+         ret_uint64(pValue, info.TotalKnownSystemMemory);
+         break;
+      case MEMINFO_PHYSICAL_USED:
+         ret_uint64(pValue, info.TotalWorkMemory);
+         break;
+      case MEMINFO_PHYSICAL_FREE:
+         ret_uint64(pValue, info.TotalKnownSystemMemory - info.TotalWorkMemory);
+         break;
+      default:
+         break;
+   }
+
+   return SYSINFO_RC_SUCCESS;
+}
+
+
+//
 // Disk information
 //
 
@@ -52,24 +70,24 @@ static LONG H_DiskInfo(char *pszParam, char *pArg, char *pValue)
 {
    struct volume_info vi;
    char szVolumeName[MAX_VOLUME_NAME_LEN + 1];
+   DWORD dwClusterSize;    // Cluster size in bytes
 
    NxGetParameterArg(pszParam, 1, szVolumeName, MAX_VOLUME_NAME_LEN);
    memset(&vi, 0, sizeof(struct volume_info));
    if (netware_vol_info_from_name(&vi, szVolumeName) != 0)
       return SYSINFO_RC_UNSUPPORTED;
 
-printf("bs=%llu bc=%llu bf=%llu\n",vi.BlockSize,vi.BlockCount,vi.BlocksFree);
-
+   dwClusterSize = vi.SectorSize * vi.SectorsPerCluster;
    switch(pArg[0])
    {
       case 'T':   // Total size
-         ret_uint64(pValue, vi.BlockCount * vi.BlockSize);
+         ret_uint64(pValue, (QWORD)vi.VolumeSizeInClusters * (QWORD)dwClusterSize);
          break;
       case 'F':   // Free space
-         ret_uint64(pValue, vi.BlocksFree * vi.BlockSize);
+         ret_uint64(pValue, ((QWORD)vi.FreedClusters + (QWORD)vi.SubAllocFreeableClusters) * (QWORD)dwClusterSize + (QWORD)vi.FreeableLimboSectors * (QWORD)vi.SectorSize);
          break;
       case 'U':   // Used space
-         ret_uint64(pValue, (vi.BlockCount - vi.BlocksFree) * vi.BlockSize);
+         ret_uint64(pValue, ((QWORD)(vi.VolumeSizeInClusters - vi.FreedClusters - vi.SubAllocFreeableClusters) * (QWORD)vi.SectorsPerCluster - (QWORD)vi.FreeableLimboSectors) * (QWORD)vi.SectorSize);
          break;
       default:
          return SYSINFO_RC_UNSUPPORTED;
@@ -212,18 +230,21 @@ static THREAD_RESULT THREAD_CALL CollectorThread(void *pArg)
 
 static NETXMS_SUBAGENT_PARAM m_parameters[] =
 {
-   { "Disk.Free(*)", H_DiskInfo, "F" },
-   { "Disk.Total(*)", H_DiskInfo, "T" },
-   { "Disk.Used(*)", H_DiskInfo, "U" },
-   { "System.CPU.Count", H_CpuCount, NULL },
-   { "System.CPU.Usage", H_CpuUsage, (char *)60 },
-   { "System.CPU.Usage5", H_CpuUsage, (char *)300 },
-   { "System.CPU.Usage15", H_CpuUsage, (char *)900 },
-   { "System.CPU.Usage(*)", H_CpuUsage, (char *)60 },
-   { "System.CPU.Usage5(*)", H_CpuUsage, (char *)300 },
-   { "System.CPU.Usage15(*)", H_CpuUsage, (char *)900 },
-   { "System.Hostname", H_HostName, NULL },
-   { "System.PlatformName", H_PlatformName, NULL }
+   { "Disk.Free(*)", H_DiskInfo, "F", DCI_DT_UINT64, "Free disk space on {instance}" },
+   { "Disk.Total(*)", H_DiskInfo, "T", DCI_DT_UINT64, "Total disk space on {instance}" },
+   { "Disk.Used(*)", H_DiskInfo, "U", DCI_DT_UINT64, "Used disk space on {instance}" },
+   { "System.CPU.Count", H_CpuCount, NULL, DCI_DT_UINT, "Number of CPU in the system" },
+   { "System.CPU.Usage", H_CpuUsage, (char *)60, DCI_DT_FLOAT, "Average CPU(s) utilization for last minute" },
+   { "System.CPU.Usage5", H_CpuUsage, (char *)300, DCI_DT_FLOAT, "Average CPU(s) utilization for last 5 minutes" },
+   { "System.CPU.Usage15", H_CpuUsage, (char *)900, DCI_DT_FLOAT, "Average CPU(s) utilization for last 15 minutes" },
+   { "System.CPU.Usage(*)", H_CpuUsage, (char *)60, DCI_DT_FLOAT, "Average CPU {instance} utilization for last minute" },
+   { "System.CPU.Usage5(*)", H_CpuUsage, (char *)300, DCI_DT_FLOAT, "Average CPU {instance} utilization for last 5 minutes" },
+   { "System.CPU.Usage15(*)", H_CpuUsage, (char *)900, DCI_DT_FLOAT, "Average CPU {instance} utilization for last 15 minutes" },
+   { "System.Hostname", H_HostName, NULL, DCI_DT_STRING, "Host name" },
+	{ "System.Memory.Physical.Free", H_MemoryInfo, (char *)MEMINFO_PHYSICAL_FREE, DCI_DT_UINT64, "Available physical memory" },
+	{ "System.Memory.Physical.Total", H_MemoryInfo, (char *)MEMINFO_PHYSICAL_TOTAL, DCI_DT_UINT64, "Total amount of physical memory" },
+	{ "System.Memory.Physical.Used", H_MemoryInfo, (char *)MEMINFO_PHYSICAL_USED, DCI_DT_UINT64, "Used physical memory" },
+   { "System.PlatformName", H_PlatformName, NULL, DCI_DT_STRING, NULL }
 };
 static NETXMS_SUBAGENT_ENUM m_enums[] =
 {
