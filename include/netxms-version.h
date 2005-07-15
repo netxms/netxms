9d6aa215189903556940a66ac1afc71819099d10
Victor Kirhenshtein <victor@netxms.org>
2005-07-15 07:45:34 +0000

    - Version number changed to 0.2.3 - Fixed issue #29 (patch applied)

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -30,8 +30,8 @@
 
 #define NETXMS_VERSION_MAJOR        0
 #define NETXMS_VERSION_MINOR        2
-#define NETXMS_VERSION_BUILD        2
-#define NETXMS_VERSION_STRING       _T("0.2.2")
+#define NETXMS_VERSION_BUILD        3
+#define NETXMS_VERSION_STRING       _T("0.2.3-dev")
 
 
 //
--- src/server/tools/nxadm/nxadm.cpp
+++ src/server/tools/nxadm/nxadm.cpp
@@ -110,7 +110,7 @@ static void Shell(void)
 
 #if USE_READLINE
    // Initialize readline library if we use it
-# if (RL_READLINE_VERSION && RL_VERSION_MAJOR >= 5) || __FreeBSD__ >= 5
+# if (RL_READLINE_VERSION && RL_VERSION_MAJOR >= 4 && RL_VERSION_MINOR >= 2) || __FreeBSD__ >= 5
    rl_bind_key('\t', (rl_command_func_t *)rl_insert);
 # else 
    rl_bind_key('\t', (Function *)rl_insert);
