e9d6d4fc1dcc1bc69d337d6bad1f9e08ee28980a
Victor Kirhenshtein <victor@netxms.org>
2005-08-13 14:03:14 +0000

    Added actions System.Shutdown and System.Restart

--- src/agent/subagents/winnt/main.cpp
+++ src/agent/subagents/winnt/main.cpp
@@ -45,6 +45,67 @@ DWORD (__stdcall *imp_GetGuiResources)(HANDLE, DWORD) = NULL;
 
 
 //
+// Set or clear current privilege
+//
+
+static BOOL SetCurrentPrivilege(LPCTSTR pszPrivilege, BOOL bEnablePrivilege)
+{
+	HANDLE hToken;
+	LUID luid;
+	TOKEN_PRIVILEGES tp, tpPrevious;
+	DWORD cbPrevious = sizeof(TOKEN_PRIVILEGES);
+	BOOL bSuccess = FALSE;
+
+	if (!LookupPrivilegeValue(NULL, pszPrivilege, &luid))
+		return FALSE;
+
+	if(!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, &hToken))
+		return FALSE;
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	tp.Privileges[0].Attributes = 0;
+
+	if (AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES),
+                             &tpPrevious, &cbPrevious))
+	{
+		tpPrevious.PrivilegeCount = 1;
+		tpPrevious.Privileges[0].Luid = luid;
+
+		if (bEnablePrivilege)
+			tpPrevious.Privileges[0].Attributes |= SE_PRIVILEGE_ENABLED;
+		else
+			tpPrevious.Privileges[0].Attributes &= ~SE_PRIVILEGE_ENABLED;
+
+		bSuccess = AdjustTokenPrivileges(hToken, FALSE, &tpPrevious, cbPrevious, NULL, NULL);
+	}
+
+	CloseHandle(hToken);
+
+	return bSuccess;
+}
+
+
+
+//
+// Shutdown system
+//
+
+static LONG H_ActionShutdown(char *pszAction, NETXMS_VALUES_LIST *pArgList, char *pData)
+{
+   LONG nRet = ERR_INTERNAL_ERROR;
+
+   if (SetCurrentPrivilege(SE_SHUTDOWN_NAME, TRUE))
+   {
+      if (InitiateSystemShutdown(NULL, NULL, 0, TRUE,
+                                 (*pData == 'R') ? TRUE : FALSE))
+         nRet = ERR_SUCCESS;
+   }
+   return nRet;
+}
+
+
+//
 // Called by master agent at unload
 //
 
@@ -81,6 +142,11 @@ static NETXMS_SUBAGENT_ENUM m_enums[] =
 {
    { "System.ProcessList", H_ProcessList, NULL }
 };
+static NETXMS_SUBAGENT_ACTION m_actions[] =
+{
+   { "System.Restart", H_ActionShutdown, "R", "Restart system" },
+   { "System.Shutdown", H_ActionShutdown, "S", "Shutdown system" }
+};
 
 static NETXMS_SUBAGENT_INFO m_info =
 {
@@ -89,7 +155,9 @@ static NETXMS_SUBAGENT_INFO m_info =
 	sizeof(m_parameters) / sizeof(NETXMS_SUBAGENT_PARAM),
 	m_parameters,
 	sizeof(m_enums) / sizeof(NETXMS_SUBAGENT_ENUM),
-	m_enums
+	m_enums,
+	sizeof(m_actions) / sizeof(NETXMS_SUBAGENT_ACTION),
+   m_actions
 };
 
 
