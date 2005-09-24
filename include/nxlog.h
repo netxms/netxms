7259dd2cc1965e959ab6eb077750dc9181f14f9b
Victor Kirhenshtein <victor@netxms.org>
2005-09-24 15:13:05 +0000

    New header file with structures and defines related to log processing

---
+++ include/nxlog.h
@@ -0,0 +1,66 @@
+/* 
+** NetXMS - Network Management System
+** Copyright (C) 2003, 2004, 2005 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** $module: nxlog.h
+**
+**/
+
+#ifndef _nxlog_h_
+#define _nxlog_h_
+
+
+//
+// Constants
+//
+
+#define MAX_LOG_MSG_LENGTH       1024
+#define MAX_SYSLOG_HOSTNAME_LEN  128
+#define MAX_SYSLOG_TAG_LEN       33
+
+
+//
+// Syslog severity codes
+//
+
+#define SYSLOG_SEVERITY_EMERGENCY      0
+#define SYSLOG_SEVERITY_ALERT          1
+#define SYSLOG_SEVERITY_CRITICAL       2
+#define SYSLOG_SEVERITY_ERROR          3
+#define SYSLOG_SEVERITY_WARNING        4
+#define SYSLOG_SEVERITY_NOTICE         5
+#define SYSLOG_SEVERITY_INFORMATIONAL  6
+#define SYSLOG_SEVERITY_DEBUG          7
+
+
+//
+// Unified log record structure
+//
+
+typedef struct
+{
+   DWORD dwTimeStamp;
+   int nFacility;
+   int nSeverity;
+   DWORD dwSourceObject;
+   TCHAR szHostName[MAX_SYSLOG_HOSTNAME_LEN];
+   TCHAR szTag[MAX_SYSLOG_TAG_LEN];
+   TCHAR szMessage[MAX_LOG_MSG_LENGTH];
+} NX_LOG_RECORD;
+
+
+#endif
