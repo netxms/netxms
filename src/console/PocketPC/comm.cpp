74400c4b1ffed977361d782796f50ec118eff294
Victor Kirhenshtein <victor@netxms.org>
2005-10-28 14:01:33 +0000

    Updated NXCConnect() call

--- src/console/PocketPC/comm.cpp
+++ src/console/PocketPC/comm.cpp
@@ -69,7 +69,8 @@ static DWORD WINAPI LoginThread(void *pArg)
    HWND hWnd = *((HWND *)pArg);    // Handle to status window
    DWORD dwResult;
 
-   dwResult = NXCConnect(g_szServer, g_szLogin, g_szPassword, &g_hSession, FALSE, FALSE);
+   dwResult = NXCConnect(g_szServer, g_szLogin, g_szPassword, &g_hSession, 
+                         _T("NetXMS Pocket Console/") NETXMS_VERSION_STRING, FALSE, FALSE);
 
    // If successful, load container objects' categories
    if (dwResult == RCC_SUCCESS)
