636ac97d795b8192ea99a16cb796971a4dd643a2
Alex Kirhenshtein <alk@netxms.org>
2006-03-09 11:14:55 +0000

    simple log scanner added

---
+++ src/agent/subagents/logscan/logscan.cpp
@@ -0,0 +1,155 @@
+/* $Id: logscan.cpp,v 1.1 2006-03-09 11:14:55 alk Exp $ */
+
+#include <nms_common.h>
+#include <nms_agent.h>
+#include <nxclapi.h>
+#include <nxcscpapi.h>
+
+#include <string>
+#include <map>
+
+#ifdef _WIN32
+#define LOGSCAN_EXPORTABLE __declspec(dllexport) __cdecl
+#else
+#define LOGSCAN_EXPORTABLE
+#endif
+
+using namespace std;
+
+//
+// Typedefs
+//
+typedef map<string, long> tLocations;
+
+//
+// static vars
+//
+static tLocations m_locations;
+static MUTEX m_locationsMutex;
+
+//
+// Forward declarations
+//
+LONG H_GetString(char *, char *, char *);
+
+//
+// Cleanup callback
+//
+static void Cleanup(void)
+{
+	MutexDestroy(m_locationsMutex);
+}
+
+//
+// Subagent information
+//
+
+static NETXMS_SUBAGENT_PARAM m_parameters[] =
+{
+   { "LogScan.FindString(*)", H_GetString, NULL, DCI_DT_STRING, ""},
+};
+
+static NETXMS_SUBAGENT_INFO m_info =
+{
+   NETXMS_SUBAGENT_INFO_MAGIC,
+	"logscan",
+	NETXMS_VERSION_STRING,
+	&Cleanup, // unload handler
+	NULL, // command handler
+	sizeof(m_parameters) / sizeof(NETXMS_SUBAGENT_PARAM),
+	m_parameters,
+	0, //sizeof(m_enums) / sizeof(NETXMS_SUBAGENT_ENUM),
+	NULL, //m_enums,
+   0, NULL     // actions
+};
+
+//
+// Entry point for NetXMS agent
+//
+
+DECLARE_SUBAGENT_INIT(PORTCHECK)
+{
+   *ppInfo = &m_info;
+
+	m_locationsMutex = MutexCreate();
+
+   return TRUE;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// main code
+//
+
+LONG H_GetString(char *pszParam, char *pArg, char *pValue)
+{
+	int ret = SYSINFO_RC_ERROR;
+	char fileName[1024];
+	char subString[1024];
+
+   NxGetParameterArg(pszParam, 1, fileName, sizeof(fileName));
+   NxGetParameterArg(pszParam, 2, subString, sizeof(subString));
+
+	if (fileName[0] != 0 && subString[0] != 0)
+	{
+		char tmp[10240];
+		struct stat s;
+
+		if (stat(fileName, &s) == 0)
+		{
+			long location = 0;
+			MutexLock(m_locationsMutex, INFINITE);
+			tLocations::iterator it = m_locations.find(string(fileName) + string(":::") + subString);
+			if (it != m_locations.end())
+			{
+				// TODO: add more strict rotation checking
+				if (it->second <= s.st_size) // last location is beyond end of file
+				{
+					location = it->second;
+				}
+			}
+			MutexUnlock(m_locationsMutex);
+
+			FILE *f = fopen(fileName, "r");
+			if (f != NULL)
+			{
+				fseek(f, location, SEEK_SET);
+
+				tmp[0] = 0;
+				while (fgets(tmp,sizeof(tmp), f) != NULL)
+				{
+					if (strcasestr(tmp, subString) != NULL)
+					{
+						ret_string(pValue, tmp);
+						ret = SYSINFO_RC_SUCCESS;
+						break;
+					}
+					tmp[0] = 0;
+				}
+
+				if (ret != SYSINFO_RC_SUCCESS)
+				{
+					ret = SYSINFO_RC_SUCCESS;
+					ret_string(pValue, "");
+				}
+
+				MutexLock(m_locationsMutex, INFINITE);
+				//m_locations[string(fileName) + string(":::") + subString] = ftell(f);
+				m_locations[string(fileName) + string(":::") + subString] = s.st_size;
+				MutexUnlock(m_locationsMutex);
+
+				fclose(f);
+			}
+		}
+	}
+
+	return ret;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+
+$Log: not supported by cvs2svn $
+
+*/
