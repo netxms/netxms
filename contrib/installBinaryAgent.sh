75c349dd66243a2c9f766963b42c4ba7630f6691
Alex Kirhenshtein <alk@netxms.org>
2007-05-11 08:46:38 +0000

    upgrade script for binary packages added

---
+++ contrib/installBinaryAgent.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+# $Id: installBinaryAgent.sh,v 1.1 2007-05-11 08:46:38 alk Exp $
+
+# Copyright (c) 2007, NetXMS Team
+#
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the <ORGANIZATION> nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+prefix=/opt/netxms
+config=/etc/nxagentd.conf
+log=/tmp/nxagentbinupdate.log
+
+###############################################################################
+#
+# Main code
+#
+
+# process args
+while [ "x"$1 != "x" ]; do
+	name=`echo $1|cut -d= -f1`
+	val=`echo $1|cut -d= -f2`
+	eval $name=$val
+	shift
+done
+
+case `uname -s` in
+	Linux)
+		pkill=killall
+		;;
+	SunOS)
+		pkill=pkill
+		;;
+	*BSD)
+		pkill=killall
+		;;
+	*)
+		pkill=pkill
+		;;
+esac
+
+cd `dirname $0`
+name=`ls nxagent-*.tar.gz 2>/dev/null|sed s',\.tar\.gz$,,'`
+if [ "x$name" = "x" ]; then
+	echo invalid package >>$log
+	exit 1
+fi
+
+rm -rf tmp
+mkdir tmp && cd tmp
+
+gzip -dc ../$name.tar.gz | tar xf - 2>/dev/null
+if [ $? != 0 ]; then
+	echo Unable to unpack >>$log
+	exit 2
+fi
+
+# ask nxagentd gently
+$pkill nxagentd >>$log 2>&1
+# wait a few seconds and smash it down
+sleep 15 && $pkill -9 nxagentd >>$log 2>&1
+
+# backup current version
+rm -rf $prefix.backup
+cp -R $prefix $prefix.backup 2>/dev/null
+
+# install new files
+mkdir $prefix 2>/dev/null
+cp -R * $prefix/ 2>>$log
+if [ $? != 0 ]; then
+	echo unable to copy new files to $prefix >>$log
+	exit 3
+fi
+
+# show config
+echo "config ($config):" >>$log
+cat $config >>$log 2>&1
+echo "---------------" >>$log
+
+# and restart
+echo "Starting agent: $prefix/bin/nxagentd -d -c $config" >>$log
+$prefix/bin/nxagentd -d -c $config >>$log 2>&1
+ret=$?
+if [ $ret != 0 ]; then
+	echo nxagentd not started \($ret\) >>$log
+	exit 5
+fi
+rm -f $log.tmp
