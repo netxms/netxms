ee8ac0a1792ff996b692d41862c45e91db6481bb
Victor Kirhenshtein <victor@netxms.org>
2007-06-07 16:09:40 +0000

    Added script for .apkg files creation

---
+++ tools/create_apkg.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+version=$1
+if [ "x$version" == "x" ]; then
+	echo "Usage: tools/create_apkg.sh <version>"
+	exit 1
+fi
+
+if [ ! \( -a netxms-$version.tar.gz \) ]; then
+	echo "Unable to find distribution package netxms-$version.tar.gz"
+	exit 1
+fi
+
+cd contrib
+if [ $? != 0 ]; then
+	echo "Cannot change directory to contrib. Do you run script from source tree root?"
+	exit 1
+fi
+
+rm -rf inst
+mkdir inst
+cp ../netxms-$version.tar.gz inst/
+cp installAgent.sh inst
+tar zcvf inst.tar.gz inst
+mv inst.tar.gz ../tools/
+cd ../tools/
+bash ./sharIt inst.tar.gz inst/installAgent.sh ../nxagent-$version.apkg
+cd ..
+rm -rf contrib/inst
