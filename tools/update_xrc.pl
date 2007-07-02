92184bf27a4c6c2302ad4ed2f052866bbb5d62a2
Victor Kirhenshtein <victor@netxms.org>
2007-07-02 07:43:34 +0000

    Added missing file/removed unneeded file

---
+++ tools/update_xrc.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+$mode = 0;
+$level = 0;
+
+while(<>)
+{
+   chomp;
+   $line = $_;
+   if ($mode == 0)
+   {
+      if ($line =~ /^.*\<object class="wxFrame" name="delete_.*/)
+      {
+         $mode = 1;
+         $level++;
+         $line = "";
+      }
+   }
+   elsif ($mode == 1)
+   {
+      if ($line =~ /^.*\<object.*\/\>.*/)
+      {
+      }
+      elsif ($line =~ /^.*\<object.*/)
+      {
+         $level++;
+      }
+      elsif ($line =~ /^.*\<\/object.*/)
+      {
+         $level--;
+         if ($level == 0)
+         {
+            $line = "";
+            $mode = 0;
+         }
+      }
+      elsif ($level == 1)
+      {
+         $line = "";
+      }
+   }
+   if ($line ne "")
+   {
+      print "$line\n";
+   }
+}
