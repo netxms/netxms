4e9b562eb6a350185b88ef61d10aa0440f6743ff
Victor Kirhenshtein <victor@netxms.org>
2007-08-20 07:08:46 +0000

    Added script for create binary agent packages

---
+++ tools/create_bin_apkg.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+prefix=$1
+version=$2
+if [ -z "$prefix" ]; then
+	echo "Usage: tools/create_bin_apkg.sh <prefix> <version>"
+	exit 1
+fi
+
+if [ ! -r $prefix/bin/nxagentd ]; then
+	echo "Unable to find compiled agent undex $prefix"
+	exit 1
+fi
+
+cd contrib
+if [ $? != 0 ]; then
+	echo "Cannot change directory to contrib. Do you run script from source tree root?"
+	exit 1
+fi
+
+currdir=`pwd`
+cd $prefix &&
+tar cvf nxagent-binary.tar * &&
+gzip nxagent-binary.tar &&
+cd $currdir &&
+rm -rf inst &&
+mkdir inst &&
+mv $prefix/nxagent-binary.tar.gz inst/ &&
+cp installBinaryAgent.sh inst/ &&
+tar cvf inst.tar inst &&
+gzip inst.tar &&
+mv inst.tar.gz ../tools/ &&
+cd ../tools/ &&
+bash ./sharIt inst.tar.gz inst/installBinaryAgent.sh ../nxagent-$version.apkg &&
+cd .. &&
+rm -rf contrib/inst
