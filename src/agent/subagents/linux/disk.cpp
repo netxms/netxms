709f96b5d334bd8e164fac726409b0f2cf8e3e56
Alex Kirhenshtein <alk@netxms.org>
2007-11-28 16:16:01 +0000

    Now Disk.* should be able to work with device names too (not only mount points)

--- src/agent/subagents/linux/disk.cpp
+++ src/agent/subagents/linux/disk.cpp
@@ -1,8 +1,8 @@
-/* $Id: disk.cpp,v 1.6 2007-09-27 09:16:41 alk Exp $ */
+/* $Id: disk.cpp,v 1.7 2007-11-28 16:16:01 alk Exp $ */
 
 /* 
 ** NetXMS subagent for GNU/Linux
-** Copyright (C) 2004 Alex Kirhenshtein
+** Copyright (C) 2004 - 2007 Alex Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -23,54 +23,81 @@
 #include <nms_common.h>
 #include <nms_agent.h>
 
-#include <sys/statvfs.h>
+#include <paths.h>
+#include <mntent.h>
+#include <sys/vfs.h>
 
 #include "disk.h"
+
+static void findMountpointByDevice(char *dev, int size)
+{
+	char *name;
+	struct mntent *mnt;
+	FILE *f;
+
+	f = setmntent(_PATH_MOUNTED, "r");
+	if (f != NULL) {
+		while ((mnt = getmntent(f)) != NULL)
+		{
+			if (!strcmp(mnt->mnt_fsname, dev))
+			{
+				strncpy(dev, mnt->mnt_dir, size);
+				break;
+			}
+		}
+		endmntent(f);
+	}
+}
 
 LONG H_DiskInfo(char *pszParam, char *pArg, char *pValue)
 {
 	int nRet = SYSINFO_RC_ERROR;
-	struct statvfs s;
+	struct statfs s;
 	char szArg[512] = {0};
 
 	NxGetParameterArg(pszParam, 1, szArg, sizeof(szArg));
 
-	if (szArg[0] != 0 && statvfs(szArg, &s) == 0)
+	if (szArg[0] != 0)
 	{
-		nRet = SYSINFO_RC_SUCCESS;
-		
-		QWORD usedBlocks = (QWORD)(s.f_blocks - s.f_bfree);
-		QWORD totalBlocks = (QWORD)s.f_blocks;
-		QWORD blockSize = (QWORD)s.f_frsize;
-		QWORD freeBlocks = (QWORD)s.f_bfree;
-		QWORD availableBlocks = (QWORD)s.f_bavail;
-		
-		switch((long)pArg)
+		findMountpointByDevice(szArg, sizeof(szArg));
+
+		if (statfs(szArg, &s) == 0)
 		{
-			case DISK_TOTAL:
-				ret_uint64(pValue, totalBlocks * blockSize);
-				break;
-			case DISK_USED:
-				ret_uint64(pValue, usedBlocks * blockSize);
-				break;
-			case DISK_FREE:
-				ret_uint64(pValue, freeBlocks * blockSize);
-				break;
-			case DISK_AVAIL:
-				ret_uint64(pValue, availableBlocks * blockSize);
-				break;
-			case DISK_USED_PERC:
-				ret_double(pValue, (usedBlocks * 100) / totalBlocks);
-				break;
-			case DISK_AVAIL_PERC:
-				ret_double(pValue, (availableBlocks * 100) / totalBlocks);
-				break;
-			case DISK_FREE_PERC:
-				ret_double(pValue, (freeBlocks * 100) / totalBlocks);
-				break;
-			default:
-				nRet = SYSINFO_RC_ERROR;
-				break;
+			nRet = SYSINFO_RC_SUCCESS;
+			
+			QWORD usedBlocks = (QWORD)(s.f_blocks - s.f_bfree);
+			QWORD totalBlocks = (QWORD)s.f_blocks;
+			QWORD blockSize = (QWORD)s.f_frsize;
+			QWORD freeBlocks = (QWORD)s.f_bfree;
+			QWORD availableBlocks = (QWORD)s.f_bavail;
+			
+			switch((long)pArg)
+			{
+				case DISK_TOTAL:
+					ret_uint64(pValue, totalBlocks * blockSize);
+					break;
+				case DISK_USED:
+					ret_uint64(pValue, usedBlocks * blockSize);
+					break;
+				case DISK_FREE:
+					ret_uint64(pValue, freeBlocks * blockSize);
+					break;
+				case DISK_AVAIL:
+					ret_uint64(pValue, availableBlocks * blockSize);
+					break;
+				case DISK_USED_PERC:
+					ret_double(pValue, (usedBlocks * 100) / totalBlocks);
+					break;
+				case DISK_AVAIL_PERC:
+					ret_double(pValue, (availableBlocks * 100) / totalBlocks);
+					break;
+				case DISK_FREE_PERC:
+					ret_double(pValue, (freeBlocks * 100) / totalBlocks);
+					break;
+				default:
+					nRet = SYSINFO_RC_ERROR;
+					break;
+			}
 		}
 	}
 
@@ -81,6 +108,9 @@ LONG H_DiskInfo(char *pszParam, char *pArg, char *pValue)
 /*
 
 $Log: not supported by cvs2svn $
+Revision 1.6  2007/09/27 09:16:41  alk
+disk usage fixed (%), should work for up to 1T mountpoints
+
 Revision 1.5  2007/09/17 18:55:04  alk
 freespace(%) fixed for large partitions
 
