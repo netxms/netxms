3b2e04826a20bff13971169d88e2d0829fbe005b
Victor Kirhenshtein <victor@netxms.org>
2008-10-17 19:19:42 +0000

    initial Java code for client library - NXCP message and variable classes

---
+++ src/java/netxms/src/org/netxms/base/NXCPMessage.java
@@ -0,0 +1,199 @@
+/**
+ * 
+ */
+package org.netxms.base;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+public class NXCPMessage
+{
+	private Integer msgCode;
+	private Long msgId;
+	private Map<Long, NXCPVariable> variableMap = new HashMap<Long, NXCPVariable>();
+	
+	
+	/**
+	 * @param msgCode
+	 */
+	public NXCPMessage(Integer msgCode)
+	{
+		this.msgCode = msgCode;
+		msgId = 0L;
+	}
+
+	/**
+	 * @param msgCode
+	 * @param msgId
+	 */
+	public NXCPMessage(Integer msgCode, Long msgId)
+	{
+		this.msgCode = msgCode;
+		this.msgId = msgId;
+	}
+
+	/**
+	 * Create NXCPMessage from binary NXCP message
+	 * 
+	 * @param nxcpMessage binary NXCP message
+	 */
+	public NXCPMessage(byte[] nxcpMessage) throws IOException
+	{
+		DataInputStream in = new DataInputStream(new ByteArrayInputStream(nxcpMessage));
+		int i, numVars;
+		NXCPVariable var;
+		
+		msgCode = in.readUnsignedShort();
+		in.skipBytes(6);	// Message flags and size
+		msgId = Long.valueOf(in.readInt());
+		numVars = in.readInt();
+		
+		for(i = 0; i < numVars; i++)
+		{
+			// Read first 8 bytes - any DF is at least 8 bytes long
+			byte[] dfTmp = new byte[8];
+			in.readFully(dfTmp);
+			if (dfTmp[4] == NXCPVariable.TYPE_INT16)
+			{
+				// DF of type INT16 is 8 bytes long, no additional data needed
+				var = new NXCPVariable(dfTmp);
+			}
+			else
+			{
+				byte[] df;
+				
+				switch(dfTmp[4])
+				{
+					case NXCPVariable.TYPE_FLOAT:		// all these types requires additional 8 bytes
+					case NXCPVariable.TYPE_INTEGER:
+					case NXCPVariable.TYPE_INT64:
+						df = new byte[16];
+						System.arraycopy(dfTmp, 0, df, 0, 8);
+						in.readFully(dfTmp);
+						System.arraycopy(dfTmp, 0, df, 8, 8);
+						break;
+					case NXCPVariable.TYPE_STRING:		// all these types has 4-byte length field followed by actual content
+					case NXCPVariable.TYPE_BINARY:
+						break;
+				}
+				var = new NXCPVariable(df);
+			}
+			variableMap.put(var.getVariableId(), var);
+		}
+	}
+
+
+	/**
+	 * @return the msgCode
+	 */
+	public Integer getMsgCode()
+	{
+		return msgCode;
+	}
+
+	/**
+	 * @param msgCode the msgCode to set
+	 */
+	public void setMsgCode(Integer msgCode)
+	{
+		this.msgCode = msgCode;
+	}
+
+	/**
+	 * @return the msgId
+	 */
+	public Long getMsgId()
+	{
+		return msgId;
+	}
+
+	/**
+	 * @param msgId the msgId to set
+	 */
+	public void setMsgId(Long msgId)
+	{
+		this.msgId = msgId;
+	}
+	
+	
+	/*
+	 * @param varId variable Id to find 
+	 */
+	public NXCPVariable findVariable(Long varId)
+	{
+		return variableMap.get(varId);
+	}
+
+	
+	/***************************************
+	 * Variable setters
+	 */
+	
+	/*
+	 * @param var variable to add/replace 
+	 */
+	public void setVariable(NXCPVariable var)
+	{
+		variableMap.put(var.getVariableId(), var);
+	}
+
+	public void setVariable(long varId, byte[] value)
+	{
+		setVariable(new NXCPVariable(varId, value));
+	}
+	
+	public void setVariable(long varId, String value)
+	{
+		setVariable(new NXCPVariable(varId, value));
+	}
+
+	public void setVariable(long varId, Double value)
+	{
+		setVariable(new NXCPVariable(varId, value));
+	}
+
+	public void setVariableInt64(long varId, Long value)
+	{
+		setVariable(new NXCPVariable(varId, NXCPVariable.TYPE_INT64, value));
+	}
+
+	public void setVariableInt32(long varId, Long value)
+	{
+		setVariable(new NXCPVariable(varId, NXCPVariable.TYPE_INTEGER, value));
+	}
+
+	public void setVariableInt16(long varId, Long value)
+	{
+		setVariable(new NXCPVariable(varId, NXCPVariable.TYPE_INT16, value));
+	}
+	
+	
+	/*
+	 * Create binary NXCP message
+	 */
+	public byte[] createNXCPMessage() throws IOException
+	{
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(byteStream);
+		
+		// Create byte array with all messages
+		Iterator<NXCPVariable> it = variableMap.values().iterator();
+		while(it.hasNext())
+			out.write(it.next().createNXCPDataField());
+		byte[] payload = byteStream.toByteArray();
+		
+		// Create message header in new byte stream and add payload
+		byteStream = new ByteArrayOutputStream();
+		out = new DataOutputStream(byteStream);
+		out.writeShort(msgCode);
+		out.writeShort(0);	// Message flags
+		out.writeInt(payload.length);	   // Size
+		out.writeInt(msgId.intValue());
+		out.writeInt(variableMap.size());
+		out.write(payload);
+		
+		return byteStream.toByteArray();
+	}
+}
---
+++ src/java/netxms/src/org/netxms/base/NXCPVariable.java
@@ -0,0 +1,258 @@
+package org.netxms.base;
+
+import java.io.*;
+
+/**
+ * @author victor
+ *
+ */
+public class NXCPVariable
+{
+	public final static int TYPE_INTEGER = 0;
+	public final static int TYPE_STRING = 1;
+	public final static int TYPE_INT64 = 2;
+	public final static int TYPE_INT16 = 3;
+	public final static int TYPE_BINARY = 4;
+	public final static int TYPE_FLOAT = 5;
+	
+	private long variableId;
+	private int variableType;
+	
+	private Long integerValue;
+	private Double realValue;
+	private String stringValue;
+	private byte[] binaryValue;
+	
+	
+	/**
+	 * @param varId
+	 * @param varType
+	 * @param value
+	 */
+	public NXCPVariable(long varId, int varType, Long value)
+	{
+		variableId = varId;
+		variableType = varType;
+		integerValue = value;
+		stringValue = integerValue.toString();
+		realValue = integerValue.doubleValue();
+	}
+		
+	/**
+	 * @param varId
+	 * @param value
+	 */
+	public NXCPVariable(long varId, String value)
+	{
+		variableId = varId;
+		variableType = TYPE_STRING;
+		stringValue = value;
+		integerValue = Long.parseLong(stringValue);
+		realValue = Double.parseDouble(stringValue);
+	}
+
+	/**
+	 * @param varId
+	 * @param value
+	 */
+	public NXCPVariable(long varId, Double value)
+	{
+		variableId = varId;
+		variableType = TYPE_FLOAT;
+		realValue = value;
+		stringValue = value.toString();
+		integerValue = realValue.longValue();
+	}
+
+	/**
+	 * @param varId
+	 * @param value
+	 */
+	public NXCPVariable(long varId, byte[] value)
+	{
+		variableId = varId;
+		variableType = TYPE_BINARY;
+		binaryValue = value;
+	}
+
+	/**
+	 * Create NXCPVariable from NXCP message data field
+	 * 
+	 * @param nxcpDataField
+	 */
+	public NXCPVariable(byte[] nxcpDataField) throws IOException
+	{
+		DataInputStream in = new DataInputStream(new ByteArrayInputStream(nxcpDataField));
+		
+		variableId = in.readInt();
+		variableType = in.readUnsignedByte();
+		in.skipBytes(1);	// Skip padding
+		if (variableType == TYPE_INT16)
+		{
+			integerValue = Long.valueOf(in.readUnsignedShort());
+			realValue = integerValue.doubleValue();
+			stringValue = integerValue.toString();
+		}
+		else
+		{
+			in.skipBytes(2);
+			switch(variableType)
+			{
+				case TYPE_INTEGER:
+					integerValue = Long.valueOf(in.readInt());
+					realValue = integerValue.doubleValue();
+					stringValue = integerValue.toString();
+					break;
+				case TYPE_INT64:
+					integerValue = in.readLong();
+					realValue = integerValue.doubleValue();
+					stringValue = integerValue.toString();
+					break;
+				case TYPE_FLOAT:
+					realValue = in.readDouble();
+					integerValue = realValue.longValue();
+					stringValue = realValue.toString();
+					break;
+				case TYPE_STRING:
+					int len = in.readInt();
+					while(len > 0)
+					{
+						stringValue += in.readChar();
+						len--;
+					}
+					integerValue = Long.parseLong(stringValue);
+					realValue = Double.parseDouble(stringValue);
+					break;
+				case TYPE_BINARY:
+					binaryValue = new byte[in.readInt()];
+					in.readFully(binaryValue);
+					break;
+			}
+		}
+	}
+
+	
+	public Long getAsInteger()
+	{
+		return integerValue;
+	}
+
+	public Double getAsReal()
+	{
+		return realValue;
+	}
+	
+	public String getAsString()
+	{
+		return stringValue;
+	}
+	
+	public byte[] getAsBinary()
+	{
+		return binaryValue;
+	}
+
+	/**
+	 * @return the variableId
+	 */
+	public long getVariableId()
+	{
+		return variableId;
+	}
+
+	/**
+	 * @param variableId the variableId to set
+	 */
+	public void setVariableId(long variableId)
+	{
+		this.variableId = variableId;
+	}
+
+	/**
+	 * @return the variableType
+	 */
+	public int getVariableType()
+	{
+		return variableType;
+	}
+	
+	
+	/**
+	 * Create NXCP DF structure
+	 */
+	
+	private int calculateBinarySize()
+	{
+	   int size;
+
+	   switch(variableType)
+	   {
+	      case TYPE_INTEGER:
+	         size = 12;
+	         break;
+	      case TYPE_INT64:
+	      case TYPE_FLOAT:
+	         size = 16;
+	         break;
+	      case TYPE_INT16:
+	         size = 8;
+	         break;
+	      case TYPE_STRING:
+            size = stringValue.length() + 12;
+	         break;
+	      case TYPE_BINARY:
+	         size = binaryValue.length + 12;
+	         break;
+	      default:
+	         size = 8;
+	         break;
+	   }
+		return size;
+	}
+	
+	public byte[] createNXCPDataField() throws IOException
+	{
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream(calculateBinarySize());
+		DataOutputStream out = new DataOutputStream(byteStream);
+		
+		out.writeInt(Long.valueOf(variableId).intValue());
+		out.writeByte(Long.valueOf(variableType).byteValue());
+		out.writeByte(0);		// Padding
+		if (variableType == TYPE_INT16)
+		{
+			out.writeShort(integerValue.shortValue());
+		}
+		else
+		{
+			out.writeShort(0);	// Padding
+			switch(variableType)
+			{
+				case TYPE_INTEGER:
+					out.writeInt(integerValue.intValue());
+					break;
+				case TYPE_INT64:
+					out.writeLong(integerValue);
+					break;
+				case TYPE_FLOAT:
+					out.writeDouble(realValue);
+					break;
+				case TYPE_STRING:
+					out.writeInt(stringValue.length() * 2);
+					out.writeChars(stringValue);
+					break;
+				case TYPE_BINARY:
+					out.writeInt(binaryValue.length);
+					out.write(binaryValue);
+					break;
+			}
+		}
+		
+		// Align to 8-bytes boundary
+		if ((byteStream.size() % 8) != 0)
+		{
+			out.write(new byte[8 - (byteStream.size() % 8)]);
+		}
+		
+		return byteStream.toByteArray();
+	}
+}
