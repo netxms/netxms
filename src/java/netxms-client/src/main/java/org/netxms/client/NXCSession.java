1673373df1970a3a4429ee7397e6c71c21aa16e9
Alex Kirhenshtein <alk@netxms.org>
2008-10-18 20:42:08 +0000

    - slight code reformat - dependencies fixed for -client

--- src/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
+++ src/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
@@ -1,24 +1,19 @@
 /**
- * 
+ *
  */
 package org.netxms.client;
 
-import org.netxms.base.*;
-
-
 /**
  * @author victor
- *
  */
 public class NXCSession
 {
 	private String serverAddress;
-	
+
 	public NXCSession()
 	{
-
 	}
-	
+
 	public void connect(final String addr)
 	{
 
