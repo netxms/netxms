f326f7ab9efb699505eb0f2503529df1ae867a12
Victor Kirhenshtein <victor@netxms.org>
2008-12-11 11:36:31 +0000

    updated IPSO link script

--- src/agent/core/link_ipso.sh
+++ src/agent/core/link_ipso.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc -o nxagentd -nostdlib /usr/lib/crt0.o *.o ../subagents/ipso/.libs/libnsm_ipso.a ../subagents/ping/.libs/libnsm_ping.a ../subagents/portCheck/.libs/libnsm_portCheck.a ../subagents/ups/.libs/libnsm_ups.a ../../libnetxms/.libs/libnetxms.a /usr/lib/libgcc.a -lkvm /usr/lib/libc.so.2.2 -lnet -lpth
+gcc -o nxagentd -nostdlib /usr/lib/crt0.o *.o ../subagents/ipso/.libs/libnsm_ipso.a ../subagents/logwatch/.libs/libnsm_logwatch.a ../subagents/ping/.libs/libnsm_ping.a ../subagents/portCheck/.libs/libnsm_portCheck.a ../subagents/ups/.libs/libnsm_ups.a ../../libnxlp/.libs/libnxlp.a ../../libnetxms/.libs/libnetxms.a /usr/lib/libgcc.a -lkvm /usr/lib/libc.so.2.2 -lnet -lpth
 strip nxagentd
