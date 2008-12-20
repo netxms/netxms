228ad08784b0d68be604c01145c1c4d47d6ea5a5
Victor Kirhenshtein <victor@netxms.org>
2008-12-20 10:00:10 +0000

    version number set to 0.9.0-rc1

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -30,10 +30,10 @@
 //
 
 #define NETXMS_VERSION_MAJOR        0
-#define NETXMS_VERSION_MINOR        2
-#define NETXMS_VERSION_BUILD        24
+#define NETXMS_VERSION_MINOR        9
+#define NETXMS_VERSION_BUILD        0
 #define NETXMS_VERSION_HOTFIX       0
-#define NETXMS_VERSION_STRING       _T("0.2.24")
+#define NETXMS_VERSION_STRING       _T("0.9.0-rc1")
 
 
 //
