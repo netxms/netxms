bd55edd7f745b3fb8778c8bf2c44cbc2690d46bd
Victor Kirhenshtein <victor@netxms.org>
2009-02-23 21:12:24 +0000

    object name can be changed via property page

--- src/java/netxms-eclipse/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/propertypages/General.java
+++ src/java/netxms-eclipse/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/propertypages/General.java
@@ -3,14 +3,21 @@
  */
 package org.netxms.ui.eclipse.objectmanager.propertypages;
 
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.PropertyPage;
+import org.eclipse.ui.progress.UIJob;
+import org.netxms.client.NXCException;
 import org.netxms.client.NXCObject;
+import org.netxms.ui.eclipse.objectmanager.Activator;
+import org.netxms.ui.eclipse.shared.NXMCSharedData;
 import org.netxms.ui.eclipse.tools.WidgetHelper;
 
 /**
@@ -21,6 +28,7 @@ public class General extends PropertyPage
 {
 	private Text textName;
 	private String initialName;
+	private NXCObject object;
 	
 	
 	/* (non-Javadoc)
@@ -31,7 +39,7 @@ public class General extends PropertyPage
 	{
 		Composite dialogArea = new Composite(parent, SWT.NONE);
 		
-		NXCObject object = (NXCObject)getElement().getAdapter(NXCObject.class);
+		object = (NXCObject)getElement().getAdapter(NXCObject.class);
 		if (object == null)	// Paranoid check
 			return dialogArea;
 		
@@ -56,6 +64,58 @@ public class General extends PropertyPage
 		
 		return dialogArea;
 	}
+	
+	
+	/**
+	 * Apply changes
+	 * 
+	 * @param isApply true if update operation caused by "Apply" button
+	 */
+	protected void applyChanges(final boolean isApply)
+	{
+		if (textName.getText().equals(initialName))
+			return;		// Nothing to apply
+		
+		if (isApply)
+			setValid(false);
+		
+		final String newName = new String(textName.getText());
+		new Job("Rename object") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor)
+			{
+				IStatus status;
+				
+				try
+				{
+					if (object != null)
+						NXMCSharedData.getInstance().getSession().setObjectName(object.getObjectId(), newName);
+					initialName = newName;
+					status = Status.OK_STATUS;
+				}
+				catch(Exception e)
+				{
+					status = new Status(Status.ERROR, Activator.PLUGIN_ID, 
+					                    (e instanceof NXCException) ? ((NXCException)e).getErrorCode() : 0,
+					                    "Cannot change object name: " + e.getMessage(), e);
+				}
+
+				if (isApply)
+				{
+					new UIJob("Update \"General\" property page") {
+						@Override
+						public IStatus runInUIThread(IProgressMonitor monitor)
+						{
+							General.this.setValid(true);
+							return Status.OK_STATUS;
+						}
+					}.schedule();
+				}
+
+				return status;
+			}
+		}.schedule();
+	}
 
 	
 	/* (non-Javadoc)
@@ -64,10 +124,17 @@ public class General extends PropertyPage
 	@Override
 	public boolean performOk()
 	{
-		if (!textName.getText().equals(initialName))
-		{
-			MessageDialog.openInformation(null, "OK", textName.getText());
-		}
+		applyChanges(false);
 		return true;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.preference.PreferencePage#performApply()
+	 */
+	@Override
+	protected void performApply()
+	{
+		applyChanges(true);
 	}
 }
