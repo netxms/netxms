36810fc4baf3745c0775887f5e8f586d55bc34f5
Victor Kirhenshtein <victor@netxms.org>
2009-03-05 21:19:48 +0000

    files for windows build

---
+++ src/sqlite/dllmain.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+   
+	if (dwReason == DLL_PROCESS_ATTACH)
+
+		DisableThreadLibraryCalls(hInstance);
+
+	return TRUE;
+
+}
