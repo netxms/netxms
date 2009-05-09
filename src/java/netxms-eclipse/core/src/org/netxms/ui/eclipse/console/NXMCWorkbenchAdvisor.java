4e8f89e987ca3e3237bff8c971d37231ef71547d
Alex Kirhenshtein <alk@netxms.org>
2009-05-09 15:18:55 +0000

    - code reformat

--- src/java/netxms-eclipse/core/src/org/netxms/ui/eclipse/console/NXMCWorkbenchAdvisor.java
+++ src/java/netxms-eclipse/core/src/org/netxms/ui/eclipse/console/NXMCWorkbenchAdvisor.java
@@ -8,12 +8,13 @@ public class NXMCWorkbenchAdvisor extends WorkbenchAdvisor
 {
 	private static final String PERSPECTIVE_ID = "org.netxms.ui.eclipse.console.EmptyPerspective";
 
-	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer)
 	{
 		return new NXMCWorkbenchWindowAdvisor(configurer);
 	}
-	
-	public String getInitialWindowPerspectiveId() 
+
+	public String getInitialWindowPerspectiveId()
 	{
 		return PERSPECTIVE_ID;
 	}
--- src/java/netxms-eclipse/core/src/org/netxms/ui/eclipse/console/NXMCWorkbenchWindowAdvisor.java
+++ src/java/netxms-eclipse/core/src/org/netxms/ui/eclipse/console/NXMCWorkbenchWindowAdvisor.java
@@ -15,39 +15,41 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class NXMCWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor
 {
+	public NXMCWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
+	{
+		super(configurer);
+	}
 
-    public NXMCWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer)
-    {
-        super(configurer);
-    }
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer)
+	{
+		return new NXMCActionBarAdvisor(configurer);
+	}
 
-    @Override
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer)
-    {
-        return new NXMCActionBarAdvisor(configurer);
-    }
+	@Override
+	public void preWindowOpen()
+	{
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setShowCoolBar(true);
+		configurer.setShowStatusLine(true);
+		configurer.setTitle("NetXMS Management Console");
+	}
 
-    @Override
-    public void preWindowOpen()
-    {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setShowCoolBar(true);
-        configurer.setShowStatusLine(true);
-        configurer.setTitle("NetXMS Management Console");
-    }
+	/**
+	 * Overriden to maximize the window when shwon.
+	 */
+	@Override
+	public void postWindowCreate()
+	{
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		IWorkbenchWindow window = configurer.getWindow();
+		window.getShell().setMaximized(true);
+	}
 
-    /**
-     * Overriden to maximize the window when shwon.
-     */
-    @Override
-    public void postWindowCreate() 
-    {
-   	 IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-   	 IWorkbenchWindow window = configurer.getWindow();
-   	 window.getShell().setMaximized(true);
-    }
-    
-    /* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.ui.application.WorkbenchWindowAdvisor#openIntro()
 	 */
 	@Override
@@ -55,61 +57,63 @@ public class NXMCWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor
 	{
 		doLogin();
 	}
-	
+
 	/**
 	 * Show login dialog and perform login
 	 */
 	private void doLogin()
 	{
-      IDialogSettings settings = Activator.getDefault().getDialogSettings();
-      Shell shell = getWindowConfigurer().getWindow().getShell();
-      boolean success = false;
-      
-      do
-      {
-	      LoginDialog dialog = new LoginDialog(shell);
+		IDialogSettings settings = Activator.getDefault().getDialogSettings();
+		Shell shell = getWindowConfigurer().getWindow().getShell();
+		boolean success = false;
+
+		do
+		{
+			LoginDialog dialog = new LoginDialog(shell);
 			dialog.open();
 			if (!dialog.isOk())
 				break;
-			
-	      try 
-	      {
-	      	LoginJob job = new LoginJob(settings.get("Connect.Server"), settings.get("Connect.Login"), dialog.getPassword());
-	         new ProgressMonitorDialog(shell).run(true, true, job);
-	         success = true;
-	      } 
-	      catch(InvocationTargetException e) 
-	      {
-	      	MessageDialog.openError(shell, "Exception", e.toString() + " cause: " + e.getCause().toString());
-	      } 
-	      catch(InterruptedException e) 
-	      {
-	      	MessageDialog.openError(shell, "Exception", e.toString());
-	      }
-	      catch(Exception e)
-	      {
-	      	MessageDialog.openError(shell, "Exception", e.toString());
-	      }
-      }
-      while(!success);
-      
-      if (success)
-      {
-      	Activator.getDefault().getStatusItemConnection().setText("Connected");
-     	 	try
-     	 	{
-     	 		IWorkbenchWindow window = getWindowConfigurer().getWindow();
-     	 		window.getWorkbench().showPerspective("org.netxms.ui.eclipse.console.DefaultPerspective", window);
-     	 	}
-     	 	catch(WorkbenchException e)
-     	 	{
-     	 		// TODO Auto-generated catch block
-     	 		e.printStackTrace();
-     	 	}
-      }
-      else
-      {
-      	shell.close();
-      }
+
+			try
+			{
+				LoginJob job = new LoginJob(settings.get("Connect.Server"),
+						settings.get("Connect.Login"), dialog.getPassword());
+				new ProgressMonitorDialog(shell).run(true, true, job);
+				success = true;
+			}
+			catch(InvocationTargetException e)
+			{
+				MessageDialog.openError(shell, "Exception", e.toString()
+						+ " cause: " + e.getCause().toString());
+			}
+			catch(InterruptedException e)
+			{
+				MessageDialog.openError(shell, "Exception", e.toString());
+			}
+			catch(Exception e)
+			{
+				MessageDialog.openError(shell, "Exception", e.toString());
+			}
+		} while(!success);
+
+		if (success)
+		{
+			Activator.getDefault().getStatusItemConnection().setText("Connected");
+			try
+			{
+				IWorkbenchWindow window = getWindowConfigurer().getWindow();
+				window.getWorkbench().showPerspective(
+						"org.netxms.ui.eclipse.console.DefaultPerspective", window);
+			}
+			catch(WorkbenchException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			shell.close();
+		}
 	}
 }
