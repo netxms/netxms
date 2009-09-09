9e95dce6434ff015c4c5d35ee013dda3e85a501b
Alex Kirhenshtein <alk@netxms.org>
2009-09-09 16:16:12 +0000

    code reformat

--- src/server/core/loghandle.cpp
+++ src/server/core/loghandle.cpp
@@ -108,16 +108,16 @@ bool LogHandle::query(LogFilter *filter)
 		column++;
 	}
 
-   String query;
-   switch(g_nDBSyntax)
+	String query;
+	switch(g_nDBSyntax)
 	{
 	case DB_SYNTAX_MSSQL:
 		query.AddFormattedString(_T("SELECT %s INTO %s from %s "), m_queryColumns, m_tempTable, m_log->table);
 		break;
-   case DB_SYNTAX_ORACLE:
-   case DB_SYNTAX_SQLITE:
-   case DB_SYNTAX_PGSQL:
-   case DB_SYNTAX_MYSQL:
+	case DB_SYNTAX_ORACLE:
+	case DB_SYNTAX_SQLITE:
+	case DB_SYNTAX_PGSQL:
+	case DB_SYNTAX_MYSQL:
 		query.AddFormattedString(_T("CREATE TABLE %s AS SELECT %s FROM %s"), m_tempTable, m_queryColumns, m_log->table);
 		break;
 	}
@@ -179,17 +179,17 @@ Table *LogHandle::getData(INT64 startRow, INT64 numRows)
 
 	String query;
 
-   switch(g_nDBSyntax)
+	switch(g_nDBSyntax)
 	{
 	case DB_SYNTAX_MSSQL:
 		query.AddFormattedString(_T("SELECT TOP ") INT64_FMT _T(" %s FROM %s"), startRow + numRows, m_queryColumns, m_tempTable);
 		break;
-   case DB_SYNTAX_ORACLE:
+	case DB_SYNTAX_ORACLE:
 		query.AddFormattedString(_T("SELECT %s FROM %s WHERE ROWNUM BETWEEN ") INT64_FMT _T(" AND ") INT64_FMT, m_queryColumns, m_tempTable, startRow, startRow + numRows);
 		break;
-   case DB_SYNTAX_PGSQL:
-   case DB_SYNTAX_SQLITE:
-   case DB_SYNTAX_MYSQL:
+	case DB_SYNTAX_PGSQL:
+	case DB_SYNTAX_SQLITE:
+	case DB_SYNTAX_MYSQL:
 		query.AddFormattedString(_T("SELECT %s FROM %s LIMIT ") INT64_FMT _T(" OFFSET ") INT64_FMT, m_queryColumns, m_tempTable, numRows, startRow);
 		break;
 	}
