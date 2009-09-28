e67a1d0ba766d90cf29076868f1879713b0e2d83
Victor Kirhenshtein <victor@netxms.org>
2009-09-28 19:25:11 +0000

    minor changes

--- netware.cmd
+++ netware.cmd
@@ -1,2 +1,8 @@
+@echo off
 PATH=C:\gcc-nlm\usr\bin;C:\gcc-nlm\usr\i586-netware\bin;C:\gcc-nlm\usr\lib\gcc-lib\i586-netware\3.2.3;%PATH%
+
+echo *** Welcome to NetWare build environment ***
+
 cmd
+
+echo *** Leaving NetWare build environment ***
