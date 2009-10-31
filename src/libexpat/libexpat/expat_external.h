d96a17a52aade0e139d7dfbf57ee12672bafea17
Victor Kirhenshtein <victor@netxms.org>
2009-10-31 23:54:28 +0000

    fixed typo

--- src/libexpat/libexpat/expat_external.h
+++ src/libexpat/libexpat/expat_external.h
@@ -11,11 +11,11 @@
 #define XML_USE_MSC_EXTENSIONS 1
 #endif
 
-/* NetXMS node: we use this file only internally in case when Expat
+/* NetXMS note: we use this file only internally in case when Expat
    is not available on target platform; thus we can use compiler's
-	default calling conventions whatever they are.
+   default calling conventions whatever they are.
    Leaving XMLCALL defined to __attribute__((cdecl)) with old gcc
-	causes lot of compilation errors; this broke IPSO build.
+   causes lot of compilation errors; this broke IPSO build.
 */
 
 #define XMLCALL
