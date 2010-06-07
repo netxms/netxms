8e30d3142b19bd92c5540de80b559660d69845d3
Victor Kirhenshtein <victor@netxms.org>
2010-06-07 16:40:08 +0000

    unfinished changes

---
+++ src/java/netxms-eclipse/Charts/src/org/netxms/ui/eclipse/charts/objecttabs/PerformanceTab.java
@@ -0,0 +1,51 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2010 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.ui.eclipse.charts.objecttabs;
+
+import org.eclipse.swt.widgets.Composite;
+import org.netxms.client.objects.GenericObject;
+import org.netxms.ui.eclipse.objectview.objecttabs.ObjectTab;
+
+/**
+ * Performance tab
+ *
+ */
+public class PerformanceTab extends ObjectTab
+{
+	private 
+	/* (non-Javadoc)
+	 * @see org.netxms.ui.eclipse.objectview.objecttabs.ObjectTab#createTabContent(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected void createTabContent(Composite parent)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.netxms.ui.eclipse.objectview.objecttabs.ObjectTab#objectChanged(org.netxms.client.objects.GenericObject)
+	 */
+	@Override
+	public void objectChanged(GenericObject object)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
