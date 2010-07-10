37cb1cd015ea550673ed73b432f9f48c38ab6c90
Victor Kirhenshtein <victor@netxms.org>
2010-07-10 19:17:03 +0000

    implemented object binding

--- src/java/netxms-eclipse/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/actions/BindObject.java
+++ src/java/netxms-eclipse/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/actions/BindObject.java
@@ -1,5 +1,6 @@
 package org.netxms.ui.eclipse.objectmanager.actions;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -8,20 +9,21 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.ViewPart;
+import org.netxms.client.NXCSession;
 import org.netxms.client.objects.Container;
+import org.netxms.client.objects.GenericObject;
 import org.netxms.client.objects.ServiceRoot;
+import org.netxms.ui.eclipse.jobs.ConsoleJob;
 import org.netxms.ui.eclipse.objectbrowser.dialogs.ObjectSelectionDialog;
+import org.netxms.ui.eclipse.objectmanager.Activator;
+import org.netxms.ui.eclipse.shared.NXMCSharedData;
 
 public class BindObject implements IObjectActionDelegate
 {
 	private Shell shell;
-
-	/**
-	 * Constructor
-	 */
-	public BindObject()
-	{
-	}
+	private ViewPart viewPart;
+	private long parentId;
 
 	/**
 	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
@@ -29,6 +31,7 @@ public class BindObject implements IObjectActionDelegate
 	public void setActivePart(IAction action, IWorkbenchPart targetPart)
 	{
 		shell = targetPart.getSite().getShell();
+		viewPart = (targetPart instanceof ViewPart) ? (ViewPart)targetPart : null;
 	}
 
 	/**
@@ -36,11 +39,26 @@ public class BindObject implements IObjectActionDelegate
 	 */
 	public void run(IAction action)
 	{
-		ObjectSelectionDialog dlg = new ObjectSelectionDialog(shell, null, ObjectSelectionDialog.createNodeSelectionFilter());
+		final ObjectSelectionDialog dlg = new ObjectSelectionDialog(shell, null, ObjectSelectionDialog.createNodeSelectionFilter());
 		dlg.open();
 		if (dlg.getReturnCode() == Window.OK)
 		{
-			MessageDialog.openInformation(shell, "Bind", "OK pressed");
+			final NXCSession session = NXMCSharedData.getInstance().getSession();
+			new ConsoleJob("Bind object", viewPart, Activator.PLUGIN_ID, null) {
+				@Override
+				protected String getErrorMessage()
+				{
+					return "Cannot create object binding";
+				}
+
+				@Override
+				protected void runInternal(IProgressMonitor monitor) throws Exception
+				{
+					GenericObject[] objects = dlg.getSelectedObjects();
+					for(int i = 0; i < objects.length; i++)
+						session.bindObject(parentId, objects[i].getObjectId());
+				}
+			}.start();
 		}
 	}
 
@@ -52,7 +70,21 @@ public class BindObject implements IObjectActionDelegate
 		if (selection instanceof IStructuredSelection)
 		{
 			Object obj = ((IStructuredSelection)selection).getFirstElement();
-			action.setEnabled((obj instanceof ServiceRoot) || (obj instanceof Container));
+			if ((obj instanceof ServiceRoot) || (obj instanceof Container))
+			{
+				action.setEnabled(true);
+				parentId = ((GenericObject)obj).getObjectId();
+			}
+			else
+			{
+				action.setEnabled(false);
+				parentId = 0;
+			}
+		}
+		else
+		{
+			action.setEnabled(false);
+			parentId = 0;
 		}
 	}
 }
