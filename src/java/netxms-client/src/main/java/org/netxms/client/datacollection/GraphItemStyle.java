98b11dcd3c078c12b131a910c9e53366825f5c41
Victor Kirhenshtein <victor@netxms.org>
2010-09-02 17:08:29 +0000

    some unfinished changes in predefined graph support

---
+++ src/java/netxms-client/src/main/java/org/netxms/client/datacollection/GraphItemStyle.java
@@ -0,0 +1,130 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2010 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.client.datacollection;
+
+/**
+ * This class contains styling for individual graph items
+ *
+ */
+public class GraphItemStyle
+{
+	public static final int LINE = 0;
+	public static final int AREA = 1;
+	
+	public static final int SHOW_AVERAGE = 0x0001;
+	public static final int SHOW_THRESHOLDS = 0x0002;
+	public static final int SHOW_TREND = 0x0004;
+	
+	private int type;
+	private int color;
+	private int lineWidth;
+	private int flags;
+	
+	/**
+	 * Create style record with default values.
+	 * 
+	 */
+	public GraphItemStyle()
+	{
+		type = LINE;
+		color = 0;
+		lineWidth = 0;
+		flags = 0;
+	}
+	
+	/**
+	 * constructor
+	 * 
+	 * @param type
+	 * @param color
+	 * @param lineWidth
+	 * @param flags
+	 */
+	public GraphItemStyle(int type, int color, int lineWidth, int flags)
+	{
+		this.type = type;
+		this.color = color;
+		this.lineWidth = lineWidth;
+		this.flags = flags;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public int getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public int getColor()
+	{
+		return color;
+	}
+
+	/**
+	 * @return the lineWidth
+	 */
+	public int getLineWidth()
+	{
+		return lineWidth;
+	}
+
+	/**
+	 * @return the flags
+	 */
+	public int getFlags()
+	{
+		return flags;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type)
+	{
+		this.type = type;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(int color)
+	{
+		this.color = color;
+	}
+
+	/**
+	 * @param lineWidth the lineWidth to set
+	 */
+	public void setLineWidth(int lineWidth)
+	{
+		this.lineWidth = lineWidth;
+	}
+
+	/**
+	 * @param flags the flags to set
+	 */
+	public void setFlags(int flags)
+	{
+		this.flags = flags;
+	}
+}
--- src/java/netxms-client/src/main/java/org/netxms/client/datacollection/GraphSettings.java
+++ src/java/netxms-client/src/main/java/org/netxms/client/datacollection/GraphSettings.java
@@ -19,6 +19,7 @@
 package org.netxms.client.datacollection;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.netxms.base.NXCPMessage;
 import org.netxms.client.AccessListElement;
@@ -29,10 +30,41 @@ import org.netxms.client.AccessListElement;
  */
 public class GraphSettings
 {
+	public static final int MAX_GRAPH_ITEM_COUNT = 16;
+	
+	public static final int TIME_FRAME_FIXED = 0;
+	public static final int TIME_FRAME_BACK_FROM_NOW = 1;
+	
+	public static final int TIME_UNIT_MINUTE = 0;
+	public static final int TIME_UNIT_HOUR = 1;
+	public static final int TIME_UNIT_DAY = 2;
+	
+	public static final int GF_AUTO_UPDATE     = 0x000001;
+	public static final int GF_AUTO_SCALE      = 0x000100;
+	public static final int GF_SHOW_GRID       = 0x000200;
+	public static final int GF_SHOW_LEGEND     = 0x000400;
+	public static final int GF_SHOW_RULER      = 0x000800;
+	public static final int GF_SHOW_HOST_NAMES = 0x001000;
+	public static final int GF_LOG_SCALE       = 0x002000;
+	
 	private long id;
 	private long ownerId;
 	private String name;
 	private String shortName;
+	private int flags;
+	private int timeFrameType;
+	private int timeUnit;
+	private int timeFrame;	// in given time units
+	private Date timeFrom;
+	private Date timeTo;
+	private int autoRefreshInterval;
+	private int axisColor;
+	private int backgroundColor;
+	private int gridColor;
+	private int selectionColor;
+	private int textColor;
+	private int rulerColor;
+	private GraphItemStyle[] itemStyles = new GraphItemStyle[MAX_GRAPH_ITEM_COUNT];
 	private List<AccessListElement> accessList;
 	
 	/**
@@ -69,6 +101,12 @@ public class GraphSettings
 	 */
 	private void parseGraphSettings(String settings)
 	{
+		int dciCount = 0;
+		flags = 0;
+		
+		for(int i = 0; i < MAX_GRAPH_ITEM_COUNT; i++)
+			itemStyles[i] = new GraphItemStyle();
+		
 		String[] elements = settings.split("\u007F");
 		for(int i = 0; i < elements.length; i++)
 		{
@@ -76,8 +114,126 @@ public class GraphSettings
 			if (index == -1)
 				continue;
 			
+			final String name = elements[i].substring(0, index);
+			final String value = elements[i].substring(index + 1);
 			
+			if (name.equals("A"))
+			{
+				autoRefreshInterval = safeParseInt(value, 30);
+			}
+			else if (name.equals("F"))
+			{
+				flags |= safeParseInt(value, 0);
+			}
+			else if (name.equals("N"))
+			{
+				dciCount = safeParseInt(value, 0);
+			}
+			else if (name.equals("TFT"))
+			{
+				timeFrameType = safeParseInt(value, TIME_FRAME_BACK_FROM_NOW);
+			}
+			else if (name.equals("TU"))
+			{
+				timeUnit = safeParseInt(value, TIME_UNIT_HOUR);
+			}
+			else if (name.equals("NTU"))
+			{
+				timeFrame = safeParseInt(value, 1);
+			}
+			else if (name.equals("TF"))
+			{
+				timeFrom = new Date((long)safeParseInt(value, 0) * 1000L);
+			}
+			else if (name.equals("TF"))
+			{
+				timeTo = new Date((long)safeParseInt(value, 0) * 1000L);
+			}
+			else if (name.equals("S"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_AUTO_SCALE;
+			}
+			else if (name.equals("G"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_SHOW_GRID;
+			}
+			else if (name.equals("L"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_SHOW_LEGEND;
+			}
+			else if (name.equals("R"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_SHOW_RULER;
+			}
+			else if (name.equals("H"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_SHOW_HOST_NAMES;
+			}
+			else if (name.equals("O"))
+			{
+				if (safeParseInt(value, 0) != 0)
+					flags |= GF_LOG_SCALE;
+			}
+			else if (name.equals("CA"))
+			{
+				axisColor = safeParseInt(value, 0);
+			}
+			else if (name.equals("CB"))
+			{
+				backgroundColor = safeParseInt(value, 0);
+			}
+			else if (name.equals("CG"))
+			{
+				gridColor = safeParseInt(value, 0);
+			}
+			else if (name.equals("CR"))
+			{
+				rulerColor = safeParseInt(value, 0);
+			}
+			else if (name.equals("CS"))
+			{
+				selectionColor = safeParseInt(value, 0);
+			}
+			else if (name.equals("CT"))
+			{
+				textColor = safeParseInt(value, 0);
+			}
+			else if (name.charAt(0) == 'C')	// Item color
+			{
+				int item = safeParseInt(name.substring(1), -1);
+				if ((item >= 0) && (item < MAX_GRAPH_ITEM_COUNT))
+					itemStyles[item].setColor(safeParseInt(value, 0));
+			}
+			else if (name.charAt(0) == 'T')	// Item type
+			{
+				int item = safeParseInt(name.substring(1), -1);
+				if ((item >= 0) && (item < MAX_GRAPH_ITEM_COUNT))
+					itemStyles[item].setColor(safeParseInt(value, 0));
+			}
 		}
+	}
+	
+	/**
+	 * Parse int without throwing exception
+	 * @param text text to parse
+	 * @param defVal default value to be used in case of parse error
+	 * @return parsed value
+	 */
+	static private int safeParseInt(String text, int defVal)
+	{
+		try
+		{
+			return Integer.parseInt(text);
+		}
+		catch(NumberFormatException e)
+		{
+		}
+		return defVal;
 	}
 
 	/**
