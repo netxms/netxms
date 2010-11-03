2f9865d1e2a3b740a05b067861036ef67c80e716
Alex Kirhenshtein <alk@netxms.org>
2010-11-03 21:40:15 +0000

    YesNoView sample code

--- src/java/netxms-eclipse/Charts/src/org/netxms/ui/eclipse/charts/views/YesNoView.java
+++ src/java/netxms-eclipse/Charts/src/org/netxms/ui/eclipse/charts/views/YesNoView.java
@@ -1,54 +1,75 @@
 package org.netxms.ui.eclipse.charts.views;
 
+import java.util.Random;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.FontMetrics;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+import org.netxms.ui.eclipse.tools.WidgetHelper;
 
 public class YesNoView extends ViewPart {
 
-    public static final String ID = "org.netxms.ui.eclipse.charts.views.YesNoView";
+   public static final String ID = "org.netxms.ui.eclipse.charts.views.YesNoView";
 
-    @Override
-    public void createPartControl(final Composite parent) {
-	final GridLayout gridLayout = new GridLayout();
-	gridLayout.marginTop = 0;
-	gridLayout.marginBottom = 0;
-	gridLayout.marginLeft = 0;
-	gridLayout.marginRight = 0;
+   @Override
+   public void createPartControl(final Composite parent) {
+      final GridLayout layout = new GridLayout();
+      layout.verticalSpacing = WidgetHelper.OUTER_SPACING;
+      layout.marginHeight = WidgetHelper.DIALOG_HEIGHT_MARGIN;
+      layout.marginWidth = WidgetHelper.DIALOG_WIDTH_MARGIN;
+      parent.setLayout(layout);
 
-	parent.setLayout(gridLayout);
-	final GridData data = new GridData(GridData.FILL_BOTH);
-	gridLayout.numColumns = 2;
+      layout.numColumns = 2;
 
-	final Canvas drawingCanvas = new Canvas(parent, SWT.NO_BACKGROUND);
-	drawingCanvas.setLayoutData(data);
-	drawingCanvas.addPaintListener(new PaintListener() {
+      for (int i = 0; i < 5; i++) {
+         createElement(parent, "Element " + i);
+      }
+   }
 
-	    @Override
-	    public void paintControl(PaintEvent e) {
-		Canvas canvas = (Canvas) e.widget;
-		int x = canvas.getBounds().width;
-		int y = canvas.getBounds().height;
+   private void createElement(final Composite parent, final String title) {
+      final Canvas drawingCanvas = new Canvas(parent, SWT.BORDER);
+      final GridData data = new GridData(GridData.FILL_BOTH);
+      drawingCanvas.setLayoutData(data);
 
-		final Color bgColor = parent.getShell().getDisplay().getSystemColor(SWT.COLOR_GRAY);
-		e.gc.setBackground(bgColor);
-		e.gc.fillRectangle(canvas.getBounds());
+      drawingCanvas.addPaintListener(new PaintListener() {
 
-		final Color color = parent.getShell().getDisplay().getSystemColor(SWT.COLOR_RED);
-		e.gc.setBackground(color);
-		e.gc.fillOval(0, 0, x, y);
-	    }
-	});
-    }
+         @Override
+         public void paintControl(PaintEvent e) {
+            e.gc.setAntialias(SWT.ON);
+            Canvas canvas = (Canvas) e.widget;
 
-    @Override
-    public void setFocus() {
-    }
+            final Point textExtent = e.gc.textExtent(title);
+
+            final Rectangle clientArea = canvas.getClientArea();
+
+            e.gc.drawText(title, clientArea.width / 2 - textExtent.x / 2, clientArea.height - textExtent.y);
+
+            int size = Math.min(clientArea.width, clientArea.height);
+
+            final Color color = parent.getShell().getDisplay().getSystemColor(new Random().nextBoolean() ? SWT.COLOR_RED : SWT.COLOR_GREEN);
+            e.gc.setBackground(color);
+
+            size -= textExtent.y;
+            size -= WidgetHelper.INNER_SPACING;
+
+            final int x = (clientArea.width / 2) - (size / 2);
+            final int y = (clientArea.height / 2) - (size / 2) - (textExtent.y / 2);
+            e.gc.fillOval(x, y, size, size);
+         }
+      });
+   }
+
+   @Override
+   public void setFocus() {
+   }
 
 }
