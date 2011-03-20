fed68f63367049ed5fe8e712e9ec111d3681cf5d
Alex Kalimulin <alex@netxms.org>
2011-03-20 22:45:29 +0000

    - sms pdu mode, tmp commit, ignore

---
+++ src/server/smsdrv/generic/pdumode.cpp
@@ -0,0 +1,31 @@
+/* 
+** NetXMS - Network Management System
+** Generic SMS driver
+** Copyright (C) 2003-2010 Alex Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: pdumode.cpp
+**
+**/
+
+#include "main.h"
+
+int SMSPack7BitChars(const char* input, char* output, int maxOutputLen)
+{
+	// Nothing here yet move along
+	strncpy(output, input, maxOutputLen);
+	return 0;
+}
