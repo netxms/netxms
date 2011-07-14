9dcb8e6a84bf1f5c51b8bea079a16eaa695e1c52
Victor Kirhenshtein <victor@netxms.org>
2011-07-14 19:52:30 +0000

    minor changes

--- contrib/integration/HP_EVA/hp_eva_collect_data.pl
+++ contrib/integration/HP_EVA/hp_eva_collect_data.pl
@@ -1,13 +1,31 @@
 #!/usr/bin/perl
 
+## NetXMS - Network Management System
+## Integration script for HP EVA disk arrays
+## Copyright (C) 2011 Victor Kirhenshtein
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 ### configuration ###
 ##
 $SSSU = "/opt/netxms/bin/sssu_linux_x86";
-$DATADIR="/opt/netxms/var/eva";
-$MANAGER="172.16.54.45";
+$DATADIR="/tmp";
+$MANAGER="10.0.0.1";
 $SYSTEM="EVA4400";
 $USERNAME="admin";
-$PASSWORD="%73tRUe3^*fy+";
+$PASSWORD="password";
 
 
 ### Execute SSSU ###
