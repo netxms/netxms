c45bbf8619fcb2ea8f505a174879f7e620cf60ab
Victor Kirhenshtein <victor@netxms.org>
2011-09-23 16:17:18 +0000

    removed unused views

---
+++ src/java/netxms-eclipse/DataCollection/src/org/netxms/ui/eclipse/datacollection/dialogs/CreateInterfaceDciDialog.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.netxms.ui.eclipse.datacollection.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Dialog for creating DCIs for network interface
+ */
+public class CreateInterfaceDciDialog extends Dialog
+{
+	/**
+	 * @param parentShell
+	 */
+	public CreateInterfaceDciDialog(Shell parentShell)
+	{
+		super(parentShell);
+	}
+}
