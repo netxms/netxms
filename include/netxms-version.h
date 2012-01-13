72c62a1bb9ad4b66eb09930daf08f2d789d53689
Victor Kirhenshtein <victor@netxms.org>
2012-01-13 15:10:44 +0000

    version number updated

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -30,9 +30,9 @@
 
 #define NETXMS_VERSION_MAJOR        1
 #define NETXMS_VERSION_MINOR        1
-#define NETXMS_VERSION_BUILD        8
+#define NETXMS_VERSION_BUILD        9
 #define NETXMS_VERSION_HOTFIX       0
-#define NETXMS_VERSION_STRING       _T("1.1.8")
+#define NETXMS_VERSION_STRING       _T("1.1.9")
 
 
 //
