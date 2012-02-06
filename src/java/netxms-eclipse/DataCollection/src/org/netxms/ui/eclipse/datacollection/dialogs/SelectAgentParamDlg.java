e30e2c54dcca51ce3b6419cc3bcf57eef1df2a19
Victor Kirhenshtein <victor@netxms.org>
2012-02-06 13:05:04 +0000

    fixed issue NX-73

--- src/java/netxms-eclipse/DataCollection/src/org/netxms/ui/eclipse/datacollection/dialogs/SelectAgentParamDlg.java
+++ src/java/netxms-eclipse/DataCollection/src/org/netxms/ui/eclipse/datacollection/dialogs/SelectAgentParamDlg.java
@@ -20,9 +20,23 @@ package org.netxms.ui.eclipse.datacollection.dialogs;
 
 import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.netxms.client.AgentParameter;
 import org.netxms.client.NXCSession;
+import org.netxms.client.datacollection.DataCollectionItem;
 import org.netxms.ui.eclipse.datacollection.Activator;
 import org.netxms.ui.eclipse.jobs.ConsoleJob;
 import org.netxms.ui.eclipse.shared.ConsoleSharedData;
@@ -32,6 +46,9 @@ import org.netxms.ui.eclipse.shared.ConsoleSharedData;
  */
 public class SelectAgentParamDlg extends AbstractSelectParamDlg
 {
+	private Button queryButton;
+	private Action actionQuery;
+	
 	/**
 	 * @param parentShell
 	 * @param nodeId
@@ -39,6 +56,55 @@ public class SelectAgentParamDlg extends AbstractSelectParamDlg
 	public SelectAgentParamDlg(Shell parentShell, long nodeId)
 	{
 		super(parentShell, nodeId);
+		
+		actionQuery = new Action("&Query...") {
+			@Override
+			public void run()
+			{
+				querySelectedParameter();
+			}
+		};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent)
+	{
+		((GridLayout)parent.getLayout()).numColumns++;
+		
+		queryButton = new Button(parent, SWT.PUSH);
+		queryButton.setText("&Query...");
+		GridData gd = new GridData();
+		gd.horizontalAlignment = SWT.FILL;
+		gd.grabExcessHorizontalSpace = true;
+		queryButton.setLayoutData(gd);
+		queryButton.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				querySelectedParameter();
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e)
+			{
+				widgetSelected(e);
+			}
+		});
+		
+		super.createButtonsForButtonBar(parent);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.netxms.ui.eclipse.datacollection.dialogs.AbstractSelectParamDlg#fillContextMenu(org.eclipse.jface.action.IMenuManager)
+	 */
+	@Override
+	protected void fillContextMenu(IMenuManager manager)
+	{
+		super.fillContextMenu(manager);
+		manager.add(actionQuery);
 	}
 
 	/* (non-Javadoc)
@@ -48,7 +114,6 @@ public class SelectAgentParamDlg extends AbstractSelectParamDlg
 	protected void fillParameterList()
 	{
 		final NXCSession session = (NXCSession)ConsoleSharedData.getSession();
-		
 		new ConsoleJob("Get list of supported parameters for " + object.getObjectName(), null, Activator.PLUGIN_ID, null) {
 			@Override
 			protected String getErrorMessage()
@@ -67,6 +132,54 @@ public class SelectAgentParamDlg extends AbstractSelectParamDlg
 						viewer.setInput(parameters.toArray());
 					}
 				});
+			}
+		}.start();
+	}
+
+	/**
+	 * Query current value of selected parameter
+	 */
+	protected void querySelectedParameter()
+	{
+		IStructuredSelection selection = (IStructuredSelection)viewer.getSelection();
+		if (selection.size() != 1)
+			return;
+		
+		AgentParameter p = (AgentParameter)selection.getFirstElement();
+		String n;
+		if (p.getName().contains("(*)"))
+		{
+			InputDialog dlg = new InputDialog(getShell(), "Instance", "Instance for parameter", "", null);
+			if (dlg.open() != Window.OK)
+				return;
+			
+			n = p.getName().replace("(*)", "(" + dlg.getValue() + ")");
+		}
+		else
+		{
+			n = p.getName();
+		}
+		
+		final NXCSession session = (NXCSession)ConsoleSharedData.getSession();
+		final String name = n;
+		new ConsoleJob("Query agent", null, Activator.PLUGIN_ID, null) {
+			@Override
+			protected void runInternal(IProgressMonitor monitor) throws Exception
+			{
+				final String value = session.queryParameter(object.getObjectId(), DataCollectionItem.AGENT, name);
+				runInUIThread(new Runnable() {
+					@Override
+					public void run()
+					{
+						MessageDialog.openInformation(getShell(), "Current value", "Current value is \"" + value + "\"");
+					}
+				});
+			}
+			
+			@Override
+			protected String getErrorMessage()
+			{
+				return "Cannot get current parameter value";
 			}
 		}.start();
 	}
