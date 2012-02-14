1b205cc9b0b75ec0280169a6f5b9e0bca52ca1f4
Marco Incalcaterra <jmp_3f8h@thinksoft.it>
2012-02-14 13:46:04 +0000

    This is the receiver for the BOOT_COMPLETED intent.

---
+++ android/src/console/src/org/netxms/ui/android/receiver/BootCompletedIntentReceiver.java
@@ -0,0 +1,32 @@
+package org.netxms.ui.android.receiver;
+
+import android.content.BroadcastReceiver;   
+import android.content.Context;   
+import android.content.Intent;   
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.netxms.ui.android.service.ClientConnectorService;
+
+/**
+ * @author Marco Incalcaterra
+ * 
+ * Intent receiver for boot completed broadcast message
+ * 
+ */
+public class BootCompletedIntentReceiver extends BroadcastReceiver
+{
+	 @Override  
+	 public void onReceive(Context context, Intent intent)
+	 {   
+		 if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()))
+		 {
+			 SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+			 if (sp.getBoolean("global.autostart", false))
+			 {
+				 Intent serviceIntent = new Intent(context, ClientConnectorService.class);   
+				 context.startService(serviceIntent);
+			 }
+		 }   
+	 }
+}
