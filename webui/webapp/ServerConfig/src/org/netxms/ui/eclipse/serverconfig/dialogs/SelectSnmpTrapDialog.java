e1f64896e3656158cc227c15a1e106c7a4746b14
Victor Kirhenshtein <victor@netxms.org>
2012-05-18 20:48:30 +0000

    latest changes from rcp console

--- webui/webapp/ServerConfig/src/org/netxms/ui/eclipse/serverconfig/dialogs/SelectSnmpTrapDialog.java
+++ webui/webapp/ServerConfig/src/org/netxms/ui/eclipse/serverconfig/dialogs/SelectSnmpTrapDialog.java
@@ -1,21 +1,120 @@
 /**
- * 
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2012 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 package org.netxms.ui.eclipse.serverconfig.dialogs;
 
+import java.util.List;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.netxms.client.snmp.SnmpTrap;
+import org.netxms.ui.eclipse.serverconfig.dialogs.helpers.SnmpTrapComparator;
+import org.netxms.ui.eclipse.serverconfig.dialogs.helpers.TrapListLabelProvider;
+import org.netxms.ui.eclipse.tools.WidgetHelper;
+import org.netxms.ui.eclipse.widgets.SortableTableViewer;
 
 /**
  * Select SNMP traps
  */
 public class SelectSnmpTrapDialog extends Dialog
 {
-
-	public SelectSnmpTrapDialog(Shell parentShell)
+	public static final int COLUMN_OID = 0;
+	public static final int COLUMN_EVENT = 1;
+	public static final int COLUMN_DESCRIPTION = 2;
+	
+	private List<SnmpTrap> trapList;
+	private SortableTableViewer viewer;
+	private List<SnmpTrap> selection;
+	
+	/**
+	 * @param parentShell
+	 * @param trapList
+	 */
+	public SelectSnmpTrapDialog(Shell parentShell, List<SnmpTrap> trapList)
 	{
 		super(parentShell);
-		// TODO Auto-generated constructor stub
+		this.trapList = trapList;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
+	 */
+	@Override
+	protected void configureShell(Shell newShell)
+	{
+		super.configureShell(newShell);
+		newShell.setText("Select Trap Mapping");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent)
+	{
+		final Composite dialogArea = (Composite)super.createDialogArea(parent);
+		
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = WidgetHelper.DIALOG_HEIGHT_MARGIN;
+		layout.marginWidth = WidgetHelper.DIALOG_WIDTH_MARGIN;
+		dialogArea.setLayout(layout);
+		
+		final String[] names = { "SNMP OID", "Event", "Description" };
+		final int[] widths = { 350, 250, 400 };
+		viewer = new SortableTableViewer(dialogArea, names, widths, COLUMN_OID, SWT.UP, SWT.FULL_SELECTION | SWT.MULTI | SWT.BORDER);
+		viewer.setContentProvider(new ArrayContentProvider());
+		viewer.setLabelProvider(new TrapListLabelProvider());
+		viewer.setComparator(new SnmpTrapComparator());
+		viewer.setInput(trapList.toArray());
+		
+		GridData gd = new GridData();
+		gd.horizontalAlignment = SWT.FILL;
+		gd.verticalAlignment = SWT.FILL;
+		gd.grabExcessHorizontalSpace = true;
+		gd.grabExcessVerticalSpace = true;
+		gd.heightHint = 450;
+		viewer.getControl().setLayoutData(gd);
+		
+		return dialogArea;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void okPressed()
+	{
+		selection = ((IStructuredSelection)viewer.getSelection()).toList();
+		super.okPressed();
+	}
+
+	/**
+	 * @return the selection
+	 */
+	public List<SnmpTrap> getSelection()
+	{
+		return selection;
+	}
 }
---
+++ webui/webapp/ServerConfig/src/org/netxms/ui/eclipse/serverconfig/dialogs/helpers/SnmpTrapComparator.java
@@ -0,0 +1,83 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2010 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.ui.eclipse.serverconfig.dialogs.helpers;
+
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.TableColumn;
+import org.netxms.client.NXCSession;
+import org.netxms.client.events.EventTemplate;
+import org.netxms.client.snmp.SnmpTrap;
+import org.netxms.ui.eclipse.serverconfig.dialogs.SelectSnmpTrapDialog;
+import org.netxms.ui.eclipse.shared.ConsoleSharedData;
+
+/**
+ * Comparator for SNMP trap configuration editor
+ *
+ */
+public class SnmpTrapComparator extends ViewerComparator
+{
+	private NXCSession session;
+	
+	/**
+	 * The constructor
+	 */
+	public SnmpTrapComparator()
+	{
+		session = (NXCSession)ConsoleSharedData.getSession();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ViewerComparator#compare(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(Viewer viewer, Object e1, Object e2)
+	{
+		TableColumn sortColumn = ((TableViewer)viewer).getTable().getSortColumn();
+		if (sortColumn == null)
+			return 0;
+		
+		int rc;
+		SnmpTrap trap1 = (SnmpTrap)e1;
+		SnmpTrap trap2 = (SnmpTrap)e2;
+		switch((Integer)sortColumn.getData("ID"))
+		{
+			case SelectSnmpTrapDialog.COLUMN_OID:
+				rc = trap1.getObjectId().compareTo(trap2.getObjectId());
+				break;
+			case SelectSnmpTrapDialog.COLUMN_EVENT:
+				EventTemplate evt1 = session.findEventTemplateByCode(trap1.getEventCode());
+				EventTemplate evt2 = session.findEventTemplateByCode(trap2.getEventCode());
+				String name1 = (evt1 != null) ? evt1.getName() : ("[" + Integer.toString(trap1.getEventCode()) + "]");
+				String name2 = (evt2 != null) ? evt2.getName() : ("[" + Integer.toString(trap2.getEventCode()) + "]");
+				rc = name1.compareToIgnoreCase(name2);
+				break;
+			case SelectSnmpTrapDialog.COLUMN_DESCRIPTION:
+				rc = trap1.getDescription().compareToIgnoreCase(trap2.getDescription());
+				break;
+			default:
+				rc = 0;
+				break;
+		}
+		int dir = ((TableViewer)viewer).getTable().getSortDirection();
+		return (dir == SWT.UP) ? rc : -rc;
+	}
+}
