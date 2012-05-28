478247ee50278a7c8d4c3eea3498333690b4ea46
Alex Kirhenshtein <alk@netxms.org>
2012-05-28 19:40:39 +0000

    LineChart now use default colors - quickfix

--- webui/webapp/Charts/src/org/netxms/ui/eclipse/charts/widgets/LineChart.js
+++ webui/webapp/Charts/src/org/netxms/ui/eclipse/charts/widgets/LineChart.js
@@ -6,6 +6,25 @@ qx.Class.define("org.netxms.ui.eclipse.charts.widgets.LineChart", {
 		this.setHtmlAttribute("id", id);
 		this._id = id;
 		this._plot = null;
+		
+		this._defaultColors = [
+                "rgb(64,105,156)",
+                "rgb(158,65,62)",
+                "rgb(127,154,72)",
+                "rgb(105,81,133)",
+                "rgb(60,141,163)",
+                "rgb(204,123,56)",
+                "rgb(79,129,189)",
+                "rgb(192,80,77)",
+                "rgb(155,187,89)",
+                "rgb(128,100,162)",
+                "rgb(75,172,198)",
+                "rgb(247,150,70)",
+                "rgb(170,186,215)",
+                "rgb(217,170,169)",
+                "rgb(198,214,172)",
+                "rgb(186,176,201)"
+		];
 	},
 	
 	properties : {
@@ -44,13 +63,13 @@ qx.Class.define("org.netxms.ui.eclipse.charts.widgets.LineChart", {
         },
 
         _applyLegend : function() {
-        	alert(this.getLegend());
+        	//alert(this.getLegend());
         },
         
 		update : function() {
 			qx.ui.core.Widget.flushGlobalQueues();
-			
 			var plotData = [];
+			
 			for (var i = 1; i < arguments.length; i++) {
 				elements = arguments[i].split("|");
 				var dciPoints = [];
@@ -58,7 +77,7 @@ qx.Class.define("org.netxms.ui.eclipse.charts.widgets.LineChart", {
 					var point = [elements[j], elements[j + 1]];
 					dciPoints.push(point);
 				}
-				plotData.push({ label : arguments[0][i - 1], data : dciPoints });
+				plotData.push({ label : arguments[0][i - 1], data : dciPoints, color: this._defaultColors[i - 1] });
 			}
 			//console.log(arguments);
 			//console.log(plotData);
