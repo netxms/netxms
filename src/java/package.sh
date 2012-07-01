8f385f4b916f7fb76e39cd95bdb4e10de612d507
Alex Kirhenshtein <alk@netxms.org>
2012-07-01 09:29:04 +0000

    Android libs now copied to proper directory

--- src/java/package.sh
+++ src/java/package.sh
@@ -9,9 +9,9 @@ cp netxms-base/target/netxms-base-$version.jar netxms-eclipse/library/jar/
 cp netxms-client-api/target/netxms-client-api-$version.jar netxms-eclipse/library/jar/
 cp netxms-client/target/netxms-client-$version.jar netxms-eclipse/core/jar/
 
-cp netxms-base/target/netxms-base-$version.jar ../../android/src/console/jar
-cp netxms-client-api/target/netxms-client-api-$version.jar ../../android/src/console/jar
-cp netxms-client/target/netxms-client-$version.jar ../../android/src/console/jar
+cp netxms-base/target/netxms-base-$version.jar ../../android/src/console/libs/
+cp netxms-client-api/target/netxms-client-api-$version.jar ../../android/src/console/libs/
+cp netxms-client/target/netxms-client-$version.jar ../../android/src/console/libs/
 
 cp netxms-base/target/netxms-base-$version.jar ../../webui/webapp/Core/jar
 cp netxms-client-api/target/netxms-client-api-$version.jar ../../webui/webapp/Core/jar
