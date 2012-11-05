4917e8dd71e06a4244f9a85e6fc67d14aef82474
Alex Kirhenshtein <alk@netxms.org>
2012-11-05 16:15:13 +0000

    Packaging script now use version from configure.ac

--- packages/osx/generate.sh
+++ packages/osx/generate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-version=1.2.3
+version=`grep AC_INIT ../../configure.ac|cut -d'[' -f3|cut -d']' -f1`
 
-cp Info.plist "dist/NetXMS Console.app/Contents/"
-/Users/alk/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon /Users/alk/Development/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background /Users/alk/Development/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
+rm -rf dist/*
+~/Development/yoursway-eclipse-osx-repackager/EclipseOSXRepackager out/eclipse dist/NetXMS\ Console.app
+sed "s,@version@,${version},g" < Info.plist > "dist/NetXMS Console.app/Contents/Info.plist"
+~/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon ~/Development/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
