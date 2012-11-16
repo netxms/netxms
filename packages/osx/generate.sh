e93a76499fbe715458cb120e8742ed0b624f7a87
Alex Kirhenshtein <alk@netxms.org>
2012-11-16 02:14:09 +0000

    resulting dmg removed prior to generation

--- packages/osx/generate.sh
+++ packages/osx/generate.sh
@@ -2,7 +2,8 @@
 
 version=`grep AC_INIT ../../configure.ac|cut -d'[' -f3|cut -d']' -f1`
 
-rm -rf dist/*
+rm -rf dist/* nxmc-${version}.dmg
 ~/Development/yoursway-eclipse-osx-repackager/EclipseOSXRepackager out/eclipse dist/NetXMS\ Console.app
-sed "s,@version@,${version},g" < Info.plist > "dist/NetXMS Console.app/Contents/Info.plist"
+
+cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
 ~/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon ~/Development/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
