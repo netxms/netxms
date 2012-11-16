40ced67fbd1ba6f5ebf65b0e6ae16df65461f1e1
Alex Kirhenshtein <alk@netxms.org>
2012-11-16 02:14:29 +0000

    a

--- packages/osx/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-version=`grep AC_INIT ../../configure.ac|cut -d'[' -f3|cut -d']' -f1`
-
-rm -rf dist/* nxmc-${version}.dmg
-~/Development/yoursway-eclipse-osx-repackager/EclipseOSXRepackager out/eclipse dist/NetXMS\ Console.app
-
-cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
-~/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon ~/Development/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
