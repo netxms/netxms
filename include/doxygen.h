41274f4d9302edfb089cbd6b375e33acb54abcb6
Alex Kirhenshtein <alk@netxms.org>
2013-04-03 14:28:15 +0000

    gitignore updated; some missing files added

---
+++ include/doxygen.h
@@ -0,0 +1,9 @@
+/**
+ * @mainpage
+ * Text for main page
+ *
+ * \defgroup server Core Server
+ * \defgroup agent Core Agent
+ * \defgroup SNMP SNMP Client Library
+ * \defgroup NXSL NetXMS Scripting Language Library
+ */
