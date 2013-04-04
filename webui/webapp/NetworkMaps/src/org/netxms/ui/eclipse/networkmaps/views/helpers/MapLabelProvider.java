afec5fef6398e87fc51cef138f5ab9c636d266a6
Victor Kirhenshtein <victor@netxms.org>
2013-04-04 10:25:33 +0000

    latest changes from RCP console

--- webui/webapp/NetworkMaps/src/org/netxms/ui/eclipse/networkmaps/views/helpers/MapLabelProvider.java
+++ webui/webapp/NetworkMaps/src/org/netxms/ui/eclipse/networkmaps/views/helpers/MapLabelProvider.java
@@ -89,6 +89,7 @@ public class MapLabelProvider extends LabelProvider implements IFigureProvider, 
 	private Image imgSubnet;
 	private Image imgService;
 	private Image imgCluster;
+	private Image imgAccessPoint;
 	private Image imgOther;
 	private Image imgUnknown;
 	private Image imgResCluster;
@@ -132,6 +133,7 @@ public class MapLabelProvider extends LabelProvider implements IFigureProvider, 
 		imgSubnet = Activator.getImageDescriptor("icons/objects/subnet.png").createImage();
 		imgService = Activator.getImageDescriptor("icons/objects/service.png").createImage();
 		imgCluster = Activator.getImageDescriptor("icons/objects/cluster.png").createImage();
+		imgAccessPoint = Activator.getImageDescriptor("icons/objects/accesspoint.png").createImage();
 		imgOther = Activator.getImageDescriptor("icons/objects/other.png").createImage();
 		imgUnknown = Activator.getImageDescriptor("icons/objects/unknown.png").createImage();
 		imgResCluster = Activator.getImageDescriptor("icons/resources/cluster_res.png").createImage();
@@ -211,6 +213,8 @@ public class MapLabelProvider extends LabelProvider implements IFigureProvider, 
 						return imgService;
 					case AbstractObject.OBJECT_CLUSTER:
 						return imgCluster;
+					case AbstractObject.OBJECT_ACCESSPOINT:
+						return imgAccessPoint;
 					default:
 						return imgOther;
 				}
@@ -307,6 +311,7 @@ public class MapLabelProvider extends LabelProvider implements IFigureProvider, 
 		imgSubnet.dispose();
 		imgService.dispose();
 		imgCluster.dispose();
+		imgAccessPoint.dispose();
 		imgOther.dispose();
 		imgUnknown.dispose();
 		
