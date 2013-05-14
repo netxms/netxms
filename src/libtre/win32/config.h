e271d51451b95d99dde28e4872e992341c3aa76e
Victor Kirhenshtein <victor@netxms.org>
2013-05-14 08:29:38 +0000

    fixed windows build error

--- src/libtre/win32/config.h
+++ src/libtre/win32/config.h
@@ -73,11 +73,12 @@
 
 /* Define to 1 if you have the <regex.h> header file. */
 #undef HAVE_REGEX_H
-                                                                                                /* Define to 1 if the system has the type `reg_errcode_t'. */
+
+/* Define to 1 if the system has the type `reg_errcode_t'. */
 #undef HAVE_REG_ERRCODE_T
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
+#undef HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
