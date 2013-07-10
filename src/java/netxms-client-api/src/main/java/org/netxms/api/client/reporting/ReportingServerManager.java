a9543dfdec9b6da79d2c13af534f2858120a8470
Alex Kirhenshtein <alk@netxms.org>
2013-07-10 11:17:46 +0000

    Reporting interface adjusted

--- src/java/netxms-client-api/src/main/java/org/netxms/api/client/reporting/ReportingServerManager.java
+++ src/java/netxms-client-api/src/main/java/org/netxms/api/client/reporting/ReportingServerManager.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.netxms.api.client.NetXMSClientException;
-import org.netxms.api.client.constants.ScheduleType;
 
 public interface ReportingServerManager
 {
@@ -17,8 +16,10 @@ public interface ReportingServerManager
 
 	UUID executeReport(UUID reportId, Map<String, String> parameters) throws NetXMSClientException, IOException;
 
-	void scheduleReport(UUID reportId, ScheduleType scheduleType, Date startTime, int daysOfWeek, int daysOfMonth,
-			Map<String, String> parameters) throws NetXMSClientException, IOException;
+	void scheduleReport(UUID reportId, Date startTime, int daysOfWeek, int daysOfMonth, Map<String, String> parameters)
+			throws NetXMSClientException, IOException;
+
+	List<ReportingJob> listScheduledJobs();
 
 	List<ReportResult> listReportResults(UUID reportId) throws NetXMSClientException, IOException;
 
