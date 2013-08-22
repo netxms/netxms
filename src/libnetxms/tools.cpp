79ca7ae661bdfe331bdc5f418d0f0105527abac5
Alex Kirhenshtein <alk@netxms.org>
2013-08-22 14:36:48 +0000

    HPUX networking issues fixed

--- src/libnetxms/tools.cpp
+++ src/libnetxms/tools.cpp
@@ -866,7 +866,11 @@ retry:
 #endif
 		if (nRet <= 0)
 		{
-			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			if (WSAGetLastError() == WSAEWOULDBLOCK
+#ifndef _WIN32
+			|| errno == EAGAIN
+#endif
+			)
 			{
 				// Wait until socket becomes available for writing
 				struct timeval tv;
