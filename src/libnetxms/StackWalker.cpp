07a89845c353882ae39b488e221d8076a2cd4ca2
Alex Kirhenshtein <alk@netxms.org>
2014-01-29 23:02:11 +0530

    License updated

--- src/libnetxms/StackWalker.cpp
+++ src/libnetxms/StackWalker.cpp
@@ -2,6 +2,9 @@
  * 
  * StackWalker.cpp
  *
+ * License info from the web site:
+ * This article, along with any associated source code and files,
+ * is licensed under The BSD License
  *
  * History:
  *  2005-07-27   v1    - First public release on http://www.codeproject.com/
