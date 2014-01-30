96bd6290e963f06606fb72d88964c2002ad9299a
Victor Kirhenshtein <victor@netxms.org>
2014-01-30 17:48:03 +0200

    imodifyCustomObject always called

--- src/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
+++ src/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
@@ -3302,8 +3302,7 @@ public class NXCSession implements Session, ScriptLibraryManager, UserManager, S
     * @throws IOException  if socket I/O error occurs
     * @throws NXCException if NetXMS server returns an error or operation was timed out
     */
-   public void modifyObject(final NXCObjectModificationData data, final Object userData)
-      throws IOException, NXCException
+   public void modifyObject(final NXCObjectModificationData data, final Object userData) throws IOException, NXCException
    {
       NXCPMessage msg = newMessage(NXCPCodes.CMD_MODIFY_OBJECT);
       msg.setVariableInt32(NXCPCodes.VID_OBJECT_ID, (int) data.getObjectId());
@@ -3677,7 +3676,7 @@ public class NXCSession implements Session, ScriptLibraryManager, UserManager, S
          msg.setVariableInt16(NXCPCodes.VID_HEIGHT, data.getHeight());
       }
 
-      if (userData != null) modifyCustomObject(data, userData, msg);
+      modifyCustomObject(data, userData, msg);
 
       sendMessage(msg);
       waitForRCC(msg.getMessageId());
