3c06047f9d254896ab4578e4217e6d1c1e5e1aac
Victor Kirhenshtein <victor@netxms.org>
2014-02-05 12:07:18 +0200

    geolocation background can be disabled

---
+++ src/java/netxms-eclipse/NetworkMaps/src/org/netxms/ui/eclipse/networkmaps/PreferenceInitializer.java
@@ -0,0 +1,41 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2014 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.ui.eclipse.networkmaps;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+/**
+ * Preference initializer
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
+   /* (non-Javadoc)
+    * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+    */
+   @Override
+   public void initializeDefaultPreferences()
+   {
+      final IPreferenceStore ps = Activator.getDefault().getPreferenceStore();
+      ps.setDefault("DISABLE_GEOLOCATION_BACKGROUND", false); //$NON-NLS-1$
+      ps.setDefault("NetMap.ShowStatusIcon", true); //$NON-NLS-1$
+      ps.setDefault("NetMap.ShowStatusFrame", false); //$NON-NLS-1$
+      ps.setDefault("NetMap.ShowStatusBackground", false); //$NON-NLS-1$
+   }
+}
--- webui/webapp/NetworkMaps/src/org/netxms/ui/eclipse/networkmaps/propertypages/MapBackground.java
+++ webui/webapp/NetworkMaps/src/org/netxms/ui/eclipse/networkmaps/propertypages/MapBackground.java
@@ -20,6 +20,7 @@ package org.netxms.ui.eclipse.networkmaps.propertypages;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.preference.ColorSelector;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -65,6 +66,7 @@ public class MapBackground extends PropertyPage
 	private Spinner zoomSpinner;
 	private Label zoomLabel;
 	private ColorSelector backgroundColor;
+	private boolean disableGeolocationBackground;
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
@@ -73,6 +75,9 @@ public class MapBackground extends PropertyPage
 	protected Control createContents(Composite parent)
 	{
 		Composite dialogArea = new Composite(parent, SWT.NONE);
+		
+      final IPreferenceStore ps = Activator.getDefault().getPreferenceStore();
+      disableGeolocationBackground = ps.getBoolean("DISABLE_GEOLOCATION_BACKGROUND");
 		
 		object = (NetworkMap)getElement().getAdapter(NetworkMap.class);
 
@@ -96,7 +101,7 @@ public class MapBackground extends PropertyPage
 			@Override
 			public void widgetSelected(SelectionEvent e)
 			{
-				enableControls(radioTypeImage.getSelection(), radioTypeGeoMap.getSelection());
+		      enableControls(radioTypeImage.getSelection(), disableGeolocationBackground ? false : radioTypeGeoMap.getSelection());
 			}
 			
 			@Override
@@ -116,10 +121,18 @@ public class MapBackground extends PropertyPage
       radioTypeImage.setSelection(!object.getBackground().equals(NXCommon.EMPTY_GUID) && !object.getBackground().equals(NetworkMap.GEOMAP_BACKGROUND));
       radioTypeImage.addSelectionListener(listener);
 
-      radioTypeGeoMap = new Button(typeGroup, SWT.RADIO);
-      radioTypeGeoMap.setText(Messages.get().MapBackground_GeoMap);
-      radioTypeGeoMap.setSelection(object.getBackground().equals(NetworkMap.GEOMAP_BACKGROUND));
-      radioTypeGeoMap.addSelectionListener(listener);
+      if (!disableGeolocationBackground)
+      {
+         radioTypeGeoMap = new Button(typeGroup, SWT.RADIO);
+         radioTypeGeoMap.setText(Messages.get().MapBackground_GeoMap);
+         radioTypeGeoMap.setSelection(object.getBackground().equals(NetworkMap.GEOMAP_BACKGROUND));
+         radioTypeGeoMap.addSelectionListener(listener);
+      }
+      else
+      {
+         if (object.getBackground().equals(NetworkMap.GEOMAP_BACKGROUND))
+            radioTypeNone.setSelection(true);
+      }
       
       image = new ImageSelector(dialogArea, SWT.NONE);
       image.setLabel(Messages.get().MapBackground_BkgndImage);
@@ -130,92 +143,95 @@ public class MapBackground extends PropertyPage
       gd.horizontalAlignment = SWT.FILL;
       image.setLayoutData(gd);
       
-      Group geomapGroup = new Group(dialogArea, SWT.NONE);
-      geomapGroup.setText(Messages.get().MapBackground_GroupGeoMap);
-      gd = new GridData();
-      gd.grabExcessHorizontalSpace = true;
-      gd.horizontalAlignment = SWT.FILL;
-      geomapGroup.setLayoutData(gd);
-      layout = new GridLayout();
-      geomapGroup.setLayout(layout);
-      
-      GeoLocation gl = object.getBackgroundLocation();
-
-      latitude = new LabeledText(geomapGroup, SWT.NONE);
-      latitude.setLabel(Messages.get().MapBackground_Lat);
-   	latitude.setText(gl.getLatitudeAsString());
-      gd = new GridData();
-      gd.grabExcessHorizontalSpace = true;
-      gd.horizontalAlignment = SWT.FILL;
-      latitude.setLayoutData(gd);
-      
-      longitude = new LabeledText(geomapGroup, SWT.NONE);
-      longitude.setLabel(Messages.get().MapBackground_Lon);
-      longitude.setText(gl.getLongitudeAsString());
-      gd = new GridData();
-      gd.grabExcessHorizontalSpace = true;
-      gd.horizontalAlignment = SWT.FILL;
-      longitude.setLayoutData(gd);
-      
-      Composite zoomGroup = new Composite(geomapGroup, SWT.NONE);
-      layout = new GridLayout();
-      layout.numColumns = 2;
-      layout.horizontalSpacing = WidgetHelper.OUTER_SPACING;
-      layout.marginHeight = 0;
-      layout.marginWidth = 0;
-      layout.marginTop = WidgetHelper.OUTER_SPACING;
-      zoomGroup.setLayout(layout);
-      gd = new GridData();
-      gd.horizontalAlignment = SWT.FILL;
-      gd.grabExcessHorizontalSpace = true;
-      zoomGroup.setLayoutData(gd);
-      
-      zoomLabel = new Label(zoomGroup, SWT.NONE);
-      zoomLabel.setText(Messages.get().MapBackground_ZoomLevel);
-      gd = new GridData();
-      gd.horizontalAlignment = SWT.LEFT;
-      gd.horizontalSpan = 2;
-      zoomLabel.setLayoutData(gd);
-      
-      zoomScale = new Scale(zoomGroup, SWT.HORIZONTAL);
-      zoomScale.setMinimum(1);
-      zoomScale.setMaximum(18);
-      zoomScale.setSelection(object.getBackgroundZoom());
-      gd = new GridData();
-      gd.horizontalAlignment = SWT.FILL;
-      gd.grabExcessHorizontalSpace = true;
-      zoomScale.setLayoutData(gd);
-      zoomScale.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e)
-			{
-				zoomSpinner.setSelection(zoomScale.getSelection());
-			}
-
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e)
-			{
-				widgetSelected(e);
-			}
-      });
-      
-      zoomSpinner = new Spinner(zoomGroup, SWT.BORDER);
-      zoomSpinner.setMinimum(1);
-      zoomSpinner.setMaximum(18);
-      zoomSpinner.setSelection(object.getBackgroundZoom());
-      zoomSpinner.addSelectionListener(new SelectionListener() {
-			@Override
-			public void widgetSelected(SelectionEvent e)
-			{
-				zoomScale.setSelection(zoomSpinner.getSelection());
-			}
-			
-			@Override
-			public void widgetDefaultSelected(SelectionEvent e)
-			{
-				widgetSelected(e);
-			}
-		});
+      if (!disableGeolocationBackground)
+      {
+         Group geomapGroup = new Group(dialogArea, SWT.NONE);
+         geomapGroup.setText(Messages.get().MapBackground_GroupGeoMap);
+         gd = new GridData();
+         gd.grabExcessHorizontalSpace = true;
+         gd.horizontalAlignment = SWT.FILL;
+         geomapGroup.setLayoutData(gd);
+         layout = new GridLayout();
+         geomapGroup.setLayout(layout);
+         
+         GeoLocation gl = object.getBackgroundLocation();
+   
+         latitude = new LabeledText(geomapGroup, SWT.NONE);
+         latitude.setLabel(Messages.get().MapBackground_Lat);
+      	latitude.setText(gl.getLatitudeAsString());
+         gd = new GridData();
+         gd.grabExcessHorizontalSpace = true;
+         gd.horizontalAlignment = SWT.FILL;
+         latitude.setLayoutData(gd);
+         
+         longitude = new LabeledText(geomapGroup, SWT.NONE);
+         longitude.setLabel(Messages.get().MapBackground_Lon);
+         longitude.setText(gl.getLongitudeAsString());
+         gd = new GridData();
+         gd.grabExcessHorizontalSpace = true;
+         gd.horizontalAlignment = SWT.FILL;
+         longitude.setLayoutData(gd);
+         
+         Composite zoomGroup = new Composite(geomapGroup, SWT.NONE);
+         layout = new GridLayout();
+         layout.numColumns = 2;
+         layout.horizontalSpacing = WidgetHelper.OUTER_SPACING;
+         layout.marginHeight = 0;
+         layout.marginWidth = 0;
+         layout.marginTop = WidgetHelper.OUTER_SPACING;
+         zoomGroup.setLayout(layout);
+         gd = new GridData();
+         gd.horizontalAlignment = SWT.FILL;
+         gd.grabExcessHorizontalSpace = true;
+         zoomGroup.setLayoutData(gd);
+         
+         zoomLabel = new Label(zoomGroup, SWT.NONE);
+         zoomLabel.setText(Messages.get().MapBackground_ZoomLevel);
+         gd = new GridData();
+         gd.horizontalAlignment = SWT.LEFT;
+         gd.horizontalSpan = 2;
+         zoomLabel.setLayoutData(gd);
+         
+         zoomScale = new Scale(zoomGroup, SWT.HORIZONTAL);
+         zoomScale.setMinimum(1);
+         zoomScale.setMaximum(18);
+         zoomScale.setSelection(object.getBackgroundZoom());
+         gd = new GridData();
+         gd.horizontalAlignment = SWT.FILL;
+         gd.grabExcessHorizontalSpace = true;
+         zoomScale.setLayoutData(gd);
+         zoomScale.addSelectionListener(new SelectionListener() {
+   			@Override
+   			public void widgetSelected(SelectionEvent e)
+   			{
+   				zoomSpinner.setSelection(zoomScale.getSelection());
+   			}
+   
+   			@Override
+   			public void widgetDefaultSelected(SelectionEvent e)
+   			{
+   				widgetSelected(e);
+   			}
+         });
+         
+         zoomSpinner = new Spinner(zoomGroup, SWT.BORDER);
+         zoomSpinner.setMinimum(1);
+         zoomSpinner.setMaximum(18);
+         zoomSpinner.setSelection(object.getBackgroundZoom());
+         zoomSpinner.addSelectionListener(new SelectionListener() {
+   			@Override
+   			public void widgetSelected(SelectionEvent e)
+   			{
+   				zoomScale.setSelection(zoomSpinner.getSelection());
+   			}
+   			
+   			@Override
+   			public void widgetDefaultSelected(SelectionEvent e)
+   			{
+   				widgetSelected(e);
+   			}
+   		});
+      }
       
       Composite colorArea = new Composite(dialogArea, SWT.NONE);
       layout = new GridLayout();
@@ -234,7 +250,7 @@ public class MapBackground extends PropertyPage
       backgroundColor = new ColorSelector(colorArea);
       backgroundColor.setColorValue(ColorConverter.rgbFromInt(object.getBackgroundColor()));
 
-		enableControls(radioTypeImage.getSelection(), radioTypeGeoMap.getSelection());
+		enableControls(radioTypeImage.getSelection(), disableGeolocationBackground ? false : radioTypeGeoMap.getSelection());
       return dialogArea;
 	}
 	
@@ -245,11 +261,14 @@ public class MapBackground extends PropertyPage
 	private void enableControls(boolean imageGroup, boolean geoGroup)
 	{
 		image.setEnabled(imageGroup);
-		latitude.setEnabled(geoGroup);
-		longitude.setEnabled(geoGroup);
-		zoomLabel.setEnabled(geoGroup);
-		zoomScale.setEnabled(geoGroup);
-		zoomSpinner.setEnabled(geoGroup);
+		if (!disableGeolocationBackground)
+		{
+   		latitude.setEnabled(geoGroup);
+   		longitude.setEnabled(geoGroup);
+   		zoomLabel.setEnabled(geoGroup);
+   		zoomScale.setEnabled(geoGroup);
+   		zoomSpinner.setEnabled(geoGroup);
+		}
 	}
 
 	/**
@@ -269,7 +288,7 @@ public class MapBackground extends PropertyPage
 		{
 			md.setMapBackground(image.getImageGuid(), new GeoLocation(false), 0, ColorConverter.rgbToInt(backgroundColor.getColorValue()));
 		}
-		else if (radioTypeGeoMap.getSelection())
+		else if (!disableGeolocationBackground && radioTypeGeoMap.getSelection())
 		{
 			GeoLocation location;
 			try
