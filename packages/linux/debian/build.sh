62aab1c49743739a0dd3fc3f290ae2d4902bc124
Victor Kirhenshtein <victor@netxms.org>
2014-02-17 00:06:54 +0200

    added Debian package with Oracle driver

--- packages/linux/debian/build.sh
+++ packages/linux/debian/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+mkdir -p output
+
 dpkg-deb --build netxms-base output &&
 dpkg-deb --build netxms-agent output &&
 dpkg-deb --build netxms-server output &&
 dpkg-deb --build netxms-server-mysql output &&
+dpkg-deb --build netxms-server-oracle output &&
 dpkg-deb --build netxms-server-pgsql output &&
 dpkg-deb --build netxms-server-odbc output &&
 echo "Done."
--- packages/linux/debian/clean.sh
+++ packages/linux/debian/clean.sh
@@ -4,6 +4,7 @@ rm -rf netxms-base
 rm -rf netxms-agent
 rm -rf netxms-server
 rm -rf netxms-server-mysql
+rm -rf netxms-server-oracle
 rm -rf netxms-server-pgsql
 rm -rf netxms-server-odbc
 rm -rf output/*
