0b641ee21c11b140dedfec7c8d64f590b19dc0b5
Victor Kirhenshtein <victor@netxms.org>
2014-04-28 13:04:41 +0300

    new system access right

--- src/java/netxms-eclipse/UserManager/src/org/netxms/ui/eclipse/usermanager/propertypages/SystemRights.java
+++ src/java/netxms-eclipse/UserManager/src/org/netxms/ui/eclipse/usermanager/propertypages/SystemRights.java
@@ -63,6 +63,7 @@ public class SystemRights extends PropertyPage
 		dialogArea.setLayout(layout);
 		
 		addCheckbox(dialogArea, UserAccessRights.SYSTEM_ACCESS_DELETE_ALARMS, Messages.get().SystemRights_DeleteAlarms);
+      addCheckbox(dialogArea, UserAccessRights.SYSTEM_ACCESS_UNLINK_ISSUES, "Unlink helpdesk tickets");
 		addCheckbox(dialogArea, UserAccessRights.SYSTEM_ACCESS_MANAGE_ACTIONS, Messages.get().SystemRights_ConfigureActions);
 		addCheckbox(dialogArea, UserAccessRights.SYSTEM_ACCESS_EDIT_EVENT_DB, Messages.get().SystemRights_ConfigureEvents);
 		addCheckbox(dialogArea, UserAccessRights.SYSTEM_ACCESS_VIEW_EVENT_DB, Messages.get().SystemRights_ViewEventConfig);
