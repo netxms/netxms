3130994a2b593ff7be2a5184668c1cc43188301b
Alex Kirhenshtein <alk@netxms.org>
2014-09-23 18:29:24 +0300

    typo

--- packages/osx/package.sh
+++ packages/osx/package.sh
@@ -9,5 +9,4 @@ rm -rf dist/* nxmc-${version}.dmg
 
 cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
 codesign --deep --force -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' -v 'dist/NetXMS Console.app'
-exit
 ~/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon ~/Development/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
