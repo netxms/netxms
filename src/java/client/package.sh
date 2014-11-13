35e1cf53e9e064f71227ad244f24ffffb23e1a9d
zev <zev@radensolutions.com>
2014-11-13 17:33:03 +0200

    Fixed jar build script for linux

--- src/java/client/package.sh
+++ src/java/client/package.sh
@@ -11,27 +11,11 @@ mvn -Dmaven.test.skip=true install
 version=`grep '<version>' pom.xml | cut -f 2 -d \> | cut -f 1 -d \<|head -n1`
 
 rm -f \
-  netxms-eclipse/Core/jar/netxms-base*.jar \
-  netxms-eclipse/Core/jar/netxms-client*.jar \
-  netxms-eclipse/Core/jar/certificate-manager*.jar \
-  ../../android/src/console/libs/netxms-base*.jar \
-  ../../android/src/console/libs/netxms-client*.jar \
-  ../../android/src/console/libs/certificate-manager*.jar \
-  ../../webui/webapp/Core/jar/netxms-base*.jar \
-  ../../webui/webapp/Core/jar/netxms-client*.jar \
-  ../../webui/webapp/Core/jar/certificate-manager*.jar
+  ../../../android/src/console/libs/netxms-base*.jar \
+  ../../../android/src/console/libs/netxms-client*.jar \
+  ../../../android/src/console/libs/netxms-client-api*.jar 
 
-cp netxms-base/target/netxms-base-$version.jar netxms-eclipse/Core/jar/
-cp netxms-client-api/target/netxms-client-api-$version.jar netxms-eclipse/Core/jar/
-cp netxms-client/target/netxms-client-$version.jar netxms-eclipse/Core/jar/
-cp certificate-manager/target/certificate-manager-$version.jar netxms-eclipse/Core/jar/
+cp netxms-base/target/netxms-base-$version.jar ../../../android/src/console/libs/
+cp netxms-client-api/target/netxms-client-api-$version.jar ../../../android/src/console/libs/
+cp netxms-client/target/netxms-client-$version.jar ../../../android/src/console/libs/
 
-cp netxms-base/target/netxms-base-$version.jar ../../android/src/console/libs/
-cp netxms-client-api/target/netxms-client-api-$version.jar ../../android/src/console/libs/
-cp netxms-client/target/netxms-client-$version.jar ../../android/src/console/libs/
-cp certificate-manager/target/certificate-manager-$version.jar ../../android/src/console/libs/
-
-cp netxms-base/target/netxms-base-$version.jar ../../webui/webapp/Core/jar
-cp netxms-client-api/target/netxms-client-api-$version.jar ../../webui/webapp/Core/jar
-cp netxms-client/target/netxms-client-$version.jar ../../webui/webapp/Core/jar 
-cp certificate-manager/target/certificate-manager-$version.jar ../../webui/webapp/Core/jar 
