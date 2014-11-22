33d770322eb10fba835fdaa237895c860354ba9d
Victor Kirhenshtein <victor@netxms.org>
2014-11-22 17:51:10 +0200

    Added driver for HP A-series and V-series switches

---
+++ src/server/drivers/hpsw/hpsw.cpp
@@ -0,0 +1,140 @@
+/* 
+** NetXMS - Network Management System
+** Driver for HP switches with HH3C MIB support
+** Copyright (C) 2003-2014 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: hpsw.cpp
+**/
+
+#include "hpsw.h"
+
+/**
+ * Driver name
+ */
+static TCHAR s_driverName[] = _T("HPSW");
+
+/**
+ * Driver version
+ */
+static TCHAR s_driverVersion[] = NETXMS_VERSION_STRING;
+
+/**
+ * Get driver name
+ */
+const TCHAR *HPSwitchDriver::getName()
+{
+	return s_driverName;
+}
+
+/**
+ * Get driver version
+ */
+const TCHAR *HPSwitchDriver::getVersion()
+{
+	return s_driverVersion;
+}
+
+/**
+ * Check if given device can be potentially supported by driver
+ *
+ * @param oid Device OID
+ */
+int HPSwitchDriver::isPotentialDevice(const TCHAR *oid)
+{
+	return (_tcsncmp(oid, _T(".1.3.6.1.4.1.25506.11.1."), 24) == 0) ? 255 : 0;
+}
+
+/**
+ * Check if given device is supported by driver
+ *
+ * @param snmp SNMP transport
+ * @param oid Device OID
+ */
+bool HPSwitchDriver::isDeviceSupported(SNMP_Transport *snmp, const TCHAR *oid)
+{
+	return true;
+}
+
+/**
+ * Do additional checks on the device required by driver.
+ * Driver can set device's custom attributes from within
+ * this function.
+ *
+ * @param snmp SNMP transport
+ * @param attributes Node's custom attributes
+ */
+void HPSwitchDriver::analyzeDevice(SNMP_Transport *snmp, const TCHAR *oid, StringMap *attributes, DriverData **driverData)
+{
+}
+
+/**
+ * Handler for port walk
+ */
+static UINT32 PortWalkHandler(UINT32 snmpVersion, SNMP_Variable *var, SNMP_Transport *snmp, void *arg)
+{
+   InterfaceList *ifList = (InterfaceList *)arg;
+   UINT32 ifIndex = var->getValueAsUInt();
+   for(int i = 0; i < ifList->size(); i++)
+   {
+      NX_INTERFACE_INFO *iface = ifList->get(i);
+      if (iface->dwIndex == ifIndex)
+      {
+         iface->isPhysicalPort = true;
+         iface->dwSlotNumber = var->getName()->getValue()[15];
+         iface->dwPortNumber = var->getName()->getValue()[17];
+         break;
+      }
+   }
+   return SNMP_ERR_SUCCESS;
+}
+
+/**
+ * Get list of interfaces for given node
+ *
+ * @param snmp SNMP transport
+ * @param attributes Node's custom attributes
+ */
+InterfaceList *HPSwitchDriver::getInterfaces(SNMP_Transport *snmp, StringMap *attributes, DriverData *driverData, int useAliases, bool useIfXTable)
+{
+	// Get interface list from standard MIB
+	InterfaceList *ifList = NetworkDeviceDriver::getInterfaces(snmp, attributes, driverData, useAliases, useIfXTable);
+	if (ifList == NULL)
+		return NULL;
+
+	// Find physical ports (walk hh3cLswPortIfindex)
+   SnmpWalk(snmp->getSnmpVersion(), snmp, _T(".1.3.6.1.4.1.25506.8.35.18.4.5.1.3"), PortWalkHandler, ifList, FALSE);
+	return ifList;
+}
+
+/**
+ * Driver entry point
+ */
+DECLARE_NDD_ENTRY_POINT(s_driverName, HPSwitchDriver);
+
+#ifdef _WIN32
+
+/**
+ * DLL entry point
+ */
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH)
+		DisableThreadLibraryCalls(hInstance);
+	return TRUE;
+}
+
+#endif
