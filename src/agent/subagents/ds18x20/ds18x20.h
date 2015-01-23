e58b0f7506270b96ae38bc9047595a7da9abee5d
Victor Kirhenshtein <victor@netxms.org>
2015-01-23 17:34:27 +0200

    Subagent for reading DS18T20 and DS18S20 temperature sensors on Linux

---
+++ src/agent/subagents/ds18x20/ds18x20.h
@@ -0,0 +1,30 @@
+/*
+** NetXMS DS18x20 sensor subagent
+** Copyright (C) 2004-2015 Victor Kirhenshtein
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: ds18x20.h
+**
+**/
+
+#ifndef _ds18x20_h_
+#define _ds18x20_h_
+
+#include <nms_common.h>
+#include <nms_util.h>
+#include <nms_agent.h>
+
+#endif
