f1ea977338eeba9091fa233118f76b61b864df19
Victor Kirhenshtein <victor@netxms.org>
2015-01-24 12:17:06 +0200

    build number changed

--- include/build.h
+++ include/build.h
@@ -1,5 +1,5 @@
 #ifndef __build_h
 #define __build_h
-#define NETXMS_VERSION_BUILD 8008
-#define NETXMS_VERSION_BUILD_STRING _T("8008")
+#define NETXMS_VERSION_BUILD 8009
+#define NETXMS_VERSION_BUILD_STRING _T("8009")
 #endif
--- src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
+++ src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
@@ -1,5 +1,5 @@
 package org.netxms.base;
 public final class BuildNumber {
-   public static final String TEXT = "8008";
-   public static final int NUMBER = 8008;
+   public static final String TEXT = "8009";
+   public static final int NUMBER = 8009;
 }
