13fa4a14494d4df34a02cf768588ded606a19871
Victor Kirhenshtein <victor@netxms.org>
2015-06-30 12:50:13 +0300

    build number changed

--- src/java/build/set_build_number.cmd
+++ src/java/build/set_build_number.cmd
@@ -1,1 +1,1 @@
-set build_number=8028
+set build_number=8029
--- src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
+++ src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
@@ -1,5 +1,5 @@
 package org.netxms.base;
 public final class BuildNumber {
-   public static final String TEXT = "8028";
-   public static final int NUMBER = 8028;
+   public static final String TEXT = "8029";
+   public static final int NUMBER = 8029;
 }
