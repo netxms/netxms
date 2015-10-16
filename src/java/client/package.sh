213f4780e45e705007324fa0114620c6438bfe4f
Victor Kirhenshtein <victor@netxms.org>
2015-10-16 16:12:21 +0300

    release preparation

--- src/java/client/package.sh
+++ src/java/client/package.sh
@@ -4,8 +4,9 @@ export MAVEN_OPTS='-Xmx512m'
 
 set -e
 
-cd netxms-base && mvn clean install && cd ..
-cd netxms-client && mvn clean install && cd ..
+cd netxms-base && mvn clean install javadoc:jar && cd ..
+cd netxms-client && mvn clean install javadoc:jar && cd ..
+cd mobile-agent && mvn -Dmaven.test.skip=true clean install javadoc:jar && cd ..
 
 version=`grep '<version>' netxms-base/pom.xml | cut -f 2 -d \> | cut -f 1 -d \<|head -n1`
 
@@ -15,3 +16,6 @@ rm -f \
 
 cp netxms-base/target/netxms-base-$version.jar ../../../android/src/console/libs/
 cp netxms-client/target/netxms-client-$version.jar ../../../android/src/console/libs/
+
+cp netxms-base/target/netxms-base-$version.jar ../../../android/src/agent/libs/
+cp mobile-agent/target/netxms-mobile-agent-$version.jar ../../../android/src/agent/libs/
