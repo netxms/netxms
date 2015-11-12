7b6175c502d5d992b79127a80ab8d57d37e1d8e2
Victor Kirhenshtein <victor@netxms.org>
2015-11-12 18:03:01 +0200

    build number updated

--- src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
+++ src/java/client/netxms-base/src/main/java/org/netxms/base/BuildNumber.java
@@ -1,5 +1,5 @@
 package org.netxms.base;
 public final class BuildNumber {
-   public static final String TEXT = "8048";
-   public static final int NUMBER = 8048;
+   public static final String TEXT = "8049";
+   public static final int NUMBER = 8049;
 }
