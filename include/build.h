ea3f2316d06f993d374a35d67fc4243f388edffd
Victor Kirhenshtein <victor@netxms.org>
2015-12-09 13:07:48 +0200

    build number updated

--- include/build.h
+++ include/build.h
@@ -1,5 +1,5 @@
 #ifndef __build_h
 #define __build_h
-#define NETXMS_VERSION_BUILD 8050
-#define NETXMS_VERSION_BUILD_STRING _T("8050")
+#define NETXMS_VERSION_BUILD 8051
+#define NETXMS_VERSION_BUILD_STRING _T("8051")
 #endif
