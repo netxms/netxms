140f32901fd717a160e31596765861a0f792130a
Alex Kirhenshtein <alk@netxms.org>
2015-12-15 00:43:06 +0200

    Fixed for Solaris sh

--- tools/updatetag.sh
+++ tools/updatetag.sh
@@ -6,7 +6,8 @@ HEADER="netxms-build-tag.h"
 
 BUILD_TAG=`git describe 2>/dev/null`
 [ -z $BUILD_TAG ] && BUILD_TAG=UNKNOWN
-if ! grep -q "BUILDTAG:$BUILD_TAG" $HEADER; then
+grep "BUILDTAG:$BUILD_TAG" $HEADER >/dev/null 2>&1
+if [ $? != 0 ]; then
   echo "/* BUILDTAG:$BUILD_TAG */" > $HEADER
   echo "#ifndef _build_tag_h_" >> $HEADER
   echo "#define _build_tag_h_" >> $HEADER
