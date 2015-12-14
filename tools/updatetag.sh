60bd8433356ea2cdc6bfc540db04d7862bbfbf8d
Victor Kirhenshtein <victor@netxms.org>
2015-12-14 16:35:25 +0200

    Fixed Solaris build

--- tools/updatetag.sh
+++ tools/updatetag.sh
@@ -3,10 +3,10 @@
 HEADER="netxms-build-tag.h"
 
 git describe >/dev/null 2>/dev/null
-if [ $? -eq 0 ]; then
+if test $? -eq 0; then
 	echo "Checking build tag"
 	BUILD_TAG=`git describe`
-	if [ -e $HEADER ]; then
+	if test -f $HEADER; then
 		found=`cat $HEADER | grep "BUILDTAG:$BUILD_TAG" | wc -l`
 		if [ $found -gt 0 ]; then
 			update="no"
@@ -16,7 +16,7 @@ if [ $? -eq 0 ]; then
 	else
 		update="yes"
 	fi
-	if [ "$update" = "yes" ]; then
+	if test "x$update" = "xyes"; then
 		echo "/* BUILDTAG:$BUILD_TAG */" > $HEADER
 		echo "#ifndef _build_tag_h_" >> $HEADER
 		echo "#define _build_tag_h_" >> $HEADER
