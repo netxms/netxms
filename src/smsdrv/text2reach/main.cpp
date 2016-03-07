bc96adf5f58d3a8e64d334bcd9a606e5eb292a1f
Victor Kirhenshtein <victor@netxms.org>
2016-03-07 08:51:00 +0200

    text2reach SMS driver improved

--- src/smsdrv/text2reach/main.cpp
+++ src/smsdrv/text2reach/main.cpp
@@ -3,6 +3,8 @@
 ** SMS driver for Text2Reach.com service
 ** Copyright (C) 2014-2016 Raden Solutions
 **
+** Text2Reach API documentation - http://www.text2reach.com/files/text2reach_bulkapi_v1.14.pdf
+**
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
 ** the Free Software Foundation; either version 3 of the License, or
@@ -16,9 +18,6 @@
 ** You should have received a copy of the GNU Lesser General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-**
-** File: main.cpp
-**
 **/
 
 #include <nms_common.h>
@@ -52,6 +51,8 @@ struct RequestData
  */
 static char s_apikey[128] = "apikey";
 static char s_from[128] = "from";
+static char s_unicode = true; // if set to false, characters not in GSM 7bit alphabet will be converted (ā => a) or removed
+static char s_blacklist = false; // Checks if the number is in blacklist before sending. Returns error -503 if found.
 
 /**
  * Init driver
@@ -91,6 +92,9 @@ extern "C" bool EXPORT SMSDriverInit(const TCHAR *initArgs, Config *config)
    ExtractNamedOptionValue(initArgs, _T("apikey"), s_apikey, 128);
    ExtractNamedOptionValue(initArgs, _T("from"), s_from, 128);
 #endif
+
+   s_unicode = ExtractNamedOptionValueAsBool(initArgs, _T("unicode"), true);
+   s_blacklist = ExtractNamedOptionValueAsBool(initArgs, _T("blacklist"), false);
 
 	return true;
 }
@@ -142,19 +146,22 @@ extern "C" bool EXPORT SMSDriverSend(const TCHAR *phoneNumber, const TCHAR *text
 
 #ifdef UNICODE
       char *mbphone = MBStringFromWideString(phoneNumber);
-      char *mbmsg = MBStringFromWideString(text);
+      char *mbmsg = UTF8StringFromWideString(text);
       char *phone = curl_easy_escape(curl, mbphone, 0);
       char *msg = curl_easy_escape(curl, mbmsg, 0);
       free(mbphone);
       free(mbmsg);
 #else
       char *phone = curl_easy_escape(curl, phoneNumber, 0);
-      char *msg = curl_easy_escape(curl, text, 0);
+      char *mbmsg = UTF8StringFromMBString(text);
+      char *msg = curl_easy_escape(curl, mbmsg, 0);
+      free(mbmsg);
 #endif
 
       char url[4096];
-      snprintf(url, 4096, "http://www.text2reach.com/api/1.1/sms/bulk/?api_key=%s&phone=%s&from=%s&message=%s", s_apikey, phone, s_from, msg);
-      nxlog_debug(4, _T("Text2Reach: URL set to \"%hs\""), url);
+      snprintf(url, 4096, "http://www.text2reach.com/api/1.1/sms/bulk/?api_key=%s&phone=%s&from=%s&message=%s&unicode=%s&blacklist=%s",
+               s_apikey, phone, s_from, msg, (s_unicode ? "true" : "false"), (s_blacklist ? "true" : "false"));
+      nxlog_debug(7, _T("Text2Reach: URL set to \"%hs\""), url);
 
       curl_free(phone);
       curl_free(msg);
@@ -165,19 +172,35 @@ extern "C" bool EXPORT SMSDriverSend(const TCHAR *phoneNumber, const TCHAR *text
          {
             long response = 500;
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response);
-			if (response == 200)
-			{
-				nxlog_debug(4, _T("Text2Reach: SMS successfully sent"));
-				success = true;
-			}
-			else
-			{
-				nxlog_debug(4, _T("Text2Reach: request cannot be fulfilled, HTTP response code %03d"), response);
-			}
+            if (response == 200)
+            {
+               if (data->allocated > 0)
+               {
+                  data->data[data->size] = 0;
+                  long messageId = strtol(data->data, NULL, 0);
+                  if (messageId > 0)
+                  {
+                     nxlog_debug(4, _T("Text2Reach: SMS successfully sent"));
+                     success = true;
+                  }
+                  else if (messageId == 0)
+                  {
+                     nxlog_debug(4, _T("Text2Reach: malformed response %hs"), data->data);
+                  }
+                  else
+                  {
+                     nxlog_debug(4, _T("Text2Reach: sending error %d"), (int)messageId);
+                  }
+               }
+            }
+            else
+            {
+               nxlog_debug(4, _T("Text2Reach: request cannot be fulfilled, HTTP response code %03d"), response);
+            }
          }
          else
          {
-			nxlog_debug(4, _T("Text2Reach: call to curl_easy_perform() failed"));
+            nxlog_debug(4, _T("Text2Reach: call to curl_easy_perform() failed"));
          }
       }
       else
