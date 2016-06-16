b5ce2e1d887885990d8404fd03981406e5688843
Victor Kirhenshtein <victor@netxms.org>
2016-06-16 17:43:57 +0300

    Fixed possible deadlock in NXCSession.executeScript

--- src/java/client/netxms-client/src/main/java/org/netxms/client/NXCSession.java
+++ src/java/client/netxms-client/src/main/java/org/netxms/client/NXCSession.java
@@ -5379,12 +5379,15 @@ public class NXCSession
       waitForRCC(msg.getMessageId());
       synchronized(handler)
       {
-         try
+         if (!handler.isComplete())
          {
-            handler.wait();
-         }
-         catch(InterruptedException e)
-         {
+            try
+            {
+               handler.wait();
+            }
+            catch(InterruptedException e)
+            {
+            }
          }
       }
       if (handler.isTimeout())
