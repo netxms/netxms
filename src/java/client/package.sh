3ba68bbdcd5b36a4617fac9f571b236ce6d22dd2
Alex Kirhenshtein <alk@netxms.org>
2016-10-08 17:38:54 +0300

    Maven parallel build enabled (1 thread per CPU)

--- src/java/client/package.sh
+++ src/java/client/package.sh
@@ -6,9 +6,9 @@ unset CDPATH cdpath
 
 export MAVEN_OPTS='-Xmx512m'
 
-cd netxms-base && mvn clean install javadoc:jar && cd ..
-cd netxms-client && mvn clean install javadoc:jar && cd ..
-cd mobile-agent && mvn -Dmaven.test.skip=true clean install javadoc:jar && cd ..
+cd netxms-base && mvn -T 1C clean install javadoc:jar && cd ..
+cd netxms-client && mvn -T 1C clean install javadoc:jar && cd ..
+cd mobile-agent && mvn -T 1C -Dmaven.test.skip=true clean install javadoc:jar && cd ..
 
 version=`grep '<version>' netxms-base/pom.xml | cut -f 2 -d \> | cut -f 1 -d \<|head -n1`
 
