a67fb98ca4365c4107a39618179f2ee0855067f6
Eriks Jenkevics <eriks@netxms.org>
2016-11-08 18:51:31 +0200

    Added ANSI color sequence support in webui Fixes #1332

---
+++ webui/webapp/Core/src/org/netxms/ui/eclipse/widgets/StyledText.java
@@ -0,0 +1,269 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2016 RadenSolutions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.ui.eclipse.widgets;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.netxms.ui.eclipse.tools.RefreshTimer;
+import org.netxms.ui.eclipse.widgets.helpers.LineStyleEvent;
+import org.netxms.ui.eclipse.widgets.helpers.LineStyleListener;
+import org.netxms.ui.eclipse.widgets.helpers.StyleRange;
+
+/**
+ * Widget for displaying html formated rich text
+ */
+public class StyledText extends Composite
+{
+   private final Browser textArea;
+   private final RefreshTimer refreshTimer;
+   private String textBuffer = "";
+   private String largeTextBuffer = "";
+   private boolean resetBuffer = true;
+   private boolean setText = false;
+   private Set<LineStyleListener> lineStyleListeners = new HashSet<LineStyleListener>(0); 
+
+   /**
+    * @param parent
+    * @param style
+    */
+   public StyledText(Composite parent, int style)
+   {
+      super(parent, style);
+      
+      setLayout(new FillLayout());
+      
+      textArea = new Browser(this, SWT.NONE);
+      textArea.setText("<pre id=\"textArea\"></pre>");
+
+      refreshTimer = new RefreshTimer(200, this, new Runnable()
+      {
+        @Override
+        public void run()
+        {
+          refresh();
+        }
+      });
+   }
+   
+   /**
+    * Set widget text
+    * @param text
+    */
+   public void setText(String string)
+   {
+      if (!fireLineStyleListener(string))
+      {
+         setText = true;
+         textBuffer = "";
+         largeTextBuffer = "";
+         append(string);
+      }
+   }
+
+   /**
+    * Append string to widget
+    * @param string
+    */
+   public void append(String string)
+   {
+      if (!fireLineStyleListener(string))
+      {
+         textBuffer += string;
+         largeTextBuffer += string;
+         refreshTimer.execute();
+      }
+   }
+
+   /** Set style for specific range of text
+    * @param range
+    */
+   public void setStyleRange(StyleRange range)
+   {
+      int red = range.foreground.getRed();
+      int green = range.foreground.getGreen();
+      int blue = range.foreground.getBlue();
+
+      StringBuilder sb = new StringBuilder();
+      sb.append(largeTextBuffer.substring(0, (range.start)));
+      sb.append("<span style=\"color:rgb(" + red + "," + green + "," + blue +
+                ")\">" + largeTextBuffer.substring(range.start, (range.start + range.length)) + "</span>");
+      
+      largeTextBuffer = sb.toString();
+      textBuffer = largeTextBuffer;
+      
+      setText = true;
+      refreshTimer.execute();
+   }
+   
+   /** Set style for specific range of text from event
+    * @param event
+    */
+   public void setStyleFromEvent(LineStyleEvent e, String text)
+   {
+      if (e.styles == null)
+         return;
+      
+      StringBuilder sb = new StringBuilder();
+      int startOffset = 0;
+      for(StyleRange range : e.styles)
+      {
+         int red = range.foreground.getRed();
+         int green = range.foreground.getGreen();
+         int blue = range.foreground.getBlue();
+   
+         sb.append(largeTextBuffer.substring(0, (range.start + startOffset)));
+         sb.append("<span style=\"color:rgb(" + String.format("%03d", red) + "," + String.format("%03d", green) + "," + String.format("%03d", blue) +
+                   ")\">" + largeTextBuffer.substring((range.start + startOffset), ((range.start + startOffset) + range.length)) + "</span>");
+         
+         startOffset += 47;
+      }
+      largeTextBuffer = sb.toString();
+      textBuffer = largeTextBuffer;
+      
+      setText = true;
+      refreshTimer.execute();
+   }
+
+   /**
+    * @return Char count of text
+    */
+   public int getCharCount()
+   {
+      return largeTextBuffer.length();
+   }
+
+   /**
+   * @return
+   */
+  public Control getControl()
+   {
+     return textArea;
+   }
+
+  /**
+   * @return
+   */
+  public Composite getComposite()
+   {
+     return textArea;
+   }
+
+   /**
+    * @param background
+    */
+   public void setBackground(Color background)
+   {
+      textArea.setBackground(background);
+   }
+
+   /**
+    * Set focus on widget
+    */
+   @Override
+   public boolean setFocus()
+   {
+      return textArea.setFocus();
+   }
+   
+   /**
+    * Escapes chars for JavaScript query
+    * @param string
+    * @return
+    */
+   private String escapeChars(String string)
+   {
+      String result = string;
+      result = result.replaceAll("\n", "<br>");
+      result = result.replaceAll("\r", "");
+      result = result.replaceAll("'", "\\\\'");
+      
+      return result;
+   }
+
+   /**
+    * refresh widget
+    */
+   public void refresh()
+   {
+      escapeChars(textBuffer);
+      try
+      {
+         if (setText)
+         {
+            resetBuffer = textArea.execute("document.getElementById(\"textArea\").innerHTML = '" + escapeChars(textBuffer) + "';" + "window.scrollTo(0, document.body.scrollHeight);");
+            setText = false;
+         }
+         else
+            resetBuffer = textArea.execute("document.getElementById(\"textArea\").innerHTML += '" + escapeChars(textBuffer) + "';" + "window.scrollTo(0, document.body.scrollHeight);");
+      }
+      catch (Exception e)
+      {
+         resetBuffer = false;
+      }
+      
+      if (resetBuffer)
+      {
+         textBuffer = "";
+         resetBuffer = true;
+      }
+      else
+         refreshTimer.execute();
+   }
+   
+   /**
+    * @param listener
+    */
+   public void addLineStyleListener(LineStyleListener listener)
+   {
+      lineStyleListeners.add(listener);
+   }
+   
+   /**
+    * @param listener
+    */
+   public void removeLineStyleListener(LineStyleListener listener)
+   {
+      lineStyleListeners.remove(listener);
+   }
+   
+   /**
+    * Call all registered line style listeners
+    */
+   protected boolean fireLineStyleListener(String string)
+   {
+      if (lineStyleListeners.isEmpty())
+         return false;
+      
+      LineStyleEvent e = new LineStyleEvent();
+      e.lineText = string;
+      
+      for(LineStyleListener l : lineStyleListeners)
+         l.lineGetStyle(e);
+      
+      setStyleFromEvent(e, string);
+      
+      return true;
+   }
+}
--- webui/webapp/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/views/NodePollerView.java
+++ webui/webapp/ObjectManager/src/org/netxms/ui/eclipse/objectmanager/views/NodePollerView.java
@@ -24,16 +24,12 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.rap.rwt.RWT;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
@@ -47,219 +43,224 @@ import org.netxms.ui.eclipse.console.resources.RegionalSettings;
 import org.netxms.ui.eclipse.console.resources.SharedIcons;
 import org.netxms.ui.eclipse.objectmanager.Messages;
 import org.netxms.ui.eclipse.shared.ConsoleSharedData;
+import org.netxms.ui.eclipse.widgets.StyledText;
+import org.netxms.ui.eclipse.widgets.helpers.StyleRange;
 
 /**
  * Forced node poll view
  */
 public class NodePollerView extends ViewPart
 {
-	public static final String ID = "org.netxms.ui.eclipse.objectmanager.views.NodePollerView"; //$NON-NLS-1$
-	
-	private final String[] POLL_NAME = 
-	   { 
-	      "", //$NON-NLS-1$ 
-	      Messages.get().NodePollerView_StatusPoll, 
-	      Messages.get().NodePollerView_FullConfigPoll, 
-	      Messages.get().NodePollerView_InterfacePoll, 
-	      Messages.get().NodePollerView_TopologyPoll,
+   public static final String ID = "org.netxms.ui.eclipse.objectmanager.views.NodePollerView"; //$NON-NLS-1$
+   
+   private final String[] POLL_NAME = 
+      { 
+         "", //$NON-NLS-1$ 
+         Messages.get().NodePollerView_StatusPoll, 
+         Messages.get().NodePollerView_FullConfigPoll, 
+         Messages.get().NodePollerView_InterfacePoll, 
+         Messages.get().NodePollerView_TopologyPoll,
          Messages.get().NodePollerView_ConfigPoll,
          Messages.get().NodePollerView_InstanceDiscovery
-	   };
-	/*
-	private static final Color COLOR_ERROR = new Color(Display.getCurrent(), 192, 0, 0);
-	private static final Color COLOR_WARNING = new Color(Display.getCurrent(), 255, 128, 0);
-	private static final Color COLOR_INFO = new Color(Display.getCurrent(), 0, 128, 0);
-	private static final Color COLOR_LOCAL = new Color(Display.getCurrent(), 0, 0, 192);
-	*/
-	
-	private NXCSession session;
-	private AbstractNode node;
-	private NodePollType pollType;
-	private Display display;
-	private Text textArea;
-	private boolean pollActive = false;
-	private Action actionRestart;
-	private Action actionClearOutput;
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.ViewPart#init(org.eclipse.ui.IViewSite)
-	 */
-	@Override
-	public void init(IViewSite site) throws PartInitException
-	{
-		super.init(site);
-		
-		session = (NXCSession)ConsoleSharedData.getSession();
-		display = Display.getCurrent();
-		
-		// Secondary ID must by in form nodeId&pollType
-		String[] parts = site.getSecondaryId().split("&"); //$NON-NLS-1$
-		if (parts.length != 2)
-			throw new PartInitException("Internal error"); //$NON-NLS-1$
-		
-		AbstractObject obj = session.findObjectById(Long.parseLong(parts[0]));
-		node = ((obj != null) && (obj instanceof AbstractNode)) ? (AbstractNode)obj : null;
-		if (node == null)
-			throw new PartInitException(Messages.get().NodePollerView_InvalidObjectID);
-		pollType = NodePollType.valueOf(parts[1]);
-		
-		setPartName(POLL_NAME[pollType.getValue()] + " - " + node.getObjectName()); //$NON-NLS-1$
-	}
+      };
+   
+   private static final Color COLOR_ERROR = new Color(Display.getCurrent(), 192, 0, 0);
+   private static final Color COLOR_WARNING = new Color(Display.getCurrent(), 255, 128, 0);
+   private static final Color COLOR_INFO = new Color(Display.getCurrent(), 0, 128, 0);
+   private static final Color COLOR_LOCAL = new Color(Display.getCurrent(), 0, 0, 192);
+   
+   private NXCSession session;
+   private AbstractNode node;
+   private NodePollType pollType;
+   private Display display;
+   private StyledText textArea;
+   private boolean pollActive = false;
+   private Action actionRestart;
+   private Action actionClearOutput;
+   
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.part.ViewPart#init(org.eclipse.ui.IViewSite)
+    */
+   @Override
+   public void init(IViewSite site) throws PartInitException
+   {
+      super.init(site);
+      
+      session = (NXCSession)ConsoleSharedData.getSession();
+      display = Display.getCurrent();
+      
+      // Secondary ID must by in form nodeId&pollType
+      String[] parts = site.getSecondaryId().split("&"); //$NON-NLS-1$
+      if (parts.length != 2)
+         throw new PartInitException("Internal error"); //$NON-NLS-1$
+      
+      AbstractObject obj = session.findObjectById(Long.parseLong(parts[0]));
+      node = ((obj != null) && (obj instanceof AbstractNode)) ? (AbstractNode)obj : null;
+      if (node == null)
+         throw new PartInitException(Messages.get().NodePollerView_InvalidObjectID);
+      pollType = NodePollType.valueOf(parts[1]);
+      
+      setPartName(POLL_NAME[pollType.getValue()] + " - " + node.getObjectName()); //$NON-NLS-1$
+   }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
-	 */
-	@Override
-	public void createPartControl(Composite parent)
-	{
-		textArea = new Text(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		textArea.setEditable(false);
-		textArea.setData(RWT.CUSTOM_VARIANT, "monospace");
-		
-		createActions();
-		contributeToActionBars();
-		createPopupMenu();
-	}
-	
-	/**
-	 * Create actions
-	 */
-	private void createActions()
-	{
-		actionRestart = new Action(Messages.get().NodePollerView_ActionRestart, SharedIcons.RESTART) {
-			@Override
-			public void run()
-			{
-				startPoll();
-			}
-		};
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+    */
+   @Override
+   public void createPartControl(Composite parent)
+   {
+      textArea = new StyledText(parent, SWT.MULTI | SWT.V_SCROLL);
+      textArea.setBackground(new Color(Display.getCurrent(), 255, 255, 255));
+      
+      createActions();
+      contributeToActionBars();
+   }
+   
+   /**
+    * Create actions
+    */
+   private void createActions()
+   {
+      actionRestart = new Action(Messages.get().NodePollerView_ActionRestart, SharedIcons.RESTART) {
+         @Override
+         public void run()
+         {
+            startPoll();
+         }
+      };
 
-		actionClearOutput = new Action(Messages.get().NodePollerView_ActionClear, SharedIcons.CLEAR_LOG) {
-			@Override
-			public void run()
-			{
-				textArea.setText(""); //$NON-NLS-1$
-			}
-		};
-	}
+      actionClearOutput = new Action(Messages.get().NodePollerView_ActionClear, SharedIcons.CLEAR_LOG) {
+         @Override
+         public void run()
+         {
+            textArea.setText(""); //$NON-NLS-1$
+         }
+      };
+   }
 
-	/**
-	 * Contribute actions to action bar
-	 */
-	private void contributeToActionBars()
-	{
-		IActionBars bars = getViewSite().getActionBars();
-		fillLocalPullDown(bars.getMenuManager());
-		fillLocalToolBar(bars.getToolBarManager());
-	}
+   /**
+    * Contribute actions to action bar
+    */
+   private void contributeToActionBars()
+   {
+      IActionBars bars = getViewSite().getActionBars();
+      fillLocalPullDown(bars.getMenuManager());
+      fillLocalToolBar(bars.getToolBarManager());
+   }
 
-	/**
-	 * Fill local pull-down menu
-	 * 
-	 * @param manager
-	 *           Menu manager for pull-down menu
-	 */
-	private void fillLocalPullDown(IMenuManager manager)
-	{
-		manager.add(actionRestart);
-		manager.add(actionClearOutput);
-	}
+   /**
+    * Fill local pull-down menu
+    * 
+    * @param manager
+    *           Menu manager for pull-down menu
+    */
+   private void fillLocalPullDown(IMenuManager manager)
+   {
+      manager.add(actionRestart);
+      manager.add(actionClearOutput);
+   }
 
-	/**
-	 * Fill local tool bar
-	 * 
-	 * @param manager
-	 *           Menu manager for local toolbar
-	 */
-	private void fillLocalToolBar(IToolBarManager manager)
-	{
-		manager.add(actionRestart);
-		manager.add(actionClearOutput);
-	}
+   /**
+    * Fill local tool bar
+    * 
+    * @param manager
+    *           Menu manager for local toolbar
+    */
+   private void fillLocalToolBar(IToolBarManager manager)
+   {
+      manager.add(actionRestart);
+      manager.add(actionClearOutput);
+   }
 
-	/**
-	 * Create pop-up menu
-	 */
-	private void createPopupMenu()
-	{
-		// Create menu manager
-		MenuManager menuMgr = new MenuManager();
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener()
-		{
-			public void menuAboutToShow(IMenuManager mgr)
-			{
-				fillContextMenu(mgr);
-			}
-		});
+   /**
+    * Fill context menu
+    * 
+    * @param mgr Menu manager
+    */
+   protected void fillContextMenu(final IMenuManager manager)
+   {
+      manager.add(actionClearOutput);
+   }
 
-		// Create menu
-		Menu menu = menuMgr.createContextMenu(textArea);
-		textArea.setMenu(menu);
-	}
+   /**
+    * Add poller message to text area
+    * 
+    * @param message poller message
+    */
+   private void addPollerMessage(String message)
+   {
+      Date now = new Date();
+      textArea.append("[" + RegionalSettings.getDateTimeFormat().format(now) + "] "); //$NON-NLS-1$ //$NON-NLS-2$
+      
+      int index = message.indexOf(0x7F);
+      if (index != -1)
+      {
+         textArea.append(message.substring(0, index));
+         char code = message.charAt(index + 1);
+         int lastPos = textArea.getCharCount();
+         final String msgPart = message.substring(index + 2);
+         textArea.append(msgPart);
+         
+         StyleRange style = new StyleRange();
+         style.start = lastPos;
+         style.length = msgPart.length();
+         style.foreground = getTextColor(code);
+         textArea.setStyleRange(style);
+      }
+      else
+      {
+         textArea.append(message);
+      }
+   }
+   
+   /**
+    * Get color from color code
+    * 
+    * @param code
+    * @return
+    */
+   private Color getTextColor(char code)
+   {
+      switch(code)
+      {
+         case 'e':
+            return COLOR_ERROR;
+         case 'w':
+            return COLOR_WARNING;
+         case 'i':
+            return COLOR_INFO;
+         case 'l':
+            return COLOR_LOCAL;
+      }
+      return null;
+   }
 
-	/**
-	 * Fill context menu
-	 * 
-	 * @param mgr Menu manager
-	 */
-	protected void fillContextMenu(final IMenuManager manager)
-	{
-		manager.add(actionClearOutput);
-	}
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
+    */
+   @Override
+   public void setFocus()
+   {
+      textArea.setFocus();
+   }
 
-	/**
-	 * Add poller message to text area
-	 * 
-	 * @param message poller message
-	 */
-	private void addPollerMessage(String message)
-	{
-		Date now = new Date();
-		textArea.append("[" + RegionalSettings.getDateTimeFormat().format(now) + "] "); //$NON-NLS-1$ //$NON-NLS-2$
-		
-		int index = message.indexOf(0x7F);
-		if (index != -1)
-		{
-			textArea.append(message.substring(0, index));
-			final String msgPart = message.substring(index + 2);
-			textArea.append(msgPart);
-		}
-		else
-		{
-			textArea.append(message);
-		}
-		
-		//textArea.setCaretOffset(textArea.getCharCount());
-		//textArea.setTopIndex(textArea.getLineCount() - 1);
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
-	 */
-	@Override
-	public void setFocus()
-	{
-		textArea.setFocus();
-	}
-
-	/**
-	 * Start poll
-	 */
-	public void startPoll()
-	{
-		if (pollActive)
-			return;
-		pollActive = true;
-		actionRestart.setEnabled(false);
-		
-		addPollerMessage("\u007Fl**** Poll request sent to server ****\r\n"); //$NON-NLS-1$
-		
-		final TextOutputListener listener = new TextOutputListener() {
-			@Override
-			public void messageReceived(final String message)
-			{
-			   display.asyncExec(new Runnable() {
+   /**
+    * Start poll
+    */
+   public void startPoll()
+   {
+      if (pollActive)
+         return;
+      pollActive = true;
+      actionRestart.setEnabled(false);
+      
+      addPollerMessage("\u007Fl**** Poll request sent to server ****\r\n"); //$NON-NLS-1$
+      
+      final TextOutputListener listener = new TextOutputListener() {
+         @Override
+         public void messageReceived(final String message)
+         {
+            display.asyncExec(new Runnable() {
                @Override
                public void run()
                {
@@ -267,38 +268,38 @@ public class NodePollerView extends ViewPart
                      addPollerMessage(message);
                }
             });
-			}
-		};
-		
-		Job job = new Job(String.format(Messages.get().NodePollerView_JobName, node.getObjectName(), node.getObjectId())) {
-			@Override
-			protected IStatus run(IProgressMonitor monitor)
-			{
-				try
-				{
-					session.pollNode(node.getObjectId(), pollType, listener);
-					onPollComplete(true, null);
-				}
-				catch(Exception e)
-				{
-					onPollComplete(false, e.getMessage());
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.setSystem(true);
-		job.schedule();
-	}
-	
-	/**
-	 * Poll completion handler
-	 * 
-	 * @param success
-	 * @param errorMessage
-	 */
-	private void onPollComplete(final boolean success, final String errorMessage)
-	{
-	   display.asyncExec(new Runnable() {
+         }
+      };
+      
+      Job job = new Job(String.format(Messages.get().NodePollerView_JobName, node.getObjectName(), node.getObjectId())) {
+         @Override
+         protected IStatus run(IProgressMonitor monitor)
+         {
+            try
+            {
+               session.pollNode(node.getObjectId(), pollType, listener);
+               onPollComplete(true, null);
+            }
+            catch(Exception e)
+            {
+               onPollComplete(false, e.getMessage());
+            }
+            return Status.OK_STATUS;
+         }
+      };
+      job.setSystem(true);
+      job.schedule();
+   }
+   
+   /**
+    * Poll completion handler
+    * 
+    * @param success
+    * @param errorMessage
+    */
+   private void onPollComplete(final boolean success, final String errorMessage)
+   {
+      display.asyncExec(new Runnable() {
          @Override
          public void run()
          {
@@ -318,5 +319,5 @@ public class NodePollerView extends ViewPart
             actionRestart.setEnabled(true);
          }
       });
-	}
+   }
 }
