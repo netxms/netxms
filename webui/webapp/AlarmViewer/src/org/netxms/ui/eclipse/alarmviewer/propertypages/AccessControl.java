50ef1c16547b86dfbc5b42f6edbcda2f5d99dbb9
Eriks Jenkevics <eriks@netxms.org>
2017-01-30 15:15:33 +0200

    Added support for groups in category acl

--- webui/webapp/AlarmViewer/src/org/netxms/ui/eclipse/alarmviewer/propertypages/AccessControl.java
+++ webui/webapp/AlarmViewer/src/org/netxms/ui/eclipse/alarmviewer/propertypages/AccessControl.java
@@ -102,7 +102,7 @@ public class AccessControl extends PropertyPage
          @Override
          public void widgetSelected(SelectionEvent e)
          {
-            SelectUserDialog dlg = new SelectUserDialog(AccessControl.this.getShell(), User.class);
+            SelectUserDialog dlg = new SelectUserDialog(AccessControl.this.getShell(), AbstractUserObject.class);
             if (dlg.open() == Window.OK)
             {
                AbstractUserObject[] selection = dlg.getSelection();
