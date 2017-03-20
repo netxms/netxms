e458a7e1f5aa36b23d619846a1f6ab909516e591
Victor Kirhenshtein <victor@netxms.org>
2017-03-20 23:11:55 +0200

    fixed NPE in console

--- src/java/netxms-eclipse/ObjectView/src/org/netxms/ui/eclipse/objectview/objecttabs/elements/ExternalResources.java
+++ src/java/netxms-eclipse/ObjectView/src/org/netxms/ui/eclipse/objectview/objecttabs/elements/ExternalResources.java
@@ -1,5 +1,20 @@
 /**
- * 
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2017 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 package org.netxms.ui.eclipse.objectview.objecttabs.elements;
 
@@ -24,8 +39,7 @@ import org.netxms.ui.eclipse.objectview.objecttabs.ObjectTab;
 import org.netxms.ui.eclipse.tools.MessageDialogHelper;
 
 /**
- * @author victor
- *
+ * "External Resources" element
  */
 public class ExternalResources extends OverviewPageElement
 {
@@ -57,7 +71,7 @@ public class ExternalResources extends OverviewPageElement
    @Override
    public boolean isApplicableForObject(AbstractObject object)
    {
-      return object.hasUrls();
+      return (object != null) && object.hasUrls();
    }
 
    /* (non-Javadoc)
