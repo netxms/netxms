0e8667f4f8c797dd0011a30db92b16bec6c08968
Victor Kirhenshtein <victor@netxms.org>
2017-08-16 18:55:22 +0300

    fixed Windows build using makefiles; Windows build switched to OpenSSL 1.1.0

--- build/msvc_setenv_x86.cmd
+++ build/msvc_setenv_x86.cmd
@@ -1,4 +1,5 @@
 @echo off
+set ARCH=x86
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin;C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE;C:\SDK\Windows 7 SDK\Bin;%PATH%
 set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;C:\SDK\Windows 7 SDK\Include;C:\SDK\WINDDK\inc\wnet;%JAVA_HOME%\include;%JAVA_HOME%\include\win32
 set LIB=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib;C:\SDK\Windows 7 SDK\Lib;C:\SDK\WINDDK\lib\wnet\i386
