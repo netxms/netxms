559985e458553e0b5aedcce1cc82c469e3ac8865
Victor Kirhenshtein <victor@netxms.org>
2017-08-30 23:20:48 +0300

    fixed memory leak

--- src/server/core/session.cpp
+++ src/server/core/session.cpp
@@ -8997,6 +8997,7 @@ void ClientSession::sendConfigForAgent(NXCPMessage *pRequest)
                   DecodeSQLStringAndSetVariable(&msg, VID_CONFIG_FILE, pszText);
                   msg.setField(VID_CONFIG_ID, dwCfgId);
                   free(pszText);
+                  delete vm;
                   break;
                }
                else
