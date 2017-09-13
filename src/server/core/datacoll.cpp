17dec8291746960a7bde90e7f1bf17c9a2d7307d
Victor Kirhenshtein <victor@netxms.org>
2017-09-13 12:48:36 +0300

    fixed server crash if DCI queued for cache loading was deleted

--- src/server/core/datacoll.cpp
+++ src/server/core/datacoll.cpp
@@ -504,7 +504,7 @@ THREAD_RESULT THREAD_CALL CacheLoader(void *arg)
       {
          object->incRefCount();
          DCObject *dci = static_cast<DataCollectionTarget*>(object)->getDCObjectById(ref->getId(), true);
-         if (dci->getType() == DCO_TYPE_ITEM)
+         if ((dci != NULL) && (dci->getType() == DCO_TYPE_ITEM))
          {
             DbgPrintf(6, _T("Loading cache for DCI %s [%d] on %s [%d]"),
                       ref->getName(), ref->getId(), object->getName(), object->getId());
