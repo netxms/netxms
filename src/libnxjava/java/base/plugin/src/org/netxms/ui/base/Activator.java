4a32466565312aab99ece7ff3cb7e2d9176d86b5
Victor Kirhenshtein <victor@netxms.org>
2017-09-15 13:03:48 +0300

    added Base Library plugin

---
+++ src/libnxjava/java/base/plugin/src/org/netxms/ui/base/Activator.java
@@ -0,0 +1,26 @@
+package org.netxms.ui.base;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator implements BundleActivator
+{
+   /* (non-Javadoc)
+    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+    */
+   @Override
+   public void start(BundleContext context) throws Exception
+   {
+   }
+
+   /* (non-Javadoc)
+    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+    */
+   @Override
+   public void stop(BundleContext context) throws Exception
+   {
+   }
+}
