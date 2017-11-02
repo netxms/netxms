a71300bcdded9530f5685d51677cf4c7b6cd037f
Victor Kirhenshtein <victor@netxms.org>
2017-11-02 20:14:14 +0200

    fixed incorrect mutex unlock in Node::getAgentConnection

--- src/server/core/node.cpp
+++ src/server/core/node.cpp
@@ -5407,11 +5407,11 @@ AgentConnectionEx *Node::getAgentConnection()
             conn = m_agentConnection;
             conn->incRefCount();
          }
+         MutexUnlock(m_hAgentAccessMutex);
          break;
       }
       ThreadSleepMs(50);
    }
-   MutexUnlock(m_hAgentAccessMutex);
 
    if (!success)
    {
