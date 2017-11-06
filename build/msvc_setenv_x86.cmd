eb84e3cf2181621599886cb153b3ec7509cf8e26
Victor Kirhenshtein <victor@netxms.org>
2017-11-07 00:05:20 +0200

    fixed Windows build environment setup script

--- build/msvc_setenv_x86.cmd
+++ build/msvc_setenv_x86.cmd
@@ -7,7 +7,7 @@ rem set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;C
 rem set LIB=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib;C:\SDK\Windows 7 SDK\Lib;C:\SDK\WINDDK\lib\wnet\i386
 
 rem *** Windows 7 SDK with Visual Studio 2017 Compiler
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX64\x86;C:\SDK\Windows 7 SDK\Bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX64\x86;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\bin\HostX64\x64;C:\SDK\Windows 7 SDK\Bin;%PATH%
 set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\ATLMFC\include;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\include;C:\Program Files (x86)\Windows Kits\10\include\10.0.10240.0\ucrt;C:\SDK\Windows 7 SDK\Include;C:\SDK\WINDDK\inc\wnet;%JAVA_HOME%\include;%JAVA_HOME%\include\win32;C:\SDK\VSSSDK72\inc
 set LIB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\ATLMFC\lib\x86;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.11.25503\lib\x86;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x86;C:\SDK\Windows 7 SDK\Lib;C:\SDK\WINDDK\lib\wnet\i386
 
