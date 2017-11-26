2c2dd9af2e96aab3e6fd0c45172acb71654dcb5b
Victor Kirhenshtein <victor@netxms.org>
2017-11-26 21:33:14 +0200

    remove deleted nodes from trusted nodes lists (issue #NX-801)

--- src/server/include/nms_objects.h
+++ src/server/include/nms_objects.h
@@ -559,7 +559,7 @@ protected:
    bool executeQueryOnObject(DB_HANDLE hdb, const TCHAR *query) { return ExecuteQueryOnObject(hdb, m_id, query); }
 
    virtual void prepareForDeletion();
-   virtual void onObjectDelete(UINT32 dwObjectId);
+   virtual void onObjectDelete(UINT32 objectId);
 
    virtual void fillMessageInternal(NXCPMessage *msg, UINT32 userId);
    virtual void fillMessageInternalStage2(NXCPMessage *msg, UINT32 userId);
@@ -870,7 +870,7 @@ protected:
 	void paeStatusPoll(UINT32 rqId, SNMP_Transport *pTransport, Node *node);
 
 protected:
-   virtual void onObjectDelete(UINT32 dwObjectId);
+   virtual void onObjectDelete(UINT32 objectId);
 
 	virtual void fillMessageInternal(NXCPMessage *pMsg, UINT32 userId);
    virtual UINT32 modifyFromMessageInternal(NXCPMessage *pRequest);
@@ -983,7 +983,7 @@ protected:
 	int m_requiredPollCount;
    UINT32 m_responseTime;  // Response time from last poll
 
-   virtual void onObjectDelete(UINT32 dwObjectId);
+   virtual void onObjectDelete(UINT32 objectId);
 
    virtual void fillMessageInternal(NXCPMessage *pMsg, UINT32 userId);
    virtual UINT32 modifyFromMessageInternal(NXCPMessage *pRequest);
@@ -1771,7 +1771,7 @@ protected:
    virtual void collectProxyInfo(ProxyInfo *info);
 
    virtual void prepareForDeletion();
-   virtual void onObjectDelete(UINT32 dwObjectId);
+   virtual void onObjectDelete(UINT32 objectId);
 
    virtual void fillMessageInternal(NXCPMessage *pMsg, UINT32 userId);
    virtual UINT32 modifyFromMessageInternal(NXCPMessage *pRequest);
@@ -2638,7 +2638,7 @@ public:
    virtual bool deleteFromDatabase(DB_HANDLE hdb);
    virtual bool loadFromDatabase(DB_HANDLE hdb, UINT32 id);
 
-   virtual void onObjectDelete(UINT32 dwObjectId);
+   virtual void onObjectDelete(UINT32 objectId);
 
    virtual json_t *toJson();
 
