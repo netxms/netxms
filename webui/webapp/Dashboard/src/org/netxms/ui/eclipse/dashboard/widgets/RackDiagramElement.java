91f1165860a3565c3742489d40d81b99247e0be8
Sviests <eriks@netxms.org>
2017-12-15 12:05:58 +0200

    Added title to Rack dashboard element. Fixes #NX-1369

--- webui/webapp/Dashboard/src/org/netxms/ui/eclipse/dashboard/widgets/RackDiagramElement.java
+++ webui/webapp/Dashboard/src/org/netxms/ui/eclipse/dashboard/widgets/RackDiagramElement.java
@@ -18,33 +18,62 @@
  */
 package org.netxms.ui.eclipse.dashboard.widgets;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IViewPart;
 import org.netxms.client.NXCSession;
 import org.netxms.client.constants.RackOrientation;
 import org.netxms.client.dashboards.DashboardElement;
+import org.netxms.client.objects.AbstractObject;
 import org.netxms.client.objects.Rack;
 import org.netxms.ui.eclipse.console.resources.SharedColors;
+import org.netxms.ui.eclipse.dashboard.propertypages.helpers.RackView;
 import org.netxms.ui.eclipse.dashboard.widgets.internal.RackDiagramConfig;
+import org.netxms.ui.eclipse.objectbrowser.api.ObjectContextMenu;
 import org.netxms.ui.eclipse.objectview.widgets.RackWidget;
 import org.netxms.ui.eclipse.shared.ConsoleSharedData;
+import org.netxms.ui.eclipse.tools.FontTools;
+import org.netxms.ui.eclipse.objectview.widgets.helpers.RackSelectionListener;
 
 /**
  *Rack diagram element for dashboard
  */
-public class RackDiagramElement extends ElementWidget
+public class RackDiagramElement extends ElementWidget implements ISelectionProvider
 {
-   private RackWidget rackFrontWidget;
-   private RackWidget rackRearWidget;
+   private static final String[] TITLE_FONTS = { "Segoe UI", "Liberation Sans", "DejaVu Sans", "Verdana", "Arial" };
+	   
+   private RackWidget rackFrontWidget = null;
+   private RackWidget rackRearWidget = null;
    private NXCSession session;
    private RackDiagramConfig config;
    private Composite rackArea;
-
+   private ScrolledComposite scroller;
+   private Label title;
+   private Font font = null;
+   private IViewPart viewPart;
+   private ISelection selection = new StructuredSelection();
+   private Set<ISelectionChangedListener> selectionListeners = new HashSet<ISelectionChangedListener>();
    /**
     * Create new rack diagram element
     * 
@@ -55,6 +84,8 @@ public class RackDiagramElement extends ElementWidget
    protected RackDiagramElement(DashboardControl parent, DashboardElement element, IViewPart viewPart)
    {
       super(parent, element, viewPart);
+            
+      this.viewPart = viewPart;
       
       try
       {
@@ -66,44 +97,159 @@ public class RackDiagramElement extends ElementWidget
          config = new RackDiagramConfig();
       }
       
+      GridLayout layout = new GridLayout();
+      layout.marginWidth = 0;
+      layout.marginHeight = 0;
+      setLayout(layout);
+      
       session = (NXCSession)ConsoleSharedData.getSession();
       Rack rack = session.findObjectById(config.getObjectId(), Rack.class);
       
       if (rack != null)
       {
-         rackArea = new Composite(this, SWT.NONE) {
+         if (config.isShowTitle())
+         {
+            title = new Label(this, SWT.CENTER);
+            font = FontTools.createFont(TITLE_FONTS, 2, SWT.BOLD);
+            title.setFont(font);
+            title.setBackground(SharedColors.getColor(SharedColors.RACK_BACKGROUND, parent.getDisplay()));
+            title.setText(rack.getObjectName());
+            title.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+         }
+                  
+         scroller = new ScrolledComposite(this, SWT.H_SCROLL);
+         scroller.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+         
+         rackArea = new Composite(scroller, SWT.NONE) {
             @Override
             public Point computeSize(int wHint, int hHint, boolean changed)
             {
-               if ((rackFrontWidget == null) || (rackRearWidget == null) || (hHint == SWT.DEFAULT))
-                  return super.computeSize(wHint, hHint, changed);
-               
-               Point s = rackFrontWidget.computeSize(wHint, hHint, changed);
-               return new Point(s.x * 2, s.y);
+               if (rackFrontWidget != null)
+               {
+                  Point s = rackFrontWidget.computeSize(wHint, hHint, changed);
+                  return new Point(s.x * 2, s.y);
+               }
+               if (rackRearWidget != null)
+               {
+                  Point s = rackRearWidget.computeSize(wHint, hHint, changed);
+                  return new Point(s.x * 2, s.y);
+               }
+
+               return super.computeSize(wHint, hHint, changed);    
             }
          };
          rackArea.setBackground(SharedColors.getColor(SharedColors.RACK_BACKGROUND, parent.getDisplay()));
          rackArea.addControlListener(new ControlAdapter() {
             @Override
             public void controlResized(ControlEvent e)
-            {
-               if ((rackFrontWidget == null) || (rackRearWidget == null))
-                  return;
-               
+            {               
                int height = rackArea.getSize().y;
-               Point size = rackFrontWidget.computeSize(SWT.DEFAULT, height, true);
-               rackFrontWidget.setSize(size);
-               rackRearWidget.setSize(size);
-               rackRearWidget.setLocation(size.x, 0);
+
+               if ((rackFrontWidget != null) && (rackRearWidget != null))
+               {
+                  Point size = rackFrontWidget.computeSize(SWT.DEFAULT, height, true);
+                  rackFrontWidget.setSize(size);
+                  rackRearWidget.setSize(size);
+                  rackRearWidget.setLocation(size.x, 0);
+               }
+               else if (rackFrontWidget != null)
+               {
+                  Point size = rackFrontWidget.computeSize(SWT.DEFAULT, height, true);
+                  rackFrontWidget.setSize(size);
+               }
+               else if (rackRearWidget != null)
+               {
+                  Point size = rackRearWidget.computeSize(SWT.DEFAULT, height, true);
+                  rackRearWidget.setSize(size);
+               }
             }
          });
          
-         setRackFrontWidget(new RackWidget(rackArea, SWT.NONE, rack, RackOrientation.FRONT));
-         setRackRearWidget(new RackWidget(rackArea, SWT.NONE, rack, RackOrientation.REAR));
+         if (config.getView() == RackView.FULL || config.getView() == RackView.FRONT)
+            setRackFrontWidget(new RackWidget(rackArea, SWT.NONE, rack, RackOrientation.FRONT));
+         if (config.getView() == RackView.FULL || config.getView() == RackView.BACK)
+            setRackRearWidget(new RackWidget(rackArea, SWT.NONE, rack, RackOrientation.REAR));
+
+         scroller.setContent(rackArea);
+         scroller.setExpandHorizontal(true);
+         scroller.setExpandVertical(true);
+         scroller.addControlListener(new ControlAdapter() {
+            public void controlResized(ControlEvent e)
+            {
+               scroller.setMinSize(rackArea.computeSize(SWT.DEFAULT, scroller.getSize().y));
+            }
+         });
       }
-      setLayout(new FillLayout());
+
+      addDisposeListener(new DisposeListener() {
+         @Override
+         public void widgetDisposed(DisposeEvent e)
+         {
+            if (font != null)
+               font.dispose();
+         }
+      });
+      
+      RackSelectionListener listener = new RackSelectionListener() {
+         @Override
+         public void objectSelected(AbstractObject object)
+         {
+            selection = (object != null) ? new StructuredSelection(object) : new StructuredSelection();
+            for(ISelectionChangedListener listener : selectionListeners)
+               listener.selectionChanged(new SelectionChangedEvent(RackDiagramElement.this, selection));
+         }
+      };
+      
+      if (rackRearWidget != null)
+         rackRearWidget.addSelectionListener(listener);
+      if (rackFrontWidget != null)
+      rackFrontWidget.addSelectionListener(listener);
+      
+      createPopupMenu();
+   }
+   
+   /**
+    * Create pop-up menu
+    */
+   private void createPopupMenu()
+   {
+      // Create menu manager.
+      MenuManager menuMgr = new MenuManager();
+      menuMgr.setRemoveAllWhenShown(true);
+      menuMgr.addMenuListener(new IMenuListener() {
+         public void menuAboutToShow(IMenuManager mgr)
+         {
+            fillContextMenu(mgr);
+         }
+      });
+
+      // Create menu.
+      if (rackFrontWidget != null)
+      {
+         Menu menu = menuMgr.createContextMenu(rackFrontWidget);
+         rackFrontWidget.setMenu(menu);
+      }
+      
+      if (rackRearWidget != null)
+      {
+         Menu menu = menuMgr.createContextMenu(rackRearWidget);
+         rackRearWidget.setMenu(menu);
+      }
+
+      // Register menu for extension.
+      viewPart.getSite().registerContextMenu(menuMgr, this);
    }
 
+   /**
+    * Fill port context menu
+    * 
+    * @param manager
+    */
+   private void fillContextMenu(IMenuManager manager)
+   {
+      ObjectContextMenu.fill(manager, viewPart.getSite(), this);
+   }
+   
    /**
     * Get rear rack widget
     * 
@@ -142,5 +288,41 @@ public class RackDiagramElement extends ElementWidget
    public void setRackFrontWidget(RackWidget rackFrontWidget)
    {
       this.rackFrontWidget = rackFrontWidget;
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.jface.viewers.ISelectionProvider#addSelectionChangedListener(org.eclipse.jface.viewers.ISelectionChangedListener)
+    */
+   @Override
+   public void addSelectionChangedListener(ISelectionChangedListener listener)
+   {
+      selectionListeners.add(listener);
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.jface.viewers.ISelectionProvider#getSelection()
+    */
+   @Override
+   public ISelection getSelection()
+   {
+      return selection;
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.jface.viewers.ISelectionProvider#removeSelectionChangedListener(org.eclipse.jface.viewers.ISelectionChangedListener)
+    */
+   @Override
+   public void removeSelectionChangedListener(ISelectionChangedListener listener)
+   {
+      selectionListeners.remove(listener);
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.jface.viewers.ISelectionProvider#setSelection(org.eclipse.jface.viewers.ISelection)
+    */
+   @Override
+   public void setSelection(ISelection selection)
+   {
+      this.selection = selection;
    }
 }
