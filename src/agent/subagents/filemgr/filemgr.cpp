451ffbbde86c53d7b47983822c2842485de5326e
Victor Kirhenshtein <victor@netxms.org>
2018-04-26 22:44:40 +0300

    fixed agent crash if session terminated during file transfer

--- src/agent/subagents/filemgr/filemgr.cpp
+++ src/agent/subagents/filemgr/filemgr.cpp
@@ -671,6 +671,7 @@ static BOOL MoveFile(TCHAR* oldName, TCHAR* newName)
       FollowData *flData = new FollowData(data->fileName, data->fileNameCode, 0, data->session->getServerAddress());
       ThreadCreateEx(SendFileUpdatesOverNXCP, 0, flData);
    }
+   data->session->decRefCount();
    free(data->fileName);
    free(data->fileNameCode);
    g_downloadFileStopMarkers->remove(data->id);
@@ -964,6 +965,7 @@ static BOOL ProcessCommands(UINT32 command, NXCPMessage *request, NXCPMessage *r
             data->id = request->getId();
             data->offset = request->getFieldAsUInt32(VID_FILE_OFFSET);
             data->session = session;
+            session->incRefCount();
             g_downloadFileStopMarkers->set(request->getId(), new VolatileCounter(0));
 
             ThreadCreateEx(SendFile, 0, data);
@@ -979,13 +981,15 @@ static BOOL ProcessCommands(UINT32 command, NXCPMessage *request, NXCPMessage *r
       case CMD_CANCEL_FILE_DOWNLOAD:
       {
          VolatileCounter *counter = g_downloadFileStopMarkers->get(request->getFieldAsUInt32(VID_REQUEST_ID));
-         if(counter != NULL)
+         if (counter != NULL)
          {
             InterlockedIncrement(counter);
             response->setField(VID_RCC, ERR_SUCCESS);
          }
          else
+         {
             response->setField(VID_RCC, ERR_INTERNAL_ERROR);
+         }
          return TRUE;
       }
       case CMD_CANCEL_FILE_MONITORING:
