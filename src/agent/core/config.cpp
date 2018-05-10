0c14f85366c41b897464811d1cefca9e61b7fc52
Victor Kirhenshtein <victor@netxms.org>
2018-05-10 18:33:23 +0300

    recover agent data directory after Windows 10 upgrade (issue #NX-1415)

--- src/agent/core/config.cpp
+++ src/agent/core/config.cpp
@@ -1,6 +1,6 @@
 /*
 ** NetXMS multiplatform core agent
-** Copyright (C) 2003-2016 Victor Kirhenshtein
+** Copyright (C) 2003-2018 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -21,11 +21,18 @@
 **/
 
 #include "nxagentd.h"
+#include <nxstat.h>
+
+#ifdef _WIN32
+#include <shlobj.h>
+#endif
 
 /**
  * Externals
  */
 LONG H_PlatformName(const TCHAR *cmd, const TCHAR *arg, TCHAR *value, AbstractCommSession *session);
+
+extern TCHAR g_dataDirRecoveryPath[];
 
 /**
  * Max NXCP message size
@@ -142,4 +149,203 @@ BOOL DownloadConfig(TCHAR *pszServer)
    WSACleanup();
 #endif
    return bRet;
+}
+
+/**
+* Create configuration file
+*/
+int CreateConfig(const char *pszServer, const char *pszLogFile, const char *pszFileStore,
+      const char *configIncludeDir, int iNumSubAgents, char **ppszSubAgentList)
+{
+   FILE *fp;
+   time_t currTime;
+   int i;
+
+   if (_taccess(g_szConfigFile, 0) == 0)
+      return 0;  // File already exist, we shouldn't overwrite it
+
+   fp = _tfopen(g_szConfigFile, _T("w"));
+   if (fp != NULL)
+   {
+      currTime = time(NULL);
+      _ftprintf(fp, _T("#\n# NetXMS agent configuration file\n# Created by agent installer at %s#\n\n"),
+         _tctime(&currTime));
+      _ftprintf(fp, _T("MasterServers = %hs\nConfigIncludeDir = %hs\nLogFile = %hs\nFileStore = %hs\n"),
+         pszServer, configIncludeDir, pszLogFile, pszFileStore);
+
+      Array extSubAgents;
+      for (i = 0; i < iNumSubAgents; i++)
+      {
+         if (!strnicmp(ppszSubAgentList[i], "EXT:", 4))
+         {
+            extSubAgents.add(ppszSubAgentList[i] + 4);
+         }
+         else
+         {
+            _ftprintf(fp, _T("SubAgent = %hs\n"), ppszSubAgentList[i]);
+         }
+      }
+
+      for (i = 0; i < extSubAgents.size(); i++)
+      {
+         char section[MAX_PATH];
+         strncpy(section, (const char *)extSubAgents.get(i), MAX_PATH);
+         char *eptr = strrchr(section, '.');
+         if (eptr != NULL)
+            *eptr = 0;
+         strupr(section);
+         _ftprintf(fp, _T("\n[EXT:%hs]\n"), section);
+         _ftprintf(fp, _T("SubAgent = %hs\n"), (const char *)extSubAgents.get(i));
+      }
+
+      fclose(fp);
+   }
+   return (fp != NULL) ? 0 : 2;
+}
+
+/**
+* Init config
+*/
+void InitConfig(const TCHAR *configSection)
+{
+   g_config = new Config();
+   g_config->setTopLevelTag(_T("config"));
+   g_config->setAlias(_T("agent"), configSection);
+
+   // Set default data directory
+   if (!_tcscmp(g_szDataDirectory, _T("{default}")))
+   {
+#ifdef _WIN32
+      if (SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, SHGFP_TYPE_CURRENT, g_szDataDirectory) == S_OK)
+      {
+         _tcslcat(g_szDataDirectory, _T("\\nxagentd"), MAX_PATH);
+      }
+      else
+      {
+         GetNetXMSDirectory(nxDirData, g_szDataDirectory);
+      }
+#else
+      GetNetXMSDirectory(nxDirData, g_szDataDirectory);
+#endif
+   }
+}
+
+#ifdef _WIN32
+
+/**
+* Copy file/folder
+*/
+static BOOL CopyFileOrDirectory(const TCHAR *oldName, const TCHAR *newName)
+{
+   NX_STAT_STRUCT st;
+   if (CALL_STAT(oldName, &st) != 0)
+      return FALSE;
+
+   if (!S_ISDIR(st.st_mode))
+      return CopyFile(oldName, newName, FALSE);
+
+   if (!CreateDirectory(newName, NULL))
+      return FALSE;
+
+   _TDIR *dir = _topendir(oldName);
+   if (dir == NULL)
+      return FALSE;
+
+   struct _tdirent *d;
+   while ((d = _treaddir(dir)) != NULL)
+   {
+      if (!_tcscmp(d->d_name, _T(".")) || !_tcscmp(d->d_name, _T("..")))
+         continue;
+
+      TCHAR nextNewName[MAX_PATH];
+      _tcscpy(nextNewName, newName);
+      _tcscat(nextNewName, FS_PATH_SEPARATOR);
+      _tcscat(nextNewName, d->d_name);
+
+      TCHAR nextOldaName[MAX_PATH];
+      _tcscpy(nextOldaName, oldName);
+      _tcscat(nextOldaName, FS_PATH_SEPARATOR);
+      _tcscat(nextOldaName, d->d_name);
+
+      CopyFileOrDirectory(nextOldaName, nextNewName);
+   }
+   _tclosedir(dir);
+   return TRUE;
+}
+
+/**
+* Recover agent's data directory.
+* During Windows 10 upgrades local service application data directory
+* can be moved to Windows.old.
+*/
+static void RecoverDataDirectory()
+{
+   if (_taccess(g_szDataDirectory, 0) == 0)
+      return;  // Data directory exist
+
+   TCHAR appDataDir[MAX_PATH];
+   if (SHGetFolderPath(NULL, CSIDL_LOCAL_APPDATA, NULL, SHGFP_TYPE_CURRENT, appDataDir) != S_OK)
+      return;
+
+   _tcslcat(appDataDir, _T("\\nxagentd"), MAX_PATH);
+   if (_tcsicmp(appDataDir, g_szDataDirectory))
+      return;  // Data directory is not in AppData
+
+   String oldAppDataDir(appDataDir);
+   oldAppDataDir.toUppercase();
+   oldAppDataDir.replace(_T("\\WINDOWS\\"), _T("\\WINDOWS.OLD\\"));
+   if (_taccess(oldAppDataDir, 0) != 0)
+      return;  // Old directory missing
+
+   if (CopyFileOrDirectory(oldAppDataDir, g_szDataDirectory))
+   {
+      _tcslcpy(g_dataDirRecoveryPath, oldAppDataDir, MAX_PATH);
+   }
+}
+
+#endif
+
+/**
+ * Load config
+ */
+bool LoadConfig()
+{
+   bool validConfig = g_config->loadConfig(g_szConfigFile, DEFAULT_CONFIG_SECTION, false);
+   if (validConfig)
+   {
+      const TCHAR *dir = g_config->getValue(_T("/%agent/ConfigIncludeDir"));
+      if (dir != NULL)
+      {
+         validConfig = g_config->loadConfigDirectory(dir, DEFAULT_CONFIG_SECTION, false);
+         if (!validConfig)
+         {
+            ConsolePrintf(_T("Error reading additional configuration files from \"%s\"\n"), dir);
+         }
+      }
+
+      dir = g_config->getValue(_T("/%agent/DataDirectory"));
+      if (dir != NULL)
+         _tcslcpy(g_szDataDirectory, dir, MAX_PATH);
+
+#ifdef _WIN32
+      RecoverDataDirectory();
+#endif
+
+      _tcslcpy(g_szConfigPolicyDir, g_szDataDirectory, MAX_PATH - 16);
+      if (g_szConfigPolicyDir[_tcslen(g_szConfigPolicyDir) - 1] != FS_PATH_SEPARATOR_CHAR)
+         _tcscat(g_szConfigPolicyDir, FS_PATH_SEPARATOR);
+      _tcscat(g_szConfigPolicyDir, CONFIG_AP_FOLDER FS_PATH_SEPARATOR);
+      CreateFolder(g_szConfigPolicyDir);
+
+      validConfig = g_config->loadConfigDirectory(g_szConfigPolicyDir, DEFAULT_CONFIG_SECTION);
+      if (!validConfig)
+      {
+         ConsolePrintf(_T("Error reading additional configuration files from \"%s\"\n"), g_szConfigPolicyDir);
+      }
+   }
+   else
+   {
+      ConsolePrintf(_T("Error loading configuration from \"%s\" section ") DEFAULT_CONFIG_SECTION _T("\n"), g_szConfigFile);
+   }
+   return validConfig;
 }
