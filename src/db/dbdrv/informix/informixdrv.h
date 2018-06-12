fde90c862593d587d2d3152c57128f9eb882101d
Victor Kirhenshtein <victor@netxms.org>
2018-06-13 00:52:46 +0300

    fixed configure for DB/2 and Informix; Informix driver compiles with Informix client 12.10

--- src/db/dbdrv/informix/informixdrv.h
+++ src/db/dbdrv/informix/informixdrv.h
@@ -1,6 +1,6 @@
 /* 
 ** Informix Database Driver
-** Copyright (C) 2010-2015 Raden Solutions
+** Copyright (C) 2010-2018 Raden Solutions
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
