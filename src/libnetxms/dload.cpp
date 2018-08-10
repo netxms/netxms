446484102a45478fdbf804d65d44fee6e0f37733
Victor Kirhenshtein <victor@netxms.org>
2018-08-10 05:52:04 -0500

    debug output in DL* functions; removed NetWare support in DL* functions

--- src/libnetxms/dload.cpp
+++ src/libnetxms/dload.cpp
@@ -1,7 +1,7 @@
 /** 
 ** NetXMS - Network Management System
 ** Utility Library
-** Copyright (C) 2003-2017 Victor Kirhenshtein
+** Copyright (C) 2003-2018 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published
@@ -23,53 +23,21 @@
 
 #include "libnetxms.h"
 
-#if defined(_NETWARE)
-#undef SEVERITY_CRITICAL
-#include <netware.h>
-#elif !defined(_WIN32)
+#ifndef _WIN32
 #ifdef _HPUX
 // dlfcn.h on HP-UX defines it's own UINT64 which conflicts with our definition
 #define UINT64 __UINT64
 #include <dlfcn.h>
 #undef UINT64
-#else
+#else /* _HPUX */
 #include <dlfcn.h>
-#endif
+#endif /* _HPUX */
 #ifndef RTLD_LOCAL
 #define RTLD_LOCAL	0
 #endif
-#endif
+#endif /* !_WIN32 */
 
-#ifdef _NETWARE
-
-/**
- * Error texts for NetWare LoadModule()
- */
-static char *m_pszErrorText[] =
-{
-   "No error",
-   "File not found",
-   "Error reading file",
-   "File not in NLM format",
-   "Wrong NLM file version",
-   "Reentrant initialization failure",
-   "Cannot load multiple copies",
-   "Operation already in progress",
-   "Not enough memory",
-   "Initialization failure",
-   "Inconsistent file format",
-   "Cannot load module at startup",
-   "Unresolved dependencies",
-   "Unresolved external symbols",
-   "Public symbol already defined",
-   "XDC data error",
-   "Module must be loaded in the kernel",
-   "Module is NIOS compatible only and cannot be loaded on NetWare",
-   "Cannot create specified address space",
-   "Module initialization failed"
-};
-
-#endif
+#define DEBUG_TAG _T("dload")
 
 /**
  * Load DLL/shared library
@@ -78,29 +46,11 @@ HMODULE LIBNETXMS_EXPORTABLE DLOpen(const TCHAR *pszLibName, TCHAR *pszErrorText
 {
    HMODULE hModule;
 
-#if defined(_WIN32)
+#ifdef _WIN32
    hModule = LoadLibrary(pszLibName);
    if ((hModule == NULL) && (pszErrorText != NULL))
       GetSystemErrorText(GetLastError(), pszErrorText, 255);
-#elif defined(_NETWARE)
-   TCHAR szBuffer[MAX_PATH + 4];
-   int nError;
-
-   _tcslcpy(&szBuffer[4], pszLibName, MAX_PATH);
-   nError = LoadModule(getnetwarelogger(), &szBuffer[4], LO_RETURN_HANDLE);
-   if (nError == 0)
-   {
-      hModule = *((HMODULE *)szBuffer);
-		if (pszErrorText != NULL)
-      	*pszErrorText = 0;
-   }
-   else
-   {
-      hModule = NULL;
-		if (pszErrorText != NULL)
-      	_tcslcpy(pszErrorText, (nError <= 19) ? m_pszErrorText[nError] : "Unknown error code", 255);
-   }
-#else    /* not _WIN32 and not _NETWARE */
+#else  /* not _WIN32 */
 #ifdef UNICODE
 	char *mbbuffer = MBStringFromWideString(pszLibName);
    hModule = dlopen(mbbuffer, RTLD_NOW | RTLD_LOCAL);
@@ -117,6 +67,7 @@ HMODULE LIBNETXMS_EXPORTABLE DLOpen(const TCHAR *pszLibName, TCHAR *pszErrorText
       _tcslcpy(pszErrorText, dlerror(), 255);
 #endif
 #endif
+   nxlog_debug_tag(DEBUG_TAG, 7, _T("DLOpen: file=\"%s\", module=%p"), pszLibName, hModule);
    return hModule;
 }
 
@@ -127,14 +78,9 @@ void LIBNETXMS_EXPORTABLE DLClose(HMODULE hModule)
 {
    if (hModule != NULL)
    {
-#if defined(_WIN32)
+      nxlog_debug_tag(DEBUG_TAG, 7, _T("DLClose: module=%p"), hModule);
+#ifdef _WIN32
       FreeLibrary(hModule);
-#elif defined(_NETWARE)
-      char szName[MAX_PATH], szTemp[1024];
-
-      GetNLMNames(hModule, szName, szTemp);
-      szName[szName[0] + 1] = 0;
-      UnloadModule(getscreenhandle(), &szName[1]);
 #else
       dlclose(hModule);
 #endif
@@ -148,14 +94,10 @@ void LIBNETXMS_EXPORTABLE *DLGetSymbolAddr(HMODULE hModule, const char *pszSymbo
 {
    void *pAddr;
 
-#if defined(_WIN32)
+#ifdef _WIN32
    pAddr = (void *)GetProcAddress(hModule, pszSymbol);
    if ((pAddr == NULL) && (pszErrorText != NULL))
       GetSystemErrorText(GetLastError(), pszErrorText, 255);
-#elif defined(_NETWARE)
-   pAddr = ImportPublicObject(hModule, pszSymbol);
-   if (pszErrorText != NULL)
-      *pszErrorText = 0;
 #else    /* _WIN32 */
    pAddr = dlsym(hModule, pszSymbol);
    if ((pAddr == NULL) && (pszErrorText != NULL))
@@ -163,11 +105,12 @@ void LIBNETXMS_EXPORTABLE *DLGetSymbolAddr(HMODULE hModule, const char *pszSymbo
 #ifdef UNICODE
    	WCHAR *wbuffer = WideStringFromMBString(dlerror());
       _tcslcpy(pszErrorText, wbuffer, 255);
-      free(wbuffer);
+      MemFree(wbuffer);
 #else
       _tcslcpy(pszErrorText, dlerror(), 255);
 #endif
 	}
 #endif
+   nxlog_debug_tag(DEBUG_TAG, 7, _T("DLGetSymbolAddr: module=%p, symbol=%hs, address=%p"), hModule, pszSymbol, pAddr);
    return pAddr;
 }
