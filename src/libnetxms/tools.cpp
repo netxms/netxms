3326b2b7de01b6d1f1ba4e0f4d5b203adfbb06ff
Victor Kirhenshtein <victor@netxms.org>
2018-11-23 15:39:13 +0200

    fixed WriteToTerminal redirection handling on Linux/UNICODE

--- src/libnetxms/tools.cpp
+++ src/libnetxms/tools.cpp
@@ -2324,6 +2324,7 @@ static WORD ApplyTerminalAttribute(HANDLE out, WORD currAttr, long code)
 #endif
 
 #if defined(UNICODE) && !defined(_WIN32) && HAVE_FPUTWS
+#define WRT_UNICODE  1
 #define WRT_TCHAR    WCHAR
 #define _wrt_tcschr  wcschr
 #define _wrt_tcslen  wcslen
@@ -2333,10 +2334,16 @@ static WORD ApplyTerminalAttribute(HANDLE out, WORD currAttr, long code)
 #define _wrt_tcslen  strlen
 #endif
 
-#ifdef _WIN32
-#define WRT_HANDLE   HANDLE
-#else
-#define WRT_HANDLE   FILE*
+#if WRT_UNICODE
+static void fputwsl(const WCHAR *text, const WCHAR *stop, FILE *out)
+{
+   const WCHAR *p = text;
+   while(p < stop)
+   {
+      fputwc(*p, out);
+      p++;
+   }
+}
 #endif
 
 /**
@@ -2354,7 +2361,11 @@ static void WriteRedirectedTerminalOutput(const WRT_TCHAR *text)
          if (*esc == '[')
          {
             // write everything up to ESC char
+#if WRT_UNICODE
+            fputwsl(curr, esc - 1, stdout);
+#else
             fwrite(curr, sizeof(WRT_TCHAR), esc - curr - 1, stdout);
+#endif
             esc++;
             while((*esc != 0) && (*esc != _T('m')))
                esc++;
@@ -2364,13 +2375,17 @@ static void WriteRedirectedTerminalOutput(const WRT_TCHAR *text)
          }
          else
          {
+#if WRT_UNICODE
+            fputwsl(curr, esc, stdout);
+#else
             fwrite(curr, sizeof(WRT_TCHAR), esc - curr, stdout);
+#endif
          }
          curr = esc;
       }
       else
       {
-#if HAVE_FPUTWS
+#if WRT_UNICODE
          fputws(curr, stdout);
 #else
          fputs(curr, stdout);
