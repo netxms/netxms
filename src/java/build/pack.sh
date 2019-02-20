e0e53b5cfa7bd16bb7a51973fdb5636d1144ce54
Victor Kirhenshtein <victor@netxms.org>
2019-02-20 23:53:32 +0200

    cosmetic changes

--- src/java/build/pack.sh
+++ src/java/build/pack.sh
@@ -2,7 +2,7 @@
 
 . ./set_build_number.sh
 #version=2.0-RC2-$build_number
-version=2.1
+version=3.0-M0-1500
 
 cd win32.win32.x86
 zip -r nxmc-$version-win32-x86.zip nxmc
@@ -14,11 +14,11 @@ zip -r nxmc-$version-win32-x64.zip nxmc
 mv nxmc-$version-win32-x64.zip ..
 cd ..
 
-cd linux.gtk.x86
-tar cvf nxmc-$version-linux-gtk-x86.tar nxmc
-gzip nxmc-$version-linux-gtk-x86.tar
-mv nxmc-$version-linux-gtk-x86.tar.gz ..
-cd ..
+#cd linux.gtk.x86
+#tar cvf nxmc-$version-linux-gtk-x86.tar nxmc
+#gzip nxmc-$version-linux-gtk-x86.tar
+#mv nxmc-$version-linux-gtk-x86.tar.gz ..
+#cd ..
 
 cd linux.gtk.x86_64
 tar cvf nxmc-$version-linux-gtk-x64.tar nxmc
