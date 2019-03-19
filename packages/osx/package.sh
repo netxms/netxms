d081278cc7e3a687812f0e33f4373221feea30b9
Alex Kirhenshtein <alk@netxms.org>
2019-03-19 19:46:19 -0400

    avoid signing JRE inside macOS app

--- packages/osx/package.sh
+++ packages/osx/package.sh
@@ -15,9 +15,9 @@ fi
 
 rm -rf dist/* nxmc-${version}.dmg
 ~/Development/yoursway-eclipse-osx-repackager/EclipseOSXRepackager out/eclipse dist/NetXMS\ Console.app
-patch -p0 < nxmc.ini.diff
-tar zxf jre_1.8.0_151.tar.gz -C dist/NetXMS\ Console.app/Contents/MacOS/
 
 cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
 codesign --deep --force -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' -v 'dist/NetXMS Console.app'
-~/Development/yoursway-create-dmg/create-dmg --volname NetXMS --volicon ~/Development/netxms/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
+patch -p0 < nxmc.ini.diff
+tar zxf jre_1.8.0_151.tar.gz -C dist/NetXMS\ Console.app/Contents/MacOS/
+create-dmg --volname "NetXMS ${version}" --volicon ~/Development/netxms/netxms/packages/osx/dmg-icon.icns --window-size 500 320 --background ~/Development/netxms/netxms/packages/osx/dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
