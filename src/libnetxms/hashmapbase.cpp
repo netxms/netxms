8c1b60904e03c05bb34fdf4efea8ce56ab722f69
Victor Kirhenshtein <victor@netxms.org>
2019-03-28 10:36:39 +0200

    updated bundled uthash version

--- src/libnetxms/hashmapbase.cpp
+++ src/libnetxms/hashmapbase.cpp
@@ -234,15 +234,15 @@ void *HashMapIterator::next()
 
    if (m_curr == NULL)  // iteration not started
    {
-      HASH_ITER_START(hh, m_hashMap->m_data, m_curr, m_next);
+      m_curr = m_hashMap->m_data;
    }
    else
    {
       if (m_next == NULL)
          return NULL;
-
-      HASH_ITER_NEXT(hh, m_curr, m_next);
+      m_curr = m_next;
    }
+   m_next = static_cast<HashMapEntry*>(m_curr->hh.next);
    return m_curr->value;
 }
 
