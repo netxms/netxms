d44a00f5f98e92672d4376ba328f447a4479ada9
Victor Kirhenshtein <victor@netxms.org>
2019-04-16 15:00:32 +0300

    updated tag update script for Windows

--- build/updatetag.pl
+++ build/updatetag.pl
@@ -12,6 +12,14 @@ if ($tag =~ /^Release-(.*)/)
 }
 print "Git tag: $tag\n";
 
+my $version_string = $tag;
+my $build_number = 0;
+if ($tag =~ /(.*)-([0-9]+)-g.*/)
+{
+   $version_string = $1 . "." . $2;
+   $build_number = $2;
+}
+
 my $update = 1;
 
 if (open(IN, "<$file"))
@@ -40,6 +48,9 @@ if ($update == 1)
 	print OUT "#define _" . $file_prefix . "_build_tag_h_\n";
 	print OUT "#define " . $define_prefix . "_BUILD_TAG _T(\"$tag\")\n";
 	print OUT "#define " . $define_prefix . "_BUILD_TAG_A \"$tag\"\n";
+	print OUT "#define " . $define_prefix . "_BUILD_NUMBER $build_number\n";
+	print OUT "#define " . $define_prefix . "_VERSION_STRING _T(\"$version_string\")\n";
+	print OUT "#define " . $define_prefix . "_VERSION_STRING_A \"$version_string\"\n";
 	print OUT "#endif\n";
 	close OUT;
 
