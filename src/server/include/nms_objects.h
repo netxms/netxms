28beb522690ec2cbb50f340c01a62aa1a50dfdee
Victor Kirhenshtein <victor@netxms.org>
2019-05-29 18:20:17 +0300

    reduce number of agent data collection configuration synchronization requests

--- src/server/include/nms_objects.h
+++ src/server/include/nms_objects.h
@@ -1673,6 +1673,7 @@ protected:
 	MUTEX m_mutexTopoAccess;
    AgentConnectionEx *m_agentConnection;
    ProxyAgentConnection *m_proxyConnections;
+   VolatileCounter m_pendingDataConfigurationSync;
    SMCLP_Connection *m_smclpConnection;
 	UINT64 m_lastAgentTrapId;	     // ID of last received agent trap
    UINT64 m_lastAgentPushRequestId; // ID of last received agent push request
