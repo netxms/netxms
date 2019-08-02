4dc8b946f183a748106074ad081ec90d27ccdfed
Victor Kirhenshtein <victor@netxms.org>
2019-08-02 10:00:50 +0300

    fixed incorrect DB upgrade procedure for TimeScaleDB

--- src/server/tools/nxdbmgr/upgrade_v30.cpp
+++ src/server/tools/nxdbmgr/upgrade_v30.cpp
@@ -2449,16 +2449,17 @@ static bool H_UpgradeFromV11()
 static bool H_UpgradeFromV10()
 {
    CHK_EXEC(SQLQuery(_T("UPDATE metadata SET var_value='CREATE TABLE idata_%d (item_id integer not null,idata_timestamp integer not null,idata_value varchar(255) null,raw_value varchar(255) null)' WHERE var_name='IDataTableCreationCommand'")));
-
-   IntegerArray<UINT32> *targets = GetDataCollectionTargets();
-   for(int i = 0; i < targets->size(); i++)
+   if (DBMgrMetaDataReadInt32(_T("SingeTablePerfData"), 0) == 0)
    {
-      TCHAR query[256];
-      _sntprintf(query, 256, _T("ALTER TABLE idata_%d ADD raw_value varchar(255)"), targets->get(i));
-      CHK_EXEC(SQLQuery(query));
+      IntegerArray<UINT32> *targets = GetDataCollectionTargets();
+      for(int i = 0; i < targets->size(); i++)
+      {
+         TCHAR query[256];
+         _sntprintf(query, 256, _T("ALTER TABLE idata_%d ADD raw_value varchar(255)"), targets->get(i));
+         CHK_EXEC(SQLQuery(query));
+      }
+      delete targets;
    }
-   delete targets;
-
    CHK_EXEC(SetMinorSchemaVersion(11));
    return true;
 }
