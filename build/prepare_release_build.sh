0c157c1b37cb2a63b68edfacabf8ee1d798bd5c8
Alex Kirhenshtein <alk@netxms.org>
2019-09-10 16:24:06 +0300

    build/prepare_release_build.sh updated to support android client

--- build/prepare_release_build.sh
+++ build/prepare_release_build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Prepare source tree for release build
-# Must be run from root of source tree
+
+if [ ! -r configure.ac ]; then
+	echo Must be run from root of source tree\!
+	exit 1
+fi
+
+set -e
 
 ### Configuration
-POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml"
+POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml src/mobile-agent/java"
 
 ### Code
 cd build
@@ -18,10 +24,17 @@ for pom in $POM_FILES; do
 done
 atlas-mvn -f src/java/netxms-jira-connector/pom.xml versions:set -DnewVersion=$VERSION
 
-./tools/update_plugin_versions.py . $VERSION
+./build/update_plugin_versions.py . $VERSION
 
 cat src/java/netxms-eclipse/Core/plugin.xml | sed -E "s,^(.*;Version) [0-9.]+(&#x0A.*)\$,\\1 $VERSION\\2," > ./plugin.xml
 mv ./plugin.xml src/java/netxms-eclipse/Core/plugin.xml
 
 cat src/java/netxms-eclipse/Product/nxmc.product | sed -E "s,^Version [0-9.]+\$,Version $VERSION," > nxmc.product
 mv ./nxmc.product src/java/netxms-eclipse/Product/nxmc.product
+
+sed -i '' "s,org.netxms:netxms-client:3.0-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/src/agent/app/build.gradle
+sed -i '' "s,org.netxms:netxms-mobile-agent:3.0-SNAPSHOT,org.netxms:netxms-mobile-agent:$VERSION,g" android/src/agent/app/build.gradle
+sed -i '' "s,org.netxms:netxms-client:3.0-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/src/console/app/build.gradle
+sed -i '' "s,org.netxms:netxms-base:3.0-SNAPSHOT,org.netxms:netxms-base:$VERSION,g" android/src/console/app/build.gradle
+
+./reconf
