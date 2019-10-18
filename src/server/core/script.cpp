c864d6208439ef5ecdcfc2b75369eb48d35e6aec
Victor Kirhenshtein <victor@netxms.org>
2019-10-18 13:48:38 +0300

    fixed memory leak in script export

--- src/server/core/script.cpp
+++ src/server/core/script.cpp
@@ -363,7 +363,7 @@ void CreateScriptExportRecord(String &xml, UINT32 id)
    NXSL_LibraryScript *script = LoadScriptFromDatabase(id);
    if (script == NULL)
    {
-      DbgPrintf(3, _T("CreateScriptExportRecord: failed to load script with ID %d from database"), (int)id);
+      nxlog_debug(3, _T("CreateScriptExportRecord: failed to load script with ID %u from database"), id);
       return;
    }
 
@@ -379,6 +379,8 @@ void CreateScriptExportRecord(String &xml, UINT32 id)
    xml.append(_T("\t\t\t<code>"));
    xml.append(EscapeStringForXML2(script->getCode()));
    xml.append(_T("</code>\n\t\t</script>\n"));
+
+   delete script;
 }
 
 /**
