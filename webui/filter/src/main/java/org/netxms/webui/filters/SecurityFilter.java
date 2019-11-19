e314543ad68eaa0f2cae8ba1e7731b0ab86be308
Victor Kirhenshtein <victor@netxms.org>
2019-11-19 19:25:08 +0200

    use X-Frame-Options SAMEORIGIN instead of DENY

--- webui/filter/src/main/java/org/netxms/webui/filters/SecurityFilter.java
+++ webui/filter/src/main/java/org/netxms/webui/filters/SecurityFilter.java
@@ -28,7 +28,7 @@ public class SecurityFilter implements Filter
    @Override
    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
    {
-      ((HttpServletResponse)response).addHeader("X-Frame-Options", "DENY");
+      ((HttpServletResponse)response).addHeader("X-Frame-Options", "SAMEORIGIN");
       chain.doFilter(request, response);
    }
 
