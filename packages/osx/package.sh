6ace8812a9c56853e74513a9955d678137b55447
Alex Kirhenshtein <alk@netxms.org>
2020-03-07 00:00:37 +0200

    macOS app signing fixed

--- packages/osx/package.sh
+++ packages/osx/package.sh
@@ -8,10 +8,12 @@ if [ ! -r zulu11.33.15-ca-jre11.0.4-macosx_x64.zip ]; then
 fi
 
 if [ -z $1 ]; then
-   version=`grep AC_INIT ../../configure.ac|cut -d'[' -f3|cut -d']' -f1`
+   version=`git describe|cut -d- -f1,2|tr - .`
 else
    version=$1
 fi
+
+echo Version: $version
 
 mkdir -p dist
 rm -rf dist/* nxmc-${version}.dmg
@@ -22,6 +24,23 @@ cd dist/NetXMS\ Console.app/Contents/
 unzip -x ../../../zulu11.33.15-ca-jre11.0.4-macosx_x64.zip >/dev/null
 mv zulu11.33.15-ca-jre11.0.4-macosx_x64 jre
 cd - >/dev/null
-codesign --deep --force -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' -v 'dist/NetXMS Console.app'
+
+find dist/NetXMS\ Console.app -name \*.so -print0 | xargs -0 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist
+find dist/NetXMS\ Console.app -name \*.jnilib -print0 | xargs -0 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist
+find dist/NetXMS\ Console.app -name \*.dylib -print0 | xargs -0 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist
+
+cd dist/NetXMS\ Console.app/Contents/jre
+codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements ../../../../entitlements.plist `find . -type f | xargs -n 1 file | grep Mach-O | cut -d: -f1`
+cd -
+
+7z x dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.x86_64_3.113.0.v20191204-0601.jar libswt-pi-cocoa-4930r7.jnilib libswt-cocoa-4930r7.jnilib libswt-awt-cocoa-4930r7.jnilib
+codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist ./libswt-*
+7z a dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.x86_64_3.113.0.v20191204-0601.jar libswt-pi-cocoa-4930r7.jnilib libswt-cocoa-4930r7.jnilib libswt-awt-cocoa-4930r7.jnilib
+rm libswt-pi-cocoa-4930r7.jnilib libswt-cocoa-4930r7.jnilib libswt-awt-cocoa-4930r7.jnilib
+codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist dist/NetXMS\ Console.app
+
+##codesign --options=runtime --deep --force -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' -v 'dist/NetXMS Console.app'
 create-dmg --no-internet-enable --volname "NetXMS ${version}" --volicon dmg-icon.icns --window-size 500 320 --background dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
 codesign -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' nxmc-${version}.dmg
+
+xcrun altool --notarize-app --primary-bundle-id "org.netxms.ui.eclipse.console.product" --username "alk@alk.lv" --password "@keychain:AC_PASSWORD" --asc-provider KFFD69X4L6 --file nxmc-${version}.dmg
