be1f772d187b0b391b347ae9b79082c05b827a8c
Victor Kirhenshtein <victor@netxms.org>
2020-04-21 11:38:21 +0300

    updated build preparation steps on Windows

--- build/msvc_setenv_x64.cmd
+++ build/msvc_setenv_x64.cmd
@@ -2,13 +2,8 @@
 set ARCH=x64
 SET MSVC_VERSION=14.16.27023
 
-rem *** Windows 7 SDK with Visual Studio 2010 Compiler
-rem set PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64;C:\SDK\Windows 7 SDK\Bin\x64;C:\SDK\Windows 7 SDK\Bin;%PATH%
-rem set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include;C:\SDK\Windows 7 SDK\Include;C:\SDK\WINDDK\inc\wnet;%JAVA_HOME%\include;%JAVA_HOME%\include\win32
-rem set LIB=C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\lib\amd64;C:\SDK\Windows 7 SDK\Lib\x64;C:\SDK\WINDDK\lib\wnet\amd64
-
 rem *** Windows 7 SDK with Visual Studio 2017 Compiler
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\bin\HostX64\x64;C:\SDK\Windows 7 SDK\Bin\x64;C:\SDK\Windows 7 SDK\Bin;%PATH%
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\bin\HostX64\x64;C:\SDK\Windows 7 SDK\Bin\x64;C:\SDK\Windows 7 SDK\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;%PATH%
 set INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\ATLMFC\include;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\include;C:\Program Files (x86)\Windows Kits\10\include\10.0.10240.0\ucrt;C:\SDK\Windows 7 SDK\Include;C:\SDK\WINDDK\inc\wnet;%JAVA_HOME%\include;%JAVA_HOME%\include\win32;C:\SDK\VSSSDK72\inc;C:\SDK\libssh\include;C:\Program Files\Python37\include;C:\SDK\PCRE\x64\include
 set LIB=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\ATLMFC\lib\x64;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\%MSVC_VERSION%\lib\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.10240.0\ucrt\x64;C:\SDK\Windows 7 SDK\Lib\x64;C:\SDK\WINDDK\lib\wnet\amd64;C:\SDK\libssh\lib\x64;C:\Program Files\Python37\libs;C:\SDK\MSVCRT\x64;;C:\SDK\PCRE\x64\lib
 
---
+++ tools/add_vcxproj_include_dir.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+def updateProject(project, depth):
+   print("Updating " + project + " at depth " + str(depth))
+   replacement = "<AdditionalIncludeDirectories>"
+   for i in range(0, depth):
+      replacement = replacement + "..\\"
+   replacement = replacement + sys.argv[1] + ";"
+   try:
+      f_in = open(project)
+      f_out = open(project + ".new", "w")
+      for line in f_in:
+         f_out.write(line.replace("<AdditionalIncludeDirectories>", replacement))
+      f_in.close()
+      f_out.close()
+      os.remove(project)
+      os.rename(project + ".new", project)
+      print("SUCCESS")
+   except IOError:
+      print("FAILED")
+
+def getPathDepth(path):
+   depth = 0
+   tail = os.path.split(path)
+   while tail[0] != "":
+      tail = os.path.split(tail[0])
+      depth = depth + 1
+   return depth
+
+def main():
+   for (root, dirs, files) in os.walk("."):
+      for f in files:
+         if f.endswith(".vcxproj"):
+            updateProject(os.path.join(root, f), getPathDepth(root))
+
+if len(sys.argv) == 2:
+    main()
+else:
+    print("Usage: ./sync_message_files.py <include dir>")
