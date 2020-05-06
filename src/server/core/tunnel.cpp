58045f9b7619dad2b9a292dae04bbcb50247c4b3
Victor Kirhenshtein <victor@netxms.org>
2020-05-06 18:57:38 +0300

    fixed use of uninitialized vaiable

--- src/server/core/tunnel.cpp
+++ src/server/core/tunnel.cpp
@@ -951,6 +951,10 @@ int AgentTunnelCommChannel::poll(UINT32 timeout, bool write)
       success = (pthread_cond_timedwait(&m_dataCondition, &m_bufferLock, &ts) == 0);
 #endif
    }
+   else
+   {
+      success = TRUE;
+   }
 #ifdef _WIN32
    LeaveCriticalSection(&m_bufferLock);
 #else
