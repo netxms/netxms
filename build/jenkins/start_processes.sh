5546cfe7872228c8bc55dc78d316d0443fab5e5f
zev <zev@radensolutions.com>
2020-05-12 19:47:14 +0300

    Added start process jenkins script

---
+++ build/jenkins/start_processes.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "Starting netxmsd and nxagentd"
+
+if test "x$BUILD_PREFIX" = "x"; then
+	echo "ERROR: build prefix not set"
+	exit 1
+fi
+
+$BUILD_PREFIX/bin/nxdbmgr -f unlock
+$BUILD_PREFIX/bin/nxdbmgr upgrade
+$BUILD_PREFIX/bin/netxmsd -d -D6
+$BUILD_PREFIX/bin/nxagentd -d -D6 -p $BUILD_PREFIX/agent.pid
+
+#Wait process to start
+sleep 60
+
+if ps cax | grep [n]etxmsd; then
+    echo "Command netxmsd is running"
+else
+    echo "Command netxmsd is not running"
+    exit 1
+fi
+
+if ps cax | grep [n]xagentd; then
+    echo "Command nxagentd is running"
+else
+    echo "Command nxagentd is not running"
+    exit 1
+fi
