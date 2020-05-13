c359025d5f4e1803f044c197528af6d300019b94
zev <zev@radensolutions.com>
2020-05-13 17:11:28 +0300

    Added option to start usual build with wrapper

--- build/jenkins/start_processes.sh
+++ build/jenkins/start_processes.sh
@@ -7,9 +7,9 @@ if test "x$BUILD_PREFIX" = "x"; then
 	exit 1
 fi
 
-$BUILD_PREFIX/bin/nxdbmgr -f unlock
-$BUILD_PREFIX/bin/nxdbmgr upgrade
-$BUILD_PREFIX/bin/netxmsd -d -D6
+$BUILD_PREFIX/bin/nxdbmgr-asan -f unlock
+$BUILD_PREFIX/bin/nxdbmgr-asan upgrade
+$BUILD_PREFIX/bin/netxmsd-asan -d -D6
 $BUILD_PREFIX/bin/nxagentd -d -D6 -p $BUILD_PREFIX/agent.pid
 
 #Wait process to start
