5810923bf82f293a05379e205a51e1894f06a9a2
Victor Kirhenshtein <victor@netxms.org>
2020-05-18 16:11:23 +0300

    improved build preparation script for Windows

--- build/prepare_release_build.cmd
+++ build/prepare_release_build.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
 
-SET POM_FILES=src/java-common/netxms-base/pom.xml;src/libnxjava/java/pom.xml;src/client/java/netxms-client/pom.xml;src/client/nxapisrv/java/pom.xml;src/client/nxshell/java/pom.xml;src/client/nxtcpproxy/pom.xml;src/agent/subagents/bind9/pom.xml;src/agent/subagents/java/java/pom.xml;src/agent/subagents/jmx/pom.xml;src/agent/subagents/ubntlw/pom.xml;src/mobile-agent/java;src/java/nxreporting/pom.xml
+SET SUBAGENT_POM_FILES=src/agent/subagents/bind9/pom.xml;src/agent/subagents/jmx/pom.xml;src/agent/subagents/ubntlw/pom.xml
+SET POM_FILES=src/java-common/netxms-base/pom.xml;src/libnxjava/java/pom.xml;src/client/java/netxms-client/pom.xml;src/client/nxapisrv/java/pom.xml;src/client/nxshell/java/pom.xml;src/client/nxtcpproxy/pom.xml;src/agent/subagents/java/java/pom.xml;src/mobile-agent/java;src/java/nxreporting/pom.xml
 
 cd build
 updatetag.pl
@@ -10,5 +11,15 @@ copy build\netxms-build-tag.properties src\java-common\netxms-base\src\main\reso
 for /f "delims=" %%a in ('cat build/netxms-build-tag.properties ^| grep NETXMS_VERSION ^| cut -d^= -f2') do @set VERSION=%%a
 echo VERSION=%VERSION%
 
-for %%p in (%POM_FILES%) do mvn -f %%p versions:set -DnewVersion=%VERSION%
-rem atlas-mvn -f src/java/netxms-jira-connector/pom.xml versions:set -DnewVersion=%VERSION%
+for %%p in (%POM_FILES%) do cmd /C mvn -f %%p versions:set -DnewVersion=%VERSION%
+
+cmd /C mvn -f src/java-common/netxms-base/pom.xml -Dmaven.test.skip=true install -Ppkg-build
+cmd /C mvn -f src/client/java/netxms-client/pom.xml -Dmaven.test.skip=true install -Ppkg-build
+cmd /C mvn -f src/libnxjava/java/pom.xml -Dmaven.test.skip=true install
+cmd /C mvn -f src/agent/subagents/java/java/pom.xml -Dmaven.test.skip=true install
+cmd /C mvn -f src/client/nxshell/java/pom.xml package -Pnxshell-launcher
+for %%p in (%SUBAGENT_POM_FILES%) do cmd /C mvn -f %%p package
+
+cd sql
+make -f Makefile.w32
+cd ..
