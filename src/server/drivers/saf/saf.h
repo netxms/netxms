297f16ad04bade880702ba405a6e8fa0a272dd1a
Victor Kirhenshtein <victor@netxms.org>
2020-08-04 13:31:18 +0300

    added driver for SAF Tehnika Integra devices

---
+++ src/server/drivers/saf/saf.h
@@ -0,0 +1,45 @@
+/* 
+** NetXMS - Network Management System
+** Drivers for SAF Tehnika devices
+** Copyright (C) 2020 Raden Solutions
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: saf.h
+**
+**/
+
+#ifndef _saf_h_
+#define _saf_h_
+
+#include <nddrv.h>
+
+#define DEBUG_TAG _T("ndd.saf")
+
+/**
+ * Driver for SAF Tehnika IntegraB devices
+ */
+class SafIntegraBDriver : public NetworkDeviceDriver
+{
+public:
+   virtual const TCHAR *getName() override;
+   virtual const TCHAR *getVersion() override;
+
+   virtual int isPotentialDevice(const TCHAR *oid) override;
+   virtual bool isDeviceSupported(SNMP_Transport *snmp, const TCHAR *oid) override;
+   virtual bool getHardwareInformation(SNMP_Transport *snmp, NObject *node, DriverData *driverData, DeviceHardwareInfo *hwInfo) override;
+};
+
+#endif
