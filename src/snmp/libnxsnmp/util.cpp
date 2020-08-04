05fa0101f4a6c0fdda52b1ffedaa05916437a673
Victor Kirhenshtein <victor@netxms.org>
2020-08-04 13:27:38 +0300

    fixed incorrect handling of SG_GET_NEXT_REQUEST flag in SnmpGet (fixes NX-1916)

--- src/snmp/libnxsnmp/util.cpp
+++ src/snmp/libnxsnmp/util.cpp
@@ -136,7 +136,8 @@ UINT32 LIBNXSNMP_EXPORTABLE SnmpGetEx(SNMP_Transport *pTransport,
 
             if ((pVar->getType() != ASN_NO_SUCH_OBJECT) &&
                 (pVar->getType() != ASN_NO_SUCH_INSTANCE) &&
-                (pVar->getType() != ASN_END_OF_MIBVIEW))
+                (pVar->getType() != ASN_END_OF_MIBVIEW) &&
+                (!(dwFlags & SG_GET_NEXT_REQUEST) || (pVar->getName().compare(pdwVarName, nameLength) == OID_LONGER)))
             {
                if (dwFlags & SG_RAW_RESULT)
                {
