8b12ce26aa48268416f18df98f55090e55f00459
Victor Kirhenshtein <victor@netxms.org>
2020-09-28 15:10:34 +0300

    fixed bug in NXSL function ExpandString

--- src/server/core/nxslext.cpp
+++ src/server/core/nxslext.cpp
@@ -1638,6 +1638,7 @@ static int F_ExpandString(int argc, NXSL_Value **argv, NXSL_Value **result, NXSL
    {
       if (!argv[2]->isObject(_T("Event")))
          return NXSL_ERR_NOT_OBJECT;
+      event = static_cast<Event*>(argv[2]->getValueAsObject()->getData());
    }
 
    *result = vm->createValue(object->expandText(argv[0]->getValueAsCString(), NULL, event, NULL, NULL, NULL, NULL));
