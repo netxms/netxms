467ba182a5cc8d2e90c3937a0cfe0e2c24e700e5
Victor Kirhenshtein <victor@netxms.org>
2020-12-01 11:32:07 +0200

    improved release preparation script

--- build/prepare_release_build.sh
+++ build/prepare_release_build.sh
@@ -9,7 +9,7 @@ fi
 set -e
 
 ### Configuration
-POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml src/mobile-agent/java src/java/nxreporting/pom.xml"
+POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml src/mobile-agent/java src/java/nxreporting/pom.xml test/integration/pom.xml"
 
 ### Code
 cd build
@@ -32,10 +32,10 @@ mv ./plugin.xml src/java/netxms-eclipse/Core/plugin.xml
 cat src/java/netxms-eclipse/Product/nxmc.product | sed -r "s,^Version [0-9.]+\$,Version $VERSION," > nxmc.product
 mv ./nxmc.product src/java/netxms-eclipse/Product/nxmc.product
 
-sed -i -e "s,org.netxms:netxms-client:3.4-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/agent/app/build.gradle
-sed -i -e "s,org.netxms:netxms-mobile-agent:3.4-SNAPSHOT,org.netxms:netxms-mobile-agent:$VERSION,g" android/agent/app/build.gradle
-sed -i -e "s,org.netxms:netxms-client:3.4-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/console/app/build.gradle
-sed -i -e "s,org.netxms:netxms-base:3.4-SNAPSHOT,org.netxms:netxms-base:$VERSION,g" android/console/app/build.gradle
+sed -i -e "s,org.netxms:netxms-client:[0-9].[0-9]-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/agent/app/build.gradle
+sed -i -e "s,org.netxms:netxms-mobile-agent:[0-9].[0-9]-SNAPSHOT,org.netxms:netxms-mobile-agent:$VERSION,g" android/agent/app/build.gradle
+sed -i -e "s,org.netxms:netxms-client:[0-9].[0-9]-SNAPSHOT,org.netxms:netxms-client:$VERSION,g" android/console/app/build.gradle
+sed -i -e "s,org.netxms:netxms-base:[0-9].[0-9]-SNAPSHOT,org.netxms:netxms-base:$VERSION,g" android/console/app/build.gradle
 
 if [ -x ./private/branding/build/prepare_release_build_hook.sh ]; then
 	./private/branding/build/prepare_release_build_hook.sh
