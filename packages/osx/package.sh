cfd4a36218d6bc12ab2446b2a13e575bfbb9a09b
Alex Kirhenshtein <alk@netxms.org>
2020-12-08 22:36:33 +0200

    JVM updated in macOS packages

--- packages/osx/package.sh
+++ packages/osx/package.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [ ! -r zulu11.39.15-ca-jre11.0.7-macosx_x64.zip ]; then
-   echo zulu11.39.15-ca-jre11.0.7-macosx_x64.zip is missing, download from 'https://www.azul.com/downloads/zulu-community/?&version=java-11-lts&os=macos&os-details=macOS&architecture=x86-64-bit&package=jre'
+if [ ! -r zulu15.28.51-ca-jre15.0.1-macosx_x64.zip ]; then
+   echo zulu15.28.51-ca-jre15.0.1-macosx_x64.zip is missing, download from 'https://www.azul.com/downloads/zulu-community/?&version=java-11-lts&os=macos&os-details=macOS&architecture=x86-64-bit&package=jre'
    exit 1
 fi
 
@@ -22,8 +22,8 @@ rm -rf dist/NetXMS\ Console.app/Contents/Eclipse/p2/org.eclipse.equinox.p2.*
 
 cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
 cd dist/NetXMS\ Console.app/Contents/
-unzip -x ../../../zulu11.39.15-ca-jre11.0.7-macosx_x64.zip >/dev/null
-mv zulu11.39.15-ca-jre11.0.7-macosx_x64 jre
+unzip -x ../../../zulu15.28.51-ca-jre15.0.1-macosx_x64.zip >/dev/null
+mv zulu15.28.51-ca-jre15.0.1-macosx_x64 jre
 cd - >/dev/null
 
 find dist/NetXMS\ Console.app -name \*.so -print0 | xargs -0 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist
@@ -43,4 +43,4 @@ codesign --options=runtime --deep --force -s "Developer ID Application: Raden So
 create-dmg --no-internet-enable --volname "NetXMS ${version}" --volicon dmg-icon.icns --window-size 500 320 --background dmg-background.png --icon "NetXMS Console.app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
 codesign -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' nxmc-${version}.dmg
 
-xcrun altool --notarize-app --primary-bundle-id "org.netxms.ui.eclipse.console.product" --username "alk@alk.lv" --password "@keychain:AC_PASSWORD" --asc-provider KFFD69X4L6 --file nxmc-${version}.dmg
+#xcrun altool --notarize-app --primary-bundle-id "org.netxms.ui.eclipse.console.product" --username "alk@alk.lv" --password "@keychain:AC_PASSWORD" --asc-provider KFFD69X4L6 --file nxmc-${version}.dmg
