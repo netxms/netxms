d7ddc0e751df4a66925e669f9724fad074c6c8fd
Victor Kirhenshtein <victor@netxms.org>
2021-01-12 17:50:55 +0200

    base version set to 3.8

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -29,7 +29,7 @@
  * Version constants 
  */
 #define NETXMS_VERSION_MAJOR        3
-#define NETXMS_VERSION_MINOR        7
+#define NETXMS_VERSION_MINOR        8
 
 #ifdef UNICODE
 #define IS_UNICODE_BUILD_STRING     _T(" (UNICODE)")
