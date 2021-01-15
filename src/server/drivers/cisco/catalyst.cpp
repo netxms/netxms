cf51b86e5ea798959e6e026e89610b6a1e9c708b
Victor Kirhenshtein <victor@netxms.org>
2021-01-15 13:31:33 +0200

    improved device detection in CATALYST-GENERIC driver (fixes NX-1081)

--- src/server/drivers/cisco/catalyst.cpp
+++ src/server/drivers/cisco/catalyst.cpp
@@ -1,7 +1,7 @@
 /* 
 ** NetXMS - Network Management System
 ** Driver for Cisco Catalyst switches
-** Copyright (C) 2003-2018 Victor Kirhenshtein
+** Copyright (C) 2003-2021 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
@@ -27,7 +27,7 @@
  */
 const TCHAR *CatalystDriver::getName()
 {
-	return _T("CATALYST-GENERIC");
+   return _T("CATALYST-GENERIC");
 }
 
 /**
@@ -37,7 +37,7 @@ const TCHAR *CatalystDriver::getName()
  */
 int CatalystDriver::isPotentialDevice(const TCHAR *oid)
 {
-	return (_tcsncmp(oid, _T(".1.3.6.1.4.1.9.1."), 17) == 0) ? 250 : 0;
+   return (_tcsncmp(oid, _T(".1.3.6.1.4.1.9.1."), 17) == 0) ? 250 : 0;
 }
 
 /**
@@ -48,8 +48,30 @@ int CatalystDriver::isPotentialDevice(const TCHAR *oid)
  */
 bool CatalystDriver::isDeviceSupported(SNMP_Transport *snmp, const TCHAR *oid)
 {
-	UINT32 value = 0;
-	return SnmpGetEx(snmp, _T(".1.3.6.1.4.1.9.5.1.2.14.0"), NULL, 0, &value, sizeof(UINT32), 0, NULL) == SNMP_ERR_SUCCESS;
+   uint32_t value = 0;
+   if (SnmpGetEx(snmp, _T(".1.3.6.1.4.1.9.5.1.2.14.0"), nullptr, 0, &value, sizeof(uint32_t), 0, nullptr) == SNMP_ERR_SUCCESS)
+      return true;
+
+   SNMP_PDU request(SNMP_GET_NEXT_REQUEST, SnmpNewRequestId(), snmp->getSnmpVersion());
+   SNMP_ObjectId rootOid = SNMP_ObjectId::parse(_T(".1.3.6.1.4.1.9.5.1.4.1.1.11"));
+   request.bindVariable(new SNMP_Variable(rootOid));
+   SNMP_PDU *response = nullptr;
+   if (snmp->doRequest(&request, &response, SnmpGetDefaultTimeout(), 3) == SNMP_ERR_SUCCESS)
+   {
+      bool success = false;
+      if (response->getNumVariables() > 0)
+      {
+         SNMP_Variable *var = response->getVariable(0);
+         success = (var->getType() != ASN_NO_SUCH_OBJECT) &&
+                   (var->getType() != ASN_NO_SUCH_INSTANCE) &&
+                   (var->getType() != ASN_END_OF_MIBVIEW) &&
+                   (var->getName().compare(rootOid) == OID_LONGER);
+      }
+      delete response;
+      return success;
+   }
+
+   return false;
 }
 
 /**
@@ -57,18 +79,17 @@ bool CatalystDriver::isDeviceSupported(SNMP_Transport *snmp, const TCHAR *oid)
  */
 static UINT32 HandlerPortList(SNMP_Variable *var, SNMP_Transport *transport, void *arg)
 {
-	InterfaceList *ifList = (InterfaceList *)arg;
-
-	InterfaceInfo *iface = ifList->findByIfIndex(var->getValueAsUInt());
-	if (iface != NULL)
-	{
+   InterfaceList *ifList = static_cast<InterfaceList*>(arg);
+   InterfaceInfo *iface = ifList->findByIfIndex(var->getValueAsUInt());
+   if (iface != nullptr)
+   {
 		size_t nameLen = var->getName().length();
 		
-		UINT32 moduleIndex = var->getName().getElement(nameLen - 2);
+		uint32_t moduleIndex = var->getName().getElement(nameLen - 2);
 		UINT32 oid[] = { 1, 3, 6, 1, 4, 1, 9, 5, 1, 3, 1, 1, 25, 0 };
 		oid[13] = moduleIndex;
-		UINT32 slot;
-		if (SnmpGetEx(transport, NULL, oid, 14, &slot, sizeof(UINT32), 0, NULL) != SNMP_ERR_SUCCESS)
+		uint32_t slot;
+		if (SnmpGetEx(transport, nullptr, oid, 14, &slot, sizeof(uint32_t), 0, nullptr) != SNMP_ERR_SUCCESS)
 			slot = moduleIndex;	// Assume slot # equal to module index if it cannot be read
 
 		iface->location.module = slot;
@@ -86,13 +107,13 @@ static UINT32 HandlerPortList(SNMP_Variable *var, SNMP_Transport *transport, voi
  */
 InterfaceList *CatalystDriver::getInterfaces(SNMP_Transport *snmp, NObject *node, DriverData *driverData, int useAliases, bool useIfXTable)
 {
-	// Get interface list from standard MIB
-	InterfaceList *ifList = CiscoDeviceDriver::getInterfaces(snmp, node, driverData, useAliases, useIfXTable);
-	if (ifList == NULL)
-		return NULL;
+   // Get interface list from standard MIB
+   InterfaceList *ifList = CiscoDeviceDriver::getInterfaces(snmp, node, driverData, useAliases, useIfXTable);
+   if (ifList == nullptr)
+      return nullptr;
 	
-	// Set slot and port number for physical interfaces
-	SnmpWalk(snmp, _T(".1.3.6.1.4.1.9.5.1.4.1.1.11"), HandlerPortList, ifList);
+   // Set slot and port number for physical interfaces
+   SnmpWalk(snmp, _T(".1.3.6.1.4.1.9.5.1.4.1.1.11"), HandlerPortList, ifList);
 
-	return ifList;
+   return ifList;
 }
