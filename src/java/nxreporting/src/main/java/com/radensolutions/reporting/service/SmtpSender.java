24c4b3fd304917174ba5ea7aae85b83731c99ae6
Victor Kirhenshtein <victor@netxms.org>
2021-01-26 19:25:11 +0200

    removed old reporting server code

--- src/java/nxreporting/src/main/java/com/radensolutions/reporting/service/SmtpSender.java
+++
@@ -1,9 +0,0 @@
-package com.radensolutions.reporting.service;
-
-import java.io.File;
-
-public interface SmtpSender {
-
-    void mail(String to, String subject, String body, String fileName, File file);
-
-}
