1e9677a14922e39a9ef02a2a51d1b704c34a237f
Victor Kirhenshtein <victor@netxms.org>
2021-02-07 21:20:35 +0200

    removed pom version updates from release preparation script

--- build/prepare_release_build.sh
+++ build/prepare_release_build.sh
@@ -15,9 +15,6 @@ fi
 
 set -e
 
-### Configuration
-POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml src/mobile-agent/java src/server/nxreportd/java/pom.xml tests/integration/pom.xml"
-
 ### Code
 cd build
 ./updatetag.pl
@@ -25,10 +22,6 @@ cd ..
 cp build/netxms-build-tag.properties src/java-common/netxms-base/src/main/resources/ 
 
 VERSION=`cat build/netxms-build-tag.properties | grep NETXMS_VERSION | cut -d = -f 2`
-
-for pom in $POM_FILES; do
-	mvn -f $pom versions:set -DnewVersion=$VERSION
-done
 
 ./build/update_plugin_versions.py . $VERSION
 
