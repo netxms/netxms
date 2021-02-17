b42e843a2531f5cc38bea23a9453203123e5580c
zev <zev@radensolutions.com>
2021-02-17 20:30:17 +0200

    Fixed graph refresh.

--- webui/webapp/PerfView/src/org/netxms/ui/eclipse/perfview/views/HistoricalGraphView.java
+++ webui/webapp/PerfView/src/org/netxms/ui/eclipse/perfview/views/HistoricalGraphView.java
@@ -396,8 +396,8 @@ public class HistoricalGraphView extends ViewPart implements GraphSettingsChange
          settings.setTimeFrom(new Date(System.currentTimeMillis() - settings.getTimeRangeMillis()));
          settings.setTimeTo(new Date(System.currentTimeMillis()));
       }
-
-      getDataFromServer();
+      
+      updateChart();
 
       // Automatic refresh
       actionAutoRefresh.setChecked(settings.isAutoRefresh());
@@ -579,7 +579,6 @@ public class HistoricalGraphView extends ViewPart implements GraphSettingsChange
          {
             showGraphPropertyPages(settings);
             configureGraphFromSettings(); //Always refresh graph (last action was cancel, but before was few apply actions)
-            refreshMenuSelection();
          }
       };
 
@@ -943,8 +942,6 @@ public class HistoricalGraphView extends ViewPart implements GraphSettingsChange
       settings.setTimeFrom(new Date(System.currentTimeMillis() - settings.getTimeRangeMillis()));
       settings.setTimeTo(new Date(System.currentTimeMillis()));
       getDataFromServer();
-      configureGraphFromSettings();
-      refreshMenuSelection();
    }
 
    /*
@@ -1093,7 +1090,6 @@ public class HistoricalGraphView extends ViewPart implements GraphSettingsChange
             }
          }.start();
       }
-      updateChart();
    }
 
    /**
