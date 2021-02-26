8c2a49a4c3b099b7cbed818c261bd5a54ff1035b
Victor Kirhenshtein <victor@netxms.org>
2021-02-26 11:01:32 +0200

    fixed possible deadlock or crash on server shutdown

--- src/server/core/jobmgr.cpp
+++ src/server/core/jobmgr.cpp
@@ -1,6 +1,6 @@
 /*
 ** NetXMS - Network Management System
-** Copyright (C) 2003-2020 Raden Solutions
+** Copyright (C) 2003-2021 Raden Solutions
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -34,8 +34,8 @@
 /**
  * Static data
  */
-static AbstractIndexWithDestructor<ServerJobQueue> s_jobQueues(Ownership::True);
 static AbstractIndex<ServerJobQueue> s_jobQueueIndex(Ownership::False);
+static AbstractIndexWithDestructor<ServerJobQueue> s_jobQueues(Ownership::True);
 static Mutex s_indexLock;
 
 /**
