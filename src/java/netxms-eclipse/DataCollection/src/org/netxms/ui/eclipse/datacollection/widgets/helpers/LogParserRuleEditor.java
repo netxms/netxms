b379468820e110b3d4357f26423bbf9aeba73ed9
zev <zev@radensolutions.com>
2021-04-08 15:34:26 +0300

    Fixed Log parser agent action save when field is empty

--- src/java/netxms-eclipse/DataCollection/src/org/netxms/ui/eclipse/datacollection/widgets/helpers/LogParserRuleEditor.java
+++ src/java/netxms-eclipse/DataCollection/src/org/netxms/ui/eclipse/datacollection/widgets/helpers/LogParserRuleEditor.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2013 Victor Kirhenshtein
+ * Copyright (C) 2003-2021 Victor Kirhenshtein
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -702,8 +702,7 @@ public class LogParserRuleEditor extends DashboardComposite
 
       if (editor.getParserType() == LogParserType.POLICY)
       {
-         if (!agentAction.getText().trim().isEmpty())
-            rule.setAgentAction(agentAction.getText().trim());
+         rule.setAgentAction(agentAction.getText().trim());
       }
       else
          rule.setAgentAction("");
