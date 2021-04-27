0a689d66e7836798d283ec3fe8ebbd8692ea1e53
Victor Kirhenshtein <victor@netxms.org>
2021-04-27 17:58:35 +0300

    default external process execution timeout in agent set to 30 seconds

--- src/agent/core/nxagentd.cpp
+++ src/agent/core/nxagentd.cpp
@@ -207,7 +207,7 @@ TCHAR g_szSNMPTrapListenAddress[MAX_PATH] = _T("*");
 UINT16 g_wListenPort = AGENT_LISTEN_PORT;
 TCHAR g_systemName[MAX_OBJECT_NAME] = _T("");
 ObjectArray<ServerInfo> g_serverList(8, 8, Ownership::True);
-UINT32 g_execTimeout = 2000;     // External process execution timeout in milliseconds
+UINT32 g_execTimeout = 30000;  // External process execution timeout in milliseconds
 UINT32 g_eppTimeout = 30;      // External parameter processor timeout in seconds
 UINT32 g_snmpTimeout = 0;
 UINT16 g_snmpTrapPort = 162;
