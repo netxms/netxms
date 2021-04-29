bea1e2729513d44a3be66b6cd1bf7a004adb69bf
Victor Kirhenshtein <victor@netxms.org>
2021-04-29 16:41:50 +0300

    minor improvements in object toolbar

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/ObjectsPerspective.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/ObjectsPerspective.java
@@ -22,6 +22,8 @@ import java.util.HashSet;
 import java.util.Set;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -29,9 +31,11 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.netxms.client.objects.AbstractObject;
 import org.netxms.nxmc.base.views.Perspective;
 import org.netxms.nxmc.base.views.PerspectiveConfiguration;
+import org.netxms.nxmc.localization.LocalizationHelper;
 import org.netxms.nxmc.modules.alarms.views.AlarmsView;
 import org.netxms.nxmc.modules.datacollection.views.LastValuesView;
 import org.netxms.nxmc.modules.datacollection.views.PerformanceView;
@@ -41,13 +45,17 @@ import org.netxms.nxmc.modules.objects.views.InterfacesView;
 import org.netxms.nxmc.modules.objects.views.ObjectOverviewView;
 import org.netxms.nxmc.modules.objects.views.SwitchForwardingDatabaseView;
 import org.netxms.nxmc.modules.objects.widgets.ObjectTree;
+import org.netxms.nxmc.resources.SharedIcons;
 import org.netxms.nxmc.tools.FontTools;
+import org.xnap.commons.i18n.I18n;
 
 /**
  * Object browser perspective
  */
 public abstract class ObjectsPerspective extends Perspective
 {
+   private static final I18n i18n = LocalizationHelper.getI18n(ObjectsPerspective.class);
+
    private SubtreeType subtreeType;
    private ObjectTree objectTree;
    private Composite headerArea;
@@ -126,7 +134,9 @@ public abstract class ObjectsPerspective extends Perspective
       objectName.setFont(FontTools.createTitleFont());
 
       Label separator = new Label(headerArea, SWT.SEPARATOR | SWT.VERTICAL);
-      separator.setLayoutData(new GridData(SWT.CENTER, SWT.FILL, false, true));
+      GridData gd = new GridData(SWT.CENTER, SWT.FILL, false, true);
+      gd.heightHint = 1;
+      separator.setLayoutData(gd);
 
       objectToolBar = new ToolBar(headerArea, SWT.FLAT | SWT.HORIZONTAL);
    }
@@ -156,9 +166,39 @@ public abstract class ObjectsPerspective extends Perspective
     *
     * @param object selected object
     */
-   private void updateObjectToolBar(AbstractObject object)
+   private void updateObjectToolBar(final AbstractObject object)
    {
+      for(ToolItem item : objectToolBar.getItems())
+         item.dispose();
 
+      addToolBarItem(i18n.tr("Properties"), SharedIcons.IMG_EDIT, new Runnable() {
+         @Override
+         public void run()
+         {
+            ObjectPropertiesManager.openObjectPropertiesDialog(object, getWindow().getShell());
+         }
+      });
+
+      addToolBarItem(i18n.tr("Delete"), SharedIcons.IMG_DELETE_OBJECT, new Runnable() {
+         @Override
+         public void run()
+         {
+         }
+      });
+   }
+
+   private void addToolBarItem(String name, Image icon, final Runnable handler)
+   {
+      ToolItem item = new ToolItem(objectToolBar, SWT.PUSH);
+      item.setImage(icon);
+      item.setToolTipText(name);
+      item.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e)
+         {
+            handler.run();
+         }
+      });
    }
 
    /**
