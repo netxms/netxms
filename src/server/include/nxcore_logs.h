b5a5385b138c47ff1ab0202e744f9c560eee8f7b
Victor Kirhenshtein <victor@netxms.org>
2021-08-24 16:55:19 +0300

    added log field type for ATM transaction code

--- src/server/include/nxcore_logs.h
+++ src/server/include/nxcore_logs.h
@@ -41,6 +41,9 @@
 #define LC_EVENT_ORIGIN    10
 #define LC_TEXT_DETAILS    11
 #define LC_JSON_DETAILS    12
+#define LC_STATUS          13
+#define LC_ACTION_CODE     14
+#define LC_ATM_TXN_CODE    15
 
 /**
  * Column filter types
