f09f9badf60a2aeb587ee8125a316837ec57c87f
Victor Kirhenshtein <victor@netxms.org>
2021-09-20 16:51:58 +0300

    added dbx support in thread capture scripts

--- tools/capture_nxagentd_threads.sh
+++ tools/capture_nxagentd_threads.sh
@@ -3,15 +3,63 @@ exec='nxagentd'
 if [ "x$1" != "x" ]; then
 	exec="$1"
 fi
-pid=`ps -ax | grep nxagentd | grep -v grep | grep -v capture_nxagentd_threads | awk '{ print $1; }'`
+
+if command -v gdb >/dev/null 2>&1; then
+	DEBUGGER=gdb
+else
+	if command -v dbx >/dev/null 2>&1; then
+		DEBUGGER=dbx
+	else
+		echo >&2 "gdb or dbx is required. Aborting."
+		exit 1
+	fi
+fi
+
+pid=`ps -ae | grep $exec | grep -v grep | grep -v capture_netxmsd_threads | grep -v tail | grep -v less | grep -v log/$exec | grep -v $exec-asan | awk '{ print $1; }'`
+
+if [ "x$pid" = "x" ]; then
+    echo "Unable to find $exec process. Aborting."
+    exit 1
+fi
+
+if [ `echo "$pid" | wc -l` -ne 1 ]; then
+    echo "Found several $exec processes. Aborting."
+    exit 1
+fi
+
 ts=`date +%Y%m%d-%H%M%S`
-cmdfile="/tmp/capture_nxagentd_threads.gdb"
-echo "set height 0" > $cmdfile
-echo "set logging file /tmp/nxagentd-threads.$pid.$ts" >> $cmdfile
-echo "set logging on" >> $cmdfile
-echo "attach $pid" >> $cmdfile
-echo "thread apply all bt" >> $cmdfile
-echo "detach" >> $cmdfile
-echo "quit" >> $cmdfile
-gdb --batch-silent --command=$cmdfile "$exec"
-rm $cmdfile
+outputfile="/tmp/$exec-threads.$pid.$ts"
+echo "Saving output to $outputfile"
+
+cmdfile="/tmp/capture-$exec-threads.$DEBUGGER"
+
+if [ "$DEBUGGER" = "gdb" ]; then
+	echo "set height 0" > $cmdfile
+	echo "set logging file $outputfile" >> $cmdfile
+	echo "set logging on" >> $cmdfile
+	echo "attach $pid" >> $cmdfile
+	echo "thread apply all bt" >> $cmdfile
+	echo "detach" >> $cmdfile
+	echo "quit" >> $cmdfile
+	gdb --batch-silent --command=$cmdfile "$exec"
+fi
+
+if [ "$DEBUGGER" = "dbx" ]; then
+	echo "thread" > $cmdfile
+	echo "detach" >> $cmdfile
+	dbx -a $pid -c $cmdfile | awk '$1 ~ /\$t/ { gsub(/\$t/,"", $1); gsub(/\>/, "", $1); print $1 }' >$outputfile.ids 2>/dev/nul
+
+	rm -f $cmdfile
+	for i in `cat $outputfile.ids`; do
+		echo "print \"------------- tid $i -----------------\"" >> $cmdfile
+		echo "thread current $i" >> $cmdfile
+		echo "where" >> $cmdfile
+		i=$(($i+1))
+	done
+	echo "detach" >> $cmdfile
+	dbx -a $pid -c $cmdfile >$outputfile 2>/dev/nul
+
+	rm -f $outputfile.ids
+fi
+
+rm -f $cmdfile
