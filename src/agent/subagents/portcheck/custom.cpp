26704b73cec5c709fe2d845577fd17d330422328
Victor Kirhenshtein <victor@netxms.org>
2021-11-27 10:40:14 +0200

    portcheck subagent refactoring

--- src/agent/subagents/portcheck/custom.cpp
+++ src/agent/subagents/portcheck/custom.cpp
@@ -1,40 +1,51 @@
-/* $Id$ */
+/*
+** NetXMS - Network Management System
+** Copyright (C) 2003-2021 Raden Solutions
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: custom.cpp
+**
+**/
 
-#include <nms_common.h>
-#include <nms_agent.h>
-
-#include "main.h"
-#include "net.h"
+#include "portcheck.h"
 
 /**
  * Check custom service - parameter handler
  */
 LONG H_CheckCustom(const TCHAR *param, const TCHAR *arg, TCHAR *value, AbstractCommSession *session)
 {
-	LONG nRet = SYSINFO_RC_SUCCESS;
-
 	char szHost[1024];
 	TCHAR szPort[1024];
 	TCHAR szTimeout[64];
-	unsigned short nPort;
 
    AgentGetParameterArgA(param, 1, szHost, sizeof(szHost));
    AgentGetParameterArg(param, 2, szPort, sizeof(szPort));
    AgentGetParameterArg(param, 3, szTimeout, sizeof(szTimeout));
 
 	if (szHost[0] == 0 || szPort[0] == 0)
-	{
 		return SYSINFO_RC_ERROR;
-	}
 
-	nPort = (unsigned short)_tcstol(szPort, NULL, 10);
+	uint16_t nPort = static_cast<uint16_t>(_tcstol(szPort, nullptr, 10));
 	if (nPort == 0)
-	{
 		return SYSINFO_RC_ERROR;
-	}
 
-	UINT32 dwTimeout = _tcstoul(szTimeout, NULL, 0);
-   INT64 start = GetCurrentTimeMs();
+   LONG nRet = SYSINFO_RC_SUCCESS;
+
+	uint32_t dwTimeout = _tcstoul(szTimeout, nullptr, 0);
+   int64_t start = GetCurrentTimeMs();
    int result = CheckCustom(szHost, InetAddress::INVALID, nPort, dwTimeout);
    if (*arg == 'R')
    {
@@ -58,9 +69,7 @@ LONG H_CheckCustom(const TCHAR *param, const TCHAR *arg, TCHAR *value, AbstractC
 int CheckCustom(char *hostname, const InetAddress& addr, short nPort, UINT32 dwTimeout)
 {
 	int nRet;
-	SOCKET nSd;
-
-	nSd = NetConnectTCP(hostname, addr, nPort, dwTimeout);
+	SOCKET nSd = NetConnectTCP(hostname, addr, nPort, dwTimeout);
 	if (nSd != INVALID_SOCKET)
 	{
 		nRet = PC_ERR_NONE;
---
+++ src/agent/subagents/portcheck/portcheck.h
@@ -0,0 +1,110 @@
+/*
+** NetXMS - Network Management System
+** Copyright (C) 2003-2021 Raden Solutions
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: portcheck.h
+**
+**/
+
+#ifndef __portcheck_h__
+#define __portcheck_h__
+
+#include <nms_common.h>
+#include <nms_util.h>
+#include <nms_agent.h>
+
+#define SUBAGENT_DEBUG_TAG _T("portcheck")
+
+enum
+{
+   PROTOCOL_UDP,
+   PROTOCOL_TCP
+};
+
+SOCKET NetConnectTCP(const char *hostname, const InetAddress& addr, uint16_t port, uint32_t timeout);
+
+/**
+ * Write to the socket
+ */
+static inline bool NetWrite(SOCKET hSocket, const void *data, size_t size)
+{
+   return SendEx(hSocket, data, size, 0, nullptr) == static_cast<ssize_t>(size);
+}
+
+/**
+ * Read from socket
+ */
+static inline ssize_t NetRead(SOCKET hSocket, void *buffer, size_t size)
+{
+#ifdef _WIN32
+   return recv(hSocket, static_cast<char*>(buffer), static_cast<int>(size), 0);
+#else
+   ssize_t rc;
+   do
+   {
+      rc = recv(hSocket, static_cast<char*>(buffer), size, 0);
+   } while((rc == -1) && (errno == EINTR));
+   return rc;
+#endif
+}
+
+/**
+ * Close socket gracefully
+ */
+static inline void NetClose(SOCKET nSocket)
+{
+   shutdown(nSocket, SHUT_RDWR);
+   closesocket(nSocket);
+}
+
+/**
+ * Service check return codes
+ */
+enum
+{
+	PC_ERR_NONE,
+	PC_ERR_BAD_PARAMS,
+	PC_ERR_CONNECT,
+	PC_ERR_HANDSHAKE,
+	PC_ERR_INTERNAL
+};
+
+/**
+ * Flags
+ */
+#define SCF_NEGATIVE_TIME_ON_ERROR  0x0001
+
+LONG H_CheckPOP3(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckPOP3(char *, const InetAddress&, short, char *, char *, UINT32);
+LONG H_CheckSSH(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckSSH(char *, const InetAddress&, short, char *, char *, UINT32);
+LONG H_CheckSMTP(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckSMTP(char *, const InetAddress&, short, char *, UINT32);
+LONG H_CheckHTTP(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckHTTP(char *, const InetAddress&, short, char *, char *, char *, UINT32);
+int CheckHTTPS(char *, const InetAddress&, short, char *, char *, char *, UINT32);
+LONG H_CheckCustom(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckCustom(char *, const InetAddress&, short, UINT32);
+LONG H_CheckTelnet(const TCHAR *, const TCHAR *, TCHAR *, AbstractCommSession *);
+int CheckTelnet(char *, const InetAddress&, short, char *, char *, UINT32);
+
+extern char g_szDomainName[];
+extern char g_hostName[];
+extern char g_szFailedDir[];
+extern uint32_t g_serviceCheckFlags;
+
+#endif // __portcheck__h__
