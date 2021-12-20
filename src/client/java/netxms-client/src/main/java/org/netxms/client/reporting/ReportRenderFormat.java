714898f02baf118a2e7716d1c1a15d65547e7651
Victor Kirhenshtein <victor@netxms.org>
2021-12-20 21:42:02 +0200

    reporting server uses xlsx instead of xls

--- src/client/java/netxms-client/src/main/java/org/netxms/client/reporting/ReportRenderFormat.java
+++ src/client/java/netxms-client/src/main/java/org/netxms/client/reporting/ReportRenderFormat.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2014 Raden Solutions
+ * Copyright (C) 2003-2021 Raden Solutions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ import java.util.Map;
  */
 public enum ReportRenderFormat
 {
-	NONE(0, "none"), PDF(1, "pdf"), XLS(2, "xls");
+   NONE(0, "none"), PDF(1, "pdf"), XLSX(2, "xlsx");
 
 	private final int code;
 	private final String extension;
