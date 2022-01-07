4b361ce2ef201e7894a56a586db9441266a287a8
Filipp Sudanov <filipp.sudanov@radensolutions.com>
2022-01-07 18:39:18 +0200

    updated instance discovery filter hint text

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/datacollection/propertypages/InstanceDiscovery.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/datacollection/propertypages/InstanceDiscovery.java
@@ -174,7 +174,7 @@ public class InstanceDiscovery extends AbstractDCIPropertyPage
 			public Control createControl(Composite parent, int style)
 			{
 				return new ScriptEditor(parent, style, SWT.H_SCROLL | SWT.V_SCROLL, false, 
-				      "Variables:\r\n\t$1\t\t\tInstance to test;\r\n\t$2\t\t\tInstance data (value for given OID for SNMP Walk - OIDs method, secondary value for Script method);\r\n\t$dci\t\tthis DCI object;\r\n\t$isCluster\ttrue if DCI is on cluster;\r\n\t$node\t\tcurrent node object (null if DCI is not on the node);\r\n\t$object\t\tcurrent object.\r\n\r\nReturn value:\r\n\ttrue/false to accept or reject instance without additional changes or\r\n\tarray of two or three elements to modify instance:\r\n\t\t1st element - true/false to indicate acceptance;\r\n\t\t2nd element - new instance name;\r\n\t\t3rd element - new instance display name.");
+				      "Variables:\r\n\t$1\t\t\tInstance to test;\r\n\t$2\t\t\tInstance data (value for given OID for SNMP Walk - Values method, secondary value for Script method);\r\n\t$dci\t\tthis DCI object;\r\n\t$isCluster\ttrue if DCI is on cluster;\r\n\t$node\t\tcurrent node object (null if DCI is not on the node);\r\n\t$object\t\tcurrent object.\r\n\r\nReturn value:\r\n\ttrue/false to accept or reject instance without additional changes or\r\n\tarray of one, two or three elements to accept instance while modifying it:\r\n\t\t1st element - new instance name;\r\n\t\t2nd element - new instance display name;\r\n\t\t3rd element - object related to this DCI.");
 			}
       };
       filterScript = (ScriptEditor)WidgetHelper.createLabeledControl(dialogArea, SWT.BORDER,
