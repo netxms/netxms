d8eea0aa74466b2ed5649ed8f483f5aea0f3d749
zev <zev@radensolutions.com>
2022-01-28 18:21:56 +0200

    Fixed custom attribute inheritance remove population

--- src/server/libnxsrv/nobject.cpp
+++ src/server/libnxsrv/nobject.cpp
@@ -625,7 +625,7 @@ void NObject::deletePopulatedCustomAttribute(const TCHAR *name)
    bool populate = false;
    if (ca != nullptr)
    {
-      if (!ca->isRedefined())
+      if (!ca->isRedefined() && ca->sourceObject != 0) //source check required if node is multiple times under parent container
       {
          m_customAttributes.remove(name);
          populate = true;
