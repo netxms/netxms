44b0d719c0d9c0985cb5b63fe982562b4402f023
Alex Kirhenshtein <alk@netxms.org>
2022-02-10 18:18:45 +0200

    macOS packaging script for nxmc fixed to support arm64

--- packages/osx/package.sh
+++ packages/osx/package.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+# vim: ts=3 sw=3 expandtab
 
 set -e
 
-if [ ! -r zulu15.29.15-ca-jre15.0.2-macosx_x64.zip ]; then
-   echo zulu15.29.15-ca-jre15.0.2-macosx_x64.zip is missing, download from 'https://www.azul.com/downloads/zulu-community/?&version=java-11-lts&os=macos&os-details=macOS&architecture=x86-64-bit&package=jre'
-   exit 1
+if [ `uname -p` = 'arm' ]; then
+   arch=aarch64
+   jre_basename=zulu11.54.23-ca-jre11.0.14-macosx_aarch64
+else
+   arch=x86_64
+   jre_basename=zulu11.54.23-ca-jre11.0.14-macosx_x64
+fi
+
+if [ ! -r "${jre_basename}.zip" ]; then
+   curl -o "${jre_basename}.zip" "https://cdn.azul.com/zulu/bin/${jre_basename}.zip"
 fi
 
 if [ -z $1 ]; then
@@ -16,14 +24,14 @@ fi
 echo Version: $version
 
 mkdir -p dist
-rm -rf dist/* nxmc-${version}.dmg
-cp -R ../../src/java/netxms-eclipse/Product/target/products/org.netxms.ui.eclipse.console.product/macosx/cocoa/x86_64/Eclipse.app dist/NetXMS\ Console.app
+rm -rf dist/* nxmc-${version}-${arch}.dmg
+cp -R ../../src/java/netxms-eclipse/Product/target/products/org.netxms.ui.eclipse.console.product/macosx/cocoa/${arch}/Eclipse.app dist/NetXMS\ Console.app
 rm -rf dist/NetXMS\ Console.app/Contents/Eclipse/p2/org.eclipse.equinox.p2.*
 
 cat Info.plist | sed "s,@version@,${version},g" > "dist/NetXMS Console.app/Contents/Info.plist"
 cd dist/NetXMS\ Console.app/Contents/
-unzip -x ../../../zulu15.29.15-ca-jre15.0.2-macosx_x64.zip >/dev/null
-mv zulu15.29.15-ca-jre15.0.2-macosx_x64 jre
+unzip -x ../../../${jre_basename}.zip >/dev/null
+mv "${jre_basename}" jre
 cd - >/dev/null
 
 find dist/NetXMS\ Console.app -name \*.so -print0 | xargs -0 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist
@@ -34,15 +42,15 @@ cd dist/NetXMS\ Console.app/Contents/jre
 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements ../../../../entitlements.plist `find . -type f | xargs -n 1 file | grep Mach-O | cut -d: -f1`
 cd -
 
-7z x dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.x86_64_*.jar \*.jnilib
+7zz x dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.${arch}_*.jar \*.jnilib
 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist ./libswt-*
-7z a dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.x86_64_*.jar \*.jnilib
+7zz a dist/NetXMS\ Console.app/Contents/Eclipse/plugins/org.eclipse.swt.cocoa.macosx.${arch}_*.jar \*.jnilib
 rm -f *.jnilib
 codesign --options=runtime --deep --force -s "Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)" -v --entitlements entitlements.plist dist/NetXMS\ Console.app
 
-mv dist/NetXMS\ Console.app dist/NetXMS\ Console\ \($version\).app
+mv dist/NetXMS\ Console.app dist/NetXMS\ Console\ \(${version}\).app
 
-create-dmg --no-internet-enable --volname "NetXMS ${version}" --volicon dmg-icon.icns --window-size 500 320 --background dmg-background.png --icon "NetXMS Console ($version).app" 114 132 --app-drop-link 399 132 nxmc-${version}.dmg dist
-codesign -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' nxmc-${version}.dmg
+create-dmg --no-internet-enable --volname "NetXMS ${version}" --volicon dmg-icon.icns --window-size 500 320 --background dmg-background.png --icon "NetXMS Console (${version}).app" 114 132 --app-drop-link 399 132 nxmc-${version}-${arch}.dmg dist
+codesign -s 'Developer ID Application: Raden Solutions, SIA (KFFD69X4L6)' nxmc-${version}-${arch}.dmg
 
-#xcrun altool --notarize-app --primary-bundle-id "org.netxms.ui.eclipse.console.product" --username "alk@alk.lv" --password "@keychain:AC_PASSWORD" --asc-provider KFFD69X4L6 --file nxmc-${version}.dmg
+#xcrun altool --notarize-app --primary-bundle-id "org.netxms.ui.eclipse.console.product" --username "alk@alk.lv" --password "@keychain:AC_PASSWORD" --asc-provider KFFD69X4L6 --file nxmc-${version}-${arch}.dmg
