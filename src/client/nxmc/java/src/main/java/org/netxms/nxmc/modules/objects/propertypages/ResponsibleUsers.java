4cc05e746d7a179e9fbef986931f2a4b9192abd8
Victor Kirhenshtein <victor@netxms.org>
2022-04-05 11:11:22 +0300

    tags for responsible users can only be selected from list

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/propertypages/ResponsibleUsers.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/propertypages/ResponsibleUsers.java
@@ -19,6 +19,7 @@
 package org.netxms.nxmc.modules.objects.propertypages;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -26,6 +27,7 @@ import java.util.Map;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
 import org.eclipse.jface.viewers.ICellModifier;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -33,7 +35,6 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.window.Window;
@@ -49,7 +50,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Item;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Text;
 import org.netxms.client.NXCObjectModificationData;
 import org.netxms.client.NXCSession;
 import org.netxms.client.objects.AbstractObject;
@@ -123,18 +123,20 @@ public class ResponsibleUsers extends ObjectPropertyPage
       column.setWidth(300);
       column = new TableColumn(viewer.getTable(), SWT.NONE);
       column.setText(i18n.tr("Tag"));
-      column.setWidth(80);
+      column.setWidth(120);
+
+      final List<String> tags = new ArrayList<String>(session.getResponsibleUserTags());
+      tags.add("");
+      tags.sort(new Comparator<String>() {
+         @Override
+         public int compare(String s1, String s2)
+         {
+            return s1.compareToIgnoreCase(s2);
+         }
+      });
 
       viewer.setColumnProperties(new String[] { "name", "tag" });
-      viewer.setCellEditors(new CellEditor[] { null, new TextCellEditor(viewer.getTable()) {
-         @Override
-         protected Control createControl(Composite parent)
-         {
-            Control c = super.createControl(parent);
-            ((Text)c).setTextLimit(31);
-            return c;
-         }
-      } });
+      viewer.setCellEditors(new CellEditor[] { null, new ComboBoxCellEditor(viewer.getTable(), tags.toArray(new String[tags.size()]), SWT.READ_ONLY | SWT.DROP_DOWN) });
       viewer.setCellModifier(new ICellModifier() {
          @Override
          public void modify(Object element, String property, Object value)
@@ -143,7 +145,7 @@ public class ResponsibleUsers extends ObjectPropertyPage
                element = ((Item)element).getData();
             if (property.equals("tag"))
             {
-               ((ResponsibleUserInfo)element).tag = (String)value;
+               ((ResponsibleUserInfo)element).tag = tags.get((Integer)value);
                viewer.update(element, new String[] { property });
             }
          }
@@ -151,13 +153,13 @@ public class ResponsibleUsers extends ObjectPropertyPage
          @Override
          public Object getValue(Object element, String property)
          {
-            return ((ResponsibleUserInfo)element).tag;
+            return tags.indexOf(((ResponsibleUserInfo)element).tag);
          }
 
          @Override
          public boolean canModify(Object element, String property)
          {
-            return property.equals("level");
+            return property.equals("tag");
          }
       });
 
@@ -169,7 +171,7 @@ public class ResponsibleUsers extends ObjectPropertyPage
       gd.horizontalAlignment = SWT.FILL;
       gd.verticalAlignment = SWT.FILL;
       viewer.getControl().setLayoutData(gd);
-      
+
       Composite buttons = new Composite(dialogArea, SWT.NONE);
       FillLayout buttonsLayout = new FillLayout();
       buttonsLayout.spacing = WidgetHelper.INNER_SPACING;
@@ -178,7 +180,7 @@ public class ResponsibleUsers extends ObjectPropertyPage
       gd.horizontalAlignment = SWT.RIGHT;
       gd.widthHint = 184;
       buttons.setLayoutData(gd);
-      
+
       final Button addButton = new Button(buttons, SWT.PUSH);
       addButton.setText(i18n.tr("&Add..."));
       addButton.addSelectionListener(new SelectionListener() {
