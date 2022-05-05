624f7b1b4c4094ca70360890f44b99bf19403ccb
Victor Kirhenshtein <victor@netxms.org>
2022-05-05 14:20:07 +0300

    fixed make dist

--- include/doxygen.h
+++
@@ -1,9 +0,0 @@
-/**
- * @mainpage
- * Text for main page
- *
- * \defgroup server Core Server
- * \defgroup agent Core Agent
- * \defgroup SNMP SNMP Client Library
- * \defgroup NXSL NetXMS Scripting Language Library
- */
