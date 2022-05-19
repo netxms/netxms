2fc8fcfca5e8b4fdc4db22c8c655a80d0fb7ffc8
Victor Kirhenshtein <victor@netxms.org>
2022-05-19 17:37:07 +0300

    improved build script

--- build/build_java_components.sh
+++ build/build_java_components.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# Usage:
+#    build_java_components.sh [-no-revert-version] [all]
+#
 
 set -e
 
@@ -9,7 +13,9 @@ cp build/netxms-build-tag.properties src/java-common/netxms-base/src/main/resour
 
 VERSION=`cat build/netxms-build-tag.properties | grep NETXMS_VERSION | cut -d = -f 2`
 
-if [ "x$1" != "x-no-revert-version" ]; then
+if [ "x$1" = "x-no-revert-version" ]; then
+   shift
+else
    trap '
       mvn -f src/pom.xml versions:revert -DprocessAllModules=true
       mvn -f src/client/nxmc/java/pom.xml versions:revert
@@ -17,18 +23,21 @@ if [ "x$1" != "x-no-revert-version" ]; then
 fi
 
 mvn -f src/pom.xml versions:set -DnewVersion=$VERSION -DprocessAllModules=true
-mvn -f src/client/nxmc/java/pom.xml versions:set -DnewVersion=$VERSION
-
 mvn -f src/pom.xml install -Dmaven.test.skip=true
 
-if [ "$1" = "all" ]; then   
-   WORKIND_DIR='src/client/nxmc/java'
-   mvn -f $WORKIND_DIR -Dmaven.test.skip=true -Pdesktop -Pstandalone clean package 
-   cp $WORKIND_DIR/target/nxmc-${VERSION}*-standalone.jar $WORKIND_DIR/
-   mvn -f $WORKIND_DIR -Dmaven.test.skip=true -Dnetxms.build.disablePlatformProfile=true -Pweb clean package
-   cp $WORKIND_DIR/target/nxmc-${VERSION}*.war $WORKIND_DIR/
-   #build nxshell
-   mvn -f src/client/nxshell/java -Dmaven.test.skip=true -Pstandalone clean package 
-else
-   mvn -f src/client/nxmc/java/pom.xml install -Pdesktop
+OS=`uname -s`
+if [ "$OS" = "Linux" -o "$OS" = "Darwin" ]; then
+   mvn -f src/client/nxmc/java/pom.xml versions:set -DnewVersion=$VERSION
+
+   if [ "x$1" = "xall" ]; then
+      WORKIND_DIR='src/client/nxmc/java'
+      mvn -f $WORKIND_DIR -Dmaven.test.skip=true -Pdesktop -Pstandalone clean package
+      cp $WORKIND_DIR/target/nxmc-${VERSION}*-standalone.jar $WORKIND_DIR/
+      mvn -f $WORKIND_DIR -Dmaven.test.skip=true -Dnetxms.build.disablePlatformProfile=true -Pweb clean package
+      cp $WORKIND_DIR/target/nxmc-${VERSION}*.war $WORKIND_DIR/
+      #build nxshell
+      mvn -f src/client/nxshell/java -Dmaven.test.skip=true -Pstandalone clean package
+   else
+      mvn -f src/client/nxmc/java/pom.xml install -Pdesktop
+   fi
 fi
