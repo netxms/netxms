34bf08110fbb471778cc9b85771ce11d49cc1a68
Victor Kirhenshtein <victor@netxms.org>
2022-05-23 14:59:06 +0300

    fixed bug in NXCSession.syncUserDatabase (fixes NX-2262)

--- src/client/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
+++ src/client/java/netxms-client/src/main/java/org/netxms/client/NXCSession.java
@@ -1583,10 +1583,10 @@ public class NXCSession
             long startTime = System.currentTimeMillis();
             try
             {
-               if (syncObjects.tryAcquire(actualTimeout, TimeUnit.MILLISECONDS))
+               if (syncObject.tryAcquire(actualTimeout, TimeUnit.MILLISECONDS))
                {
                   success = true;
-                  syncObjects.release();
+                  syncObject.release();
                   break;
                }
             }
