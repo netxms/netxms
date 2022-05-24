3a2bd060f82c4ec475a5434a08e35bfc33eda009
Victor Kirhenshtein <victor@netxms.org>
2022-05-24 17:31:56 +0300

    base version switched to 4.2; removed outdated tools; fixed bugs in base version update script

--- build/update_base_version.sh
+++ build/update_base_version.sh
@@ -14,13 +14,11 @@ fi
 
 set -e
 
-### Configuration
-POM_FILES="src/java-common/netxms-base/pom.xml src/libnxjava/java/pom.xml src/client/java/netxms-client/pom.xml src/client/nxapisrv/java/pom.xml src/client/nxshell/java/pom.xml src/client/nxtcpproxy/pom.xml src/agent/subagents/bind9/pom.xml src/agent/subagents/java/java/pom.xml src/agent/subagents/jmx/pom.xml src/agent/subagents/ubntlw/pom.xml src/mobile-agent/java src/java/nxreporting/pom.xml"
+mvn -f src/pom.xml versions:set -DnewVersion=$VERSION-SNAPSHOT -DprocessAllModules=true
+mvn -f src/client/nxmc/java/pom.xml versions:set -DnewVersion=$VERSION-SNAPSHOT
 
-### Code
-for pom in $POM_FILES; do
-	mvn -f $pom versions:set-property -Dproperty=revision -DnewVersion=$VERSION-SNAPSHOT
-done
+mvn -f src/pom.xml versions:commit -DnewVersion=$VERSION-SNAPSHOT -DprocessAllModules=true
+mvn -f src/client/nxmc/java/pom.xml versions:commit -DnewVersion=$VERSION-SNAPSHOT
 
 ./build/update_plugin_versions.py . $VERSION.0
 
@@ -30,7 +28,9 @@ mv ./plugin.xml src/java/netxms-eclipse/Core/plugin.xml
 cat src/java/netxms-eclipse/Product/nxmc.product | sed -E "s,^Version [0-9.]+\$,Version $VERSION.0," > nxmc.product
 mv ./nxmc.product src/java/netxms-eclipse/Product/nxmc.product
 
-sed -i -e "s,org.netxms:netxms-client:[0-9.]+-SNAPSHOT,org.netxms:netxms-client:$VERSION-SNAPSHOT,g" android/agent/app/build.gradle
-sed -i -e "s,org.netxms:netxms-mobile-agent:[0-9.]+-SNAPSHOT,org.netxms:netxms-mobile-agent:$VERSION-SNAPSHOT,g" android/agent/app/build.gradle
-sed -i -e "s,org.netxms:netxms-client:[0-9.]+-SNAPSHOT,org.netxms:netxms-client:$VERSION-SNAPSHOT,g" android/console/app/build.gradle
-sed -i -e "s,org.netxms:netxms-base:[0-9.]+-SNAPSHOT,org.netxms:netxms-base:$VERSION-SNAPSHOT,g" android/console/app/build.gradle
+sed -i -E "s,org.netxms:netxms-base:[0-9.]+-SNAPSHOT,org.netxms:netxms-base:$VERSION-SNAPSHOT,g" android/agent/app/build.gradle
+sed -i -E "s,org.netxms:netxms-mobile-agent:[0-9.]+-SNAPSHOT,org.netxms:netxms-mobile-agent:$VERSION-SNAPSHOT,g" android/agent/app/build.gradle
+sed -i -E "s,org.netxms:netxms-client:[0-9.]+-SNAPSHOT,org.netxms:netxms-client:$VERSION-SNAPSHOT,g" android/console/app/build.gradle
+sed -i -E "s,org.netxms:netxms-base:[0-9.]+-SNAPSHOT,org.netxms:netxms-base:$VERSION-SNAPSHOT,g" android/console/app/build.gradle
+
+sed -i -E "s,NETXMS_VERSION=[0-9.]+-SNAPSHOT,NETXMS_VERSION=$VERSION-SNAPSHOT,g" src/java-common/netxms-base/src/main/resources/netxms-version.properties
