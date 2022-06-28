f0fbedafea96b10c9c93047cfb9f541d8ab351a8
zev <zev@radensolutions.com>
2022-06-28 12:40:44 +0300

    Notification channel usability changes moved to new console

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/serverconfig/dialogs/NotificationChannelPropertiesDialog.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/serverconfig/dialogs/NotificationChannelPropertiesDialog.java
@@ -22,6 +22,7 @@ import java.util.Collections;
 import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -35,21 +36,24 @@ import org.netxms.client.NotificationChannel;
 import org.netxms.nxmc.Registry;
 import org.netxms.nxmc.base.jobs.Job;
 import org.netxms.nxmc.base.widgets.LabeledText;
+import org.netxms.nxmc.localization.LocalizationHelper;
 import org.netxms.nxmc.tools.MessageDialogHelper;
 import org.netxms.nxmc.tools.WidgetHelper;
+import org.xnap.commons.i18n.I18n;
 
 /**
  * Notification channel dialog
  */
 public class NotificationChannelPropertiesDialog extends Dialog
 {
+   private static final I18n i18n = LocalizationHelper.getI18n(NotificationChannelPropertiesDialog.class);
+
    private NotificationChannel nc;
    private LabeledText textName;
    private LabeledText textDescription;
    private LabeledText textConfiguraiton;
    private Combo comboDriverName;
-   private boolean isNameChangeds;
-   private String newName;
+   private boolean customName;
 
    /**
     * Create new notification channel properties dialog.
@@ -70,37 +74,61 @@ public class NotificationChannelPropertiesDialog extends Dialog
    protected Control createDialogArea(Composite parent)
    {
       Composite dialogArea = (Composite)super.createDialogArea(parent);
-      
+
       GridLayout layout = new GridLayout();
       layout.marginWidth = WidgetHelper.DIALOG_WIDTH_MARGIN;
       layout.marginHeight = WidgetHelper.DIALOG_HEIGHT_MARGIN;
       layout.verticalSpacing = WidgetHelper.DIALOG_SPACING;
       dialogArea.setLayout(layout);
-      
-      textName = new LabeledText(dialogArea, SWT.NONE);
-      textName.setLabel("Name");
-      textName.getTextControl().setTextLimit(63);
+
       GridData gd = new GridData();
+      gd.horizontalAlignment = SWT.FILL;
+      gd.grabExcessHorizontalSpace = true;
+      comboDriverName = WidgetHelper.createLabeledCombo(dialogArea, SWT.READ_ONLY, i18n.tr("Driver"), gd);
+      comboDriverName.addModifyListener(listener -> {
+         final int selectionIndex = comboDriverName.getSelectionIndex();
+         if (selectionIndex != -1 && !customName)
+         {
+            textName.setText(comboDriverName.getItem(selectionIndex));
+            textName.getTextControl().selectAll();
+         }
+      });
+
+      textName = new LabeledText(dialogArea, SWT.NONE);
+      textName.setLabel(i18n.tr("Name"));
+      textName.getTextControl().setTextLimit(63);
+      gd = new GridData();
       gd.horizontalAlignment = SWT.FILL;
       gd.grabExcessHorizontalSpace = true;
       gd.widthHint = 300;
       textName.setLayoutData(gd);
-      
+      textName.getTextControl().addModifyListener(listener -> {
+         final String name = textName.getText();
+
+         final int selectionIndex = comboDriverName.getSelectionIndex();
+         if (selectionIndex != -1)
+         {
+            final String driver = comboDriverName.getItem(selectionIndex);
+            customName = !name.equals(driver);
+         }
+
+         Control button = getButton(IDialogConstants.OK_ID);
+         if (button != null)
+         {
+            button.setEnabled(!name.trim().isBlank());
+         }
+      });
+
       textDescription = new LabeledText(dialogArea, SWT.NONE);
-      textDescription.setLabel("Description");
+      textDescription.setLabel(i18n.tr("Description"));
       textDescription.getTextControl().setTextLimit(255);
       gd = new GridData();
       gd.horizontalAlignment = SWT.FILL;
       gd.grabExcessHorizontalSpace = true;
       textDescription.setLayoutData(gd);
-
-      gd = new GridData();
-      gd.horizontalAlignment = SWT.FILL;
-      gd.grabExcessHorizontalSpace = true;
-      comboDriverName = WidgetHelper.createLabeledCombo(dialogArea, SWT.READ_ONLY, "Driver", gd);
-
+      
       textConfiguraiton = new LabeledText(dialogArea, SWT.NONE, SWT.MULTI | SWT.BORDER);
-      textConfiguraiton.setLabel("Driver configuration");
+      textConfiguraiton.setLabel(i18n.tr("Driver configuration"));
       textConfiguraiton.getTextControl().setFont(JFaceResources.getTextFont());
       gd = new GridData();
       gd.horizontalAlignment = SWT.FILL;
@@ -117,7 +145,7 @@ public class NotificationChannelPropertiesDialog extends Dialog
       }
       
       final NXCSession session = Registry.getSession();
-      new Job("Get driver names", null) {
+      new Job(i18n.tr("Get driver names"), null) {
          @Override
          protected void run(IProgressMonitor monitor) throws Exception
          {
@@ -135,7 +163,7 @@ public class NotificationChannelPropertiesDialog extends Dialog
          @Override
          protected String getErrorMessage()
          {
-            return "Cannot get driver names";
+            return i18n.tr("Cannot get driver names");
          }
       }.start(); 
       
@@ -144,11 +172,19 @@ public class NotificationChannelPropertiesDialog extends Dialog
 
    private void updateUI(List<String> ncList)
    {
-      for(int i = 0; i < ncList.size(); i++)
-      {
-         comboDriverName.add(ncList.get(i));
-         if(nc != null && ncList.get(i).equals(nc.getDriverName()))
-            comboDriverName.select(i);
+      int index = 0;
+      if (!ncList.isEmpty()) {
+         for(int i = 0; i < ncList.size(); i++)
+         {
+            String item = ncList.get(i);
+            comboDriverName.add(item);
+            
+            if (nc != null && item.equals(nc.getDriverName())) {
+               customName = !textName.getText().equals(nc.getDriverName());
+               index = i;
+            }
+         }
+         comboDriverName.select(index);
       }
    }
 
@@ -159,7 +195,7 @@ public class NotificationChannelPropertiesDialog extends Dialog
    protected void configureShell(Shell newShell)
    {
       super.configureShell(newShell);
-      newShell.setText(nc != null ? "Update notification channel" : "Create notification channel");
+      newShell.setText(nc != null ? i18n.tr("Update notification channel") : i18n.tr("Create notification channel"));
    }
 
    /* (non-Javadoc)
@@ -170,26 +206,22 @@ public class NotificationChannelPropertiesDialog extends Dialog
    {
       if(textName.getText().isEmpty())
       {
-         MessageDialogHelper.openWarning(getShell(), "Warning", "Notification channel name should not be empty");
+         MessageDialogHelper.openWarning(getShell(), i18n.tr("Warning"), i18n.tr("Notification channel name should not be empty"));
          return;
       }
       
       if(comboDriverName.getSelectionIndex() == -1)
       {
-         MessageDialogHelper.openWarning(getShell(), "Warning", "Notification driver should be selected");
+         MessageDialogHelper.openWarning(getShell(), i18n.tr("Warning"), i18n.tr("Notification driver should be selected"));
          return;
       }
       
       if(nc == null)
       {
          nc = new NotificationChannel();
-         nc.setName(textName.getText());
       }
-      else if(!nc.getName().equals(textName.getText()))
-      {
-         isNameChangeds = true;
-         newName = textName.getText();
-      }
+
+      nc.setName(textName.getText());
       nc.setDescription(textDescription.getText());
       nc.setDriverName(comboDriverName.getItem(comboDriverName.getSelectionIndex()));
       nc.setConfiguration(textConfiguraiton.getText());      
@@ -202,21 +234,5 @@ public class NotificationChannelPropertiesDialog extends Dialog
    public NotificationChannel getNotificaiotnChannel()
    {
       return nc;
-   }
-   
-   /**
-    * Returns if name is changed
-    */
-   public boolean isNameChanged()
-   {
-      return isNameChangeds;
-   }
-   
-   /**
-    * Get new name
-    */
-   public String getNewName()
-   {
-      return newName;
    }
 }
