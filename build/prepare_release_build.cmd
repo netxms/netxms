2e2e5d92a0a5a5a9b24e5e174374adf1d125f14b
Victor Kirhenshtein <victor@netxms.org>
2023-01-23 22:21:49 +0200

    updated Windows build preparation script

--- build/prepare_release_build.cmd
+++ build/prepare_release_build.cmd
@@ -5,8 +5,10 @@ perl updatetag.pl
 cd ..
 copy build\netxms-build-tag.properties src\java-common\netxms-base\src\main\resources\ 
 
-for /f "delims=" %%a in ('cat build/netxms-build-tag.properties ^| grep NETXMS_VERSION ^| cut -d^= -f2') do @set VERSION=%%a
+for /f "delims=" %%a in ('cat build/netxms-build-tag.properties ^| grep NETXMS_VERSION^= ^| cut -d^= -f2') do @set VERSION=%%a
 echo VERSION=%VERSION%
+
+for /f "delims=" %%a in ('cat build/netxms-build-tag.properties ^| grep NETXMS_VERSION_BASE^= ^| cut -d^= -f2') do @set VERSION_BASE=%%a
 
 type src\java\netxms-eclipse\Core\plugin.xml | sed -r "s,^(.*;Version) [0-9.]+(&#x0A.*)$,\1 %VERSION%\2," > plugin.xml
 copy /Y plugin.xml src\java\netxms-eclipse\Core\plugin.xml
@@ -26,6 +28,6 @@ make -f Makefile.w32
 cd ..
 
 echo chcp 65001 > build\update_exe_version.cmd
-echo rcedit %%1 --set-file-version %VERSION% --set-product-version %VERSION% --set-version-string CompanyName "Raden Solutions" --set-version-string FileDescription %%2 --set-version-string ProductName "NetXMS" --set-version-string LegalCopyright "© 2021 Raden Solutions SIA. All Rights Reserved" >> build\update_exe_version.cmd
+echo rcedit %%1 --set-file-version %VERSION_BASE% --set-product-version %VERSION_BASE% --set-version-string CompanyName "Raden Solutions" --set-version-string FileDescription %%2 --set-version-string ProductName "NetXMS" --set-version-string LegalCopyright "© 2023 Raden Solutions SIA. All Rights Reserved" >> build\update_exe_version.cmd
 
 IF EXIST .\private\branding\build\prepare_release_build_hook.cmd CALL .\private\branding\build\prepare_release_build_hook.cmd
