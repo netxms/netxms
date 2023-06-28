cd2fd51121f6e380b45febf78d9b2899ea0f738f
Victor Kirhenshtein <victor@netxms.org>
2023-06-28 17:16:07 +0300

    additional debug output in SMSEagle driver; option to disable HTTPS host and peer verification in SMSEagle driver

--- src/ncdrivers/smseagle/smseagle.cpp
+++ src/ncdrivers/smseagle/smseagle.cpp
@@ -36,43 +36,52 @@
 
 #define DEBUG_TAG _T("ncd.smseagle")
 
+#define OPTION_USE_HTTPS            0x01
+#define OPTION_VERIFY_HOST          0x02
+#define OPTION_VERIFY_CERTIFICATE   0x04
+
 /**
  * SMSEagle driver class
  */
 class SMSEagleDriver : public NCDriver
 {
 private:
+   uint32_t m_flags;
    char m_baseURL[256];
    char m_login[128];
    char m_password[128];
 
 public:
    SMSEagleDriver(Config *config);
+
    virtual int send(const TCHAR* recipient, const TCHAR* subject, const TCHAR* body) override;
 };
 
 /**
- * Init driver
+ * Initialize driver
  */
 SMSEagleDriver::SMSEagleDriver(Config *config)
 {
    nxlog_debug_tag(DEBUG_TAG, 5, _T("Creating new SMSEagle driver instance"));
 
+   m_flags = OPTION_USE_HTTPS | OPTION_VERIFY_HOST | OPTION_VERIFY_CERTIFICATE;
    m_baseURL[0] = 0;
    strcpy(m_login, "user");
    strcpy(m_password, "password");
 
    char hostname[128] = "127.0.0.1";
-   uint32_t port = 0, https = 1;
+   uint16_t port = 0;
    NX_CFG_TEMPLATE configTemplate[] = 
 	{
       { _T("BaseURL"), CT_MB_STRING, 0, 0, sizeof(m_baseURL), 0, m_baseURL },
 		{ _T("Host"), CT_MB_STRING, 0, 0, sizeof(hostname), 0, hostname },
+      { _T("HTTPS"), CT_BOOLEAN_FLAG_32, 0, 0, OPTION_USE_HTTPS, 0, &m_flags },   // For compatibility
 		{ _T("Login"), CT_MB_STRING, 0, 0, sizeof(m_login), 0, m_login },
 		{ _T("Password"), CT_MB_STRING, 0, 0, sizeof(m_password), 0, m_password },
-      { _T("Port"), CT_LONG, 0, 0, 0, 0, &port },
-      { _T("UseEncryption"), CT_BOOLEAN_FLAG_32, 0, 0, 1, 0, &https },
-      { _T("HTTPS"), CT_BOOLEAN_FLAG_32, 0, 0, 1, 0, &https },   // For compatibility
+      { _T("Port"), CT_WORD, 0, 0, 0, 0, &port },
+      { _T("UseEncryption"), CT_BOOLEAN_FLAG_32, 0, 0, OPTION_USE_HTTPS, 0, &m_flags },
+      { _T("VerifyCertificate"), CT_BOOLEAN_FLAG_32, 0, 0, OPTION_VERIFY_CERTIFICATE, 0, &m_flags },
+      { _T("VerifyHost"), CT_BOOLEAN_FLAG_32, 0, 0, OPTION_VERIFY_HOST, 0, &m_flags },
 		{ _T(""), CT_END_OF_LIST, 0, 0, 0, 0, nullptr }
 	};
 	config->parseTemplate(_T("SMSEagle"), configTemplate);
@@ -80,6 +89,7 @@ SMSEagleDriver::SMSEagleDriver(Config *config)
 	if (m_baseURL[0] == 0)
 	{
 	   // Construct base URL from individual elements
+	   bool https = (m_flags & OPTION_USE_HTTPS) != 0;
 	   snprintf(m_baseURL, sizeof(m_baseURL), "%s://%s:%d/index.php/http_api", https ? "https" : "http", hostname, (port != 0) ? port : (https ? 443 : 80));
 	}
 	else
@@ -90,6 +100,8 @@ SMSEagleDriver::SMSEagleDriver(Config *config)
 	      m_baseURL[index] = 0;
 	}
    nxlog_debug_tag(DEBUG_TAG, 4, _T("Base URL set to \"%hs\""), m_baseURL);
+
+   DecryptPasswordA(m_login, m_password, m_password, 128);
 
    CURL *curl = curl_easy_init();
    if (curl != nullptr)
@@ -142,9 +154,15 @@ int SMSEagleDriver::send(const TCHAR* recipient, const TCHAR* subject, const TCH
    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &OnCurlDataReceived);
 
+   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, (m_flags & OPTION_VERIFY_CERTIFICATE) ? 1 : 0);
+   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, (m_flags & OPTION_VERIFY_HOST) ? 2 : 0);
+
    ByteStream responseData(32768);
    responseData.setAllocationStep(32768);
    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseData);
+
+   char errbuf[CURL_ERROR_SIZE];
+   curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
 
    bool intlPrefix = (recipient[0] == _T('+')); // this will be used to decide whether it is number or group name
 #ifdef UNICODE
@@ -184,7 +202,7 @@ int SMSEagleDriver::send(const TCHAR* recipient, const TCHAR* subject, const TCH
       }
       else
       {
-         nxlog_debug_tag(DEBUG_TAG, 4, _T("Call to curl_easy_perform() failed"));
+         nxlog_debug_tag(DEBUG_TAG, 4, _T("Call to curl_easy_perform() failed (%hs)"), errbuf);
       }
    }
    else
