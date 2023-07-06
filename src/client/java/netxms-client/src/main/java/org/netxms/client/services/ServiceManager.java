2d3d20f38be118ec061a9e7b459e8f809848cf8e
Victor Kirhenshtein <victor@netxms.org>
2023-07-06 12:55:53 +0300

    report missing module data provider only once

--- src/client/java/netxms-client/src/main/java/org/netxms/client/services/ServiceManager.java
+++ src/client/java/netxms-client/src/main/java/org/netxms/client/services/ServiceManager.java
@@ -19,15 +19,23 @@
 package org.netxms.client.services;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.Set;
+import org.netxms.client.ModuleDataProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Manager for client services
  */
 public final class ServiceManager
 {
-   private static Map<ClassLoader, ServiceLoader<ServiceHandler>> serviceLoaders = new HashMap<ClassLoader, ServiceLoader<ServiceHandler>>();
+   private static final Logger logger = LoggerFactory.getLogger(ServiceManager.class);
+   private static final Map<ClassLoader, ServiceLoader<ServiceHandler>> serviceLoaders = new HashMap<ClassLoader, ServiceLoader<ServiceHandler>>();
+   private static final Map<String, ModuleDataProvider> dataProviders = new HashMap<>();
+   private static final Set<String> missingDataProviders = new HashSet<>();
 
    /**
     * Reload service providers
@@ -48,23 +56,35 @@ public final class ServiceManager
    }
 
    /**
-    * Get service handler by name and check if handler class is correct.
+    * Get module data provider for given module.
     * 
-    * @param name service name
-    * @param serviceClass service handler class
-    * @return service handler for given service
+    * @param name module name
+    * @return module data provider for given module or null
     */
-   public static synchronized ServiceHandler getServiceHandler(String name, Class<? extends ServiceHandler> serviceClass)
+   public static synchronized ModuleDataProvider getModuleDataProvider(String name)
    {
+      if (missingDataProviders.contains(name))
+         return null;
+
+      ModuleDataProvider p = dataProviders.get(name);
+      if (p != null)
+         return p;
+
       for(ServiceLoader<ServiceHandler> loader : serviceLoaders.values())
       {
          for(ServiceHandler s : loader)
-            if (s.getServiceName().equals(name) && serviceClass.isInstance(s))
-               return s;
+            if (s.getServiceName().equals(name) && (s instanceof ModuleDataProvider))
+            {
+               dataProviders.put(name, (ModuleDataProvider)s);
+               return (ModuleDataProvider)s;
+            }
       }
+
+      logger.warn("Unable to find data provider for module " + name);
+      missingDataProviders.add(name);
       return null;
    }
-   
+
    /**
     * Get service handler by name
     * 
@@ -81,7 +101,7 @@ public final class ServiceManager
       }
       return null;
    }
-   
+
    /**
     * Get service handler by class
     * 
@@ -98,7 +118,7 @@ public final class ServiceManager
       }
       return null;
    }
-   
+
    /**
     * Debug method to dump all registered services
     */
