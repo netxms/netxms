daa0766693b95a6b6e8ae29fb0dc9125bab74161
zev <zev@radensolutions.com>
2023-07-28 15:16:03 +0300

    Added interface change state action to new console

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/actions/ChangeInterfaceExpectedStateAction.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/actions/ChangeInterfaceExpectedStateAction.java
@@ -21,69 +21,72 @@ package org.netxms.nxmc.modules.objects.actions;
 import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
+import org.netxms.client.NXCObjectModificationData;
 import org.netxms.client.NXCSession;
-import org.netxms.client.objects.NetworkMap;
+import org.netxms.client.objects.Interface;
 import org.netxms.nxmc.Registry;
 import org.netxms.nxmc.base.jobs.Job;
 import org.netxms.nxmc.base.views.ViewPlacement;
 import org.netxms.nxmc.localization.LocalizationHelper;
-import org.netxms.nxmc.modules.objects.dialogs.CreateObjectDialog;
+import org.netxms.nxmc.modules.objects.dialogs.SetInterfaceExpStateDlg;
 import org.xnap.commons.i18n.I18n;
 
 /**
- * Clone network map
+ * Action for creating interface DCIs
  */
-public class CloneNetworkMap extends ObjectAction<NetworkMap>
+public class ChangeInterfaceExpectedStateAction extends ObjectAction<Interface>
 {
-   private static final I18n i18n = LocalizationHelper.getI18n(CloneNetworkMap.class);
+   private static final I18n i18n = LocalizationHelper.getI18n(ChangeInterfaceExpectedStateAction.class);
 
    /**
-    * Create action to clone network map
+    * Create action for creating interface DCIs.
     *
+    * @param text action's text
     * @param viewPlacement view placement information
     * @param selectionProvider associated selection provider
     */
-   public CloneNetworkMap(ViewPlacement viewPlacement, ISelectionProvider selectionProvider)
+   public ChangeInterfaceExpectedStateAction(ViewPlacement viewPlacement, ISelectionProvider selectionProvider)
    {
-      super(NetworkMap.class, i18n.tr("Clone..."), viewPlacement, selectionProvider);
+      super(Interface.class, i18n.tr("Change interface e&xpected state..."), viewPlacement, selectionProvider);
    }
 
    /**
     * @see org.netxms.nxmc.modules.objects.actions.ObjectAction#run(java.util.List)
     */
    @Override
-   protected void run(List<NetworkMap> objects)
-   {      
-      NetworkMap object = objects.get(0);
+   protected void run(List<Interface> objects)
+   {
+
+      if (objects.size() == 0)
+         return;
       
-      final CreateObjectDialog dlg = new CreateObjectDialog(getShell(), i18n.tr("Clone network map"));
+      SetInterfaceExpStateDlg dlg = new SetInterfaceExpStateDlg(getShell());
       if (dlg.open() != Window.OK)
          return;
       
+      final long[] idList = new long[objects.size()];
+      for(int i = 0; i < idList.length; i++)
+         idList[i] = objects.get(i).getObjectId();
+      final int newState = dlg.getExpectedState();
       final NXCSession session = Registry.getSession();
-      new Job(i18n.tr("Create new network map"), null, getMessageArea()) {
+      new Job(i18n.tr("Update expected state for interfaces"), null, getMessageArea()) {
          @Override
          protected void run(IProgressMonitor monitor) throws Exception
          {
-            session.cloneNetworkMap(object.getObjectId(), dlg.getObjectName(), dlg.getObjectAlias());
+            for(int i = 0; i < idList.length; i++)
+            {
+               NXCObjectModificationData md = new NXCObjectModificationData(idList[i]);
+               md.setExpectedState(newState);
+               session.modifyObject(md);
+            }
          }
-
+         
          @Override
          protected String getErrorMessage()
          {
-            return String.format(i18n.tr("Cannot create network map object \"%s\""), dlg.getObjectName());
+            return i18n.tr("Cannot update expected state for interface object");
          }
       }.start();
-   }
-
-   /**
-    * @see org.netxms.nxmc.modules.objects.actions.ObjectAction#isValidForSelection(org.eclipse.jface.viewers.IStructuredSelection)
-    */
-   @Override
-   public boolean isValidForSelection(IStructuredSelection selection)
-   {
-      return (selection.size() == 1) && (selection.getFirstElement() instanceof NetworkMap);
    }
 }
---
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/objects/dialogs/SetInterfaceExpStateDlg.java
@@ -0,0 +1,111 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2013 Raden Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.nxmc.modules.objects.dialogs;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.netxms.nxmc.localization.LocalizationHelper;
+import org.netxms.nxmc.tools.WidgetHelper;
+import org.xnap.commons.i18n.I18n;
+
+/**
+ * Set expected state for interface(s)
+ */
+public class SetInterfaceExpStateDlg extends Dialog
+{
+   private I18n i18n = LocalizationHelper.getI18n(SetInterfaceExpStateDlg.class);
+	private Combo comboStates;
+	private int expectedState;
+	
+	/**
+	 * @param parentShell
+	 */
+	public SetInterfaceExpStateDlg(Shell parentShell)
+	{
+		super(parentShell);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
+	 */
+	@Override
+	protected void configureShell(Shell newShell)
+	{
+		super.configureShell(newShell);
+		newShell.setText(i18n.tr("Set Interface Expected State"));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent)
+	{
+		Composite dialogArea = (Composite)super.createDialogArea(parent);
+		
+		GridLayout layout = new GridLayout();
+		layout.horizontalSpacing = WidgetHelper.OUTER_SPACING;
+		layout.marginHeight = WidgetHelper.DIALOG_HEIGHT_MARGIN;
+		layout.marginWidth = WidgetHelper.DIALOG_WIDTH_MARGIN;
+		layout.numColumns = 2;
+		dialogArea.setLayout(layout);
+		
+		Label label = new Label(dialogArea, SWT.NONE);
+		label.setText(i18n.tr("New expected state for selected interface(s):"));
+		GridData gd = new GridData();
+		gd.verticalAlignment = SWT.CENTER;
+		label.setLayoutData(gd);
+		
+		comboStates = new Combo(dialogArea, SWT.BORDER | SWT.READ_ONLY);
+		gd = new GridData();
+		gd.verticalAlignment = SWT.CENTER;
+		comboStates.setLayoutData(gd);
+		comboStates.add(i18n.tr("UP"));
+		comboStates.add(i18n.tr("DOWN"));
+		comboStates.add(i18n.tr("IGNORE"));
+		comboStates.select(0);
+
+		return dialogArea;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
+	@Override
+	protected void okPressed()
+	{
+		expectedState = comboStates.getSelectionIndex();
+		super.okPressed();
+	}
+
+	/**
+	 * @return the expectedState
+	 */
+	public int getExpectedState()
+	{
+		return expectedState;
+	}
+}
