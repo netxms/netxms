69e942e9546aa117090268f72df81acef03d1e63
Victor Kirhenshtein <victor@netxms.org>
2023-08-10 00:35:14 +0300

    added driver for Hirschmann HiOS switches

---
+++ src/server/drivers/hirschmann/hirschmann.h
@@ -0,0 +1,45 @@
+/* 
+** NetXMS - Network Management System
+** Drivers for Hirschmann devices
+** Copyright (C) 2023 Raden Solutions
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**
+** File: hirschmann.h
+**
+**/
+
+#ifndef _hirschmann_h_
+#define _hirschmann_h_
+
+#include <nddrv.h>
+#include <netxms-version.h>
+
+/**
+ * Driver for HiOS devices
+ */
+class HirschmannHiOSDriver : public NetworkDeviceDriver
+{
+public:
+   virtual const TCHAR *getName() override;
+   virtual const TCHAR *getVersion() override;
+
+   virtual int isPotentialDevice(const TCHAR *oid) override;
+   virtual bool isDeviceSupported(SNMP_Transport *snmp, const TCHAR *oid) override;
+   virtual bool getHardwareInformation(SNMP_Transport *snmp, NObject *node, DriverData *driverData, DeviceHardwareInfo *hwInfo) override;
+   virtual InterfaceList *getInterfaces(SNMP_Transport *snmp, NObject *node, DriverData *driverData, bool useIfXTable) override;
+};
+
+#endif
