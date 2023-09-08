6929805195924af84e012701f9ca5118930a95a6
Victor Kirhenshtein <victor@netxms.org>
2023-09-08 09:33:01 +0300

    SessionListener annotated as functional interface

--- src/client/java/netxms-client/src/main/java/org/netxms/client/SessionListener.java
+++ src/client/java/netxms-client/src/main/java/org/netxms/client/SessionListener.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2010 Victor Kirhenshtein
+ * Copyright (C) 2003-2023 Victor Kirhenshtein
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,8 +20,8 @@ package org.netxms.client;
 
 /**
  * Interface for NetXMS client library notification listeners
- *
  */
+@FunctionalInterface
 public interface SessionListener
 {
 	abstract public void notificationHandler(SessionNotification n);
