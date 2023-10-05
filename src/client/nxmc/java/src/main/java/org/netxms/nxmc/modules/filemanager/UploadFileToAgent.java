9c33863195d31828640e677546fcf060fd6bb06a
zev <zev@radensolutions.com>
2023-10-05 16:13:42 +0300

    Added 'upload file to agent' object menu item without 'job on hold' option to new console

---
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/filemanager/UploadFileToAgent.java
@@ -0,0 +1,170 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2011 Victor Kirhenshtein
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */package org.netxms.nxmc.modules.filemanager;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.window.Window;
+import org.netxms.client.NXCSession;
+import org.netxms.client.ScheduledTask;
+import org.netxms.client.constants.UserAccessRights;
+import org.netxms.client.objects.AbstractNode;
+import org.netxms.client.objects.AbstractObject;
+import org.netxms.client.objects.Container;
+import org.netxms.client.objects.EntireNetwork;
+import org.netxms.client.objects.ServiceRoot;
+import org.netxms.client.objects.Subnet;
+import org.netxms.client.server.ServerFile;
+import org.netxms.nxmc.Registry;
+import org.netxms.nxmc.base.jobs.Job;
+import org.netxms.nxmc.base.views.ViewPlacement;
+import org.netxms.nxmc.localization.LocalizationHelper;
+import org.netxms.nxmc.modules.assetmanagement.LinkAssetToObjectAction;
+import org.netxms.nxmc.modules.filemanager.dialogs.StartServerToAgentFileUploadDialog;
+import org.netxms.nxmc.modules.objects.actions.ObjectAction;
+import org.netxms.nxmc.resources.ResourceManager;
+import org.xnap.commons.i18n.I18n;
+
+/**
+ * Action: upload file from server to agent(s)
+ *
+ */
+public class UploadFileToAgent extends ObjectAction<AbstractObject>
+{
+   private static final I18n i18n = LocalizationHelper.getI18n(LinkAssetToObjectAction.class);
+
+   /**
+    * Create action for linking asset to object.
+    *
+    * @param viewPlacement view placement information
+    * @param selectionProvider associated selection provider
+    */
+   public UploadFileToAgent(ViewPlacement viewPlacement, ISelectionProvider selectionProvider)
+   {
+      super(AbstractObject.class, i18n.tr("Upload file..."), viewPlacement, selectionProvider);
+      setImageDescriptor(ResourceManager.getImageDescriptor("icons/upload.png"));
+   }
+
+   /**
+    * @see org.netxms.nxmc.modules.objects.actions.ObjectAction#run(java.util.List)
+    */
+   @Override
+   protected void run(List<AbstractObject> objects)
+   {
+      final NXCSession session = Registry.getSession();
+      Set<Long> nodes = new HashSet<Long>();
+      for (Object object : objects)
+      {
+         if (object instanceof AbstractNode)
+         {
+            nodes.add(((AbstractObject)object).getObjectId());
+         }
+         else if ((object instanceof Container) || (object instanceof ServiceRoot) || 
+               (object instanceof Subnet)  || (object instanceof EntireNetwork))
+         {
+            Set<AbstractObject> set = ((AbstractObject)object).getAllChildren(AbstractObject.OBJECT_NODE);
+            for(AbstractObject o : set)
+               nodes.add(o.getObjectId());
+         }
+      }
+      
+      boolean canScheduleFileUpload = (session.getUserSystemRights() & UserAccessRights.SYSTEM_ACCESS_SCHEDULE_FILE_UPLOAD) > 0;
+		final StartServerToAgentFileUploadDialog dlg = new StartServerToAgentFileUploadDialog(getShell(), canScheduleFileUpload);
+		if (dlg.open() == Window.OK)
+		{
+			final Long[] nodeIdList = nodes.toArray(new Long[nodes.size()]);
+			new Job(i18n.tr("Initiate file upload to agent"), null, getMessageArea()) {
+				@Override
+				protected String getErrorMessage()
+				{
+					return i18n.tr("Cannot start file upload job");
+				}
+
+				@Override
+				protected void run(IProgressMonitor monitor) throws Exception
+				{
+				   boolean multipleFiles = dlg.getServerFiles().size() > 1;
+				   for(ServerFile sf : dlg.getServerFiles())
+				   {
+   				   String remoteFileName = dlg.getRemoteFileName();
+   				   if (!remoteFileName.isEmpty())
+   				   {
+   				      if (remoteFileName.endsWith("/") || remoteFileName.endsWith("\\")) //$NON-NLS-1$ //$NON-NLS-2$
+   				      {
+   				         remoteFileName += sf.getName();
+   				      }
+   				      else if (multipleFiles)
+   				      {
+                        remoteFileName += "/" + sf.getName();
+   				      }
+   				   }
+   				   else 
+   				   {
+   				      if (!dlg.isScheduled())
+   				         remoteFileName = null;
+   				   }
+   					for(int i = 0; i < nodeIdList.length; i++)
+   					{
+   					   if (dlg.isScheduled())
+   					   {
+   					      ScheduledTask task = dlg.getScheduledTask();
+   					      String parameters = sf.getName() + "," + remoteFileName; // $NON-NLS-1$
+   					      task.setParameters(parameters);
+   					      task.setObjectId(nodeIdList[i]);
+   					      session.addScheduledTask(task);
+   					   }
+   					   else
+   					   {
+   					      session.uploadFileToAgent(nodeIdList[i], sf.getName(), remoteFileName, false);
+   					   }
+   					}
+				   }
+				}
+			}.start();
+		}
+	}
+
+   /**
+    * @see org.netxms.nxmc.modules.objects.actions.ObjectAction#isValidForSelection(org.eclipse.jface.viewers.IStructuredSelection)
+    */
+   @Override
+   public boolean isValidForSelection(IStructuredSelection selection)
+   {
+      if (!selection.isEmpty())
+      {
+         for (Object obj : ((IStructuredSelection)selection).toList())
+         {
+            if (!(((obj instanceof AbstractNode) && ((AbstractNode)obj).hasAgent()) || 
+                (obj instanceof Container) || (obj instanceof ServiceRoot) || (obj instanceof Subnet) ||
+                (obj instanceof EntireNetwork)))
+            {
+               return false;
+            }
+         }
+      }
+      else
+      {
+         return false;
+      }
+      return true;
+   }
+}
---
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/filemanager/dialogs/StartServerToAgentFileUploadDialog.java
@@ -0,0 +1,276 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2023 Raden Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package org.netxms.nxmc.modules.filemanager.dialogs;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.netxms.client.ScheduledTask;
+import org.netxms.client.server.ServerFile;
+import org.netxms.nxmc.base.widgets.LabeledText;
+import org.netxms.nxmc.base.widgets.ScheduleEditor;
+import org.netxms.nxmc.localization.LocalizationHelper;
+import org.netxms.nxmc.tools.MessageDialogHelper;
+import org.netxms.nxmc.tools.WidgetHelper;
+import org.xnap.commons.i18n.I18n;
+
+/**
+ * Dialog for starting file upload
+ */
+public class StartServerToAgentFileUploadDialog extends Dialog
+{
+   private static final I18n i18n = LocalizationHelper.getI18n(StartServerToAgentFileUploadDialog.class);
+   
+	private TableViewer fileList;
+	private Button buttonAddFile;
+   private Button buttonRemoveFile;
+	private LabeledText textRemoteFile;
+	private Button checkJobOnHold;
+   private Button checkIsSchedule;
+   private ScheduleEditor scheduleEditor;
+	private List<ServerFile> serverFiles = new ArrayList<ServerFile>();
+	private String remoteFileName;
+	private boolean scheduledTask;
+	private ScheduledTask schedule;
+	private boolean canScheduleFileUpload;
+
+	/**
+	 * 
+	 * @param parentShell
+	 * @param canScheduleFileUpload 
+	 */
+	public StartServerToAgentFileUploadDialog(Shell parentShell, boolean canScheduleFileUpload)
+	{
+		super(parentShell);
+		this.canScheduleFileUpload = canScheduleFileUpload;
+	}
+
+   /**
+    * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
+    */
+	@Override
+	protected void configureShell(Shell newShell)
+	{
+		super.configureShell(newShell);
+		newShell.setText(i18n.tr("Start File Upload"));
+	}
+
+   /**
+    * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+    */
+	@Override
+	protected Control createDialogArea(Composite parent)
+	{
+		Composite dialogArea = (Composite)super.createDialogArea(parent);
+		
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = WidgetHelper.DIALOG_HEIGHT_MARGIN;
+		layout.marginWidth = WidgetHelper.DIALOG_WIDTH_MARGIN;
+		layout.verticalSpacing = WidgetHelper.DIALOG_SPACING;
+		dialogArea.setLayout(layout);
+		
+		Label label = new Label(dialogArea, SWT.NONE);
+      label.setText(i18n.tr("Server files"));
+      
+		fileList = new TableViewer(dialogArea, SWT.BORDER | SWT.MULTI | SWT.FULL_SELECTION);
+		GridData gd = new GridData();
+		gd.horizontalAlignment = SWT.FILL;
+		gd.grabExcessHorizontalSpace = true;
+		gd.widthHint = 400;
+		gd.heightHint = 150;
+		fileList.getControl().setLayoutData(gd);
+		
+		fileList.setContentProvider(new ArrayContentProvider());
+		fileList.setLabelProvider(new LabelProvider() {
+         @Override
+         public String getText(Object element)
+         {
+            return ((ServerFile)element).getName();
+         }
+		});
+		fileList.setComparator(new ViewerComparator() {
+         @Override
+         public int compare(Viewer viewer, Object e1, Object e2)
+         {
+            return ((ServerFile)e1).getName().compareToIgnoreCase(((ServerFile)e2).getName());
+         }
+		});
+		fileList.setInput(serverFiles);
+		
+		Composite buttonArea = new Composite(dialogArea, SWT.NONE);
+      layout = new GridLayout();
+      layout.marginHeight = 0;
+      layout.marginWidth = 0;
+      layout.numColumns = 2;
+      buttonArea.setLayout(layout);
+      gd = new GridData();
+      gd.horizontalAlignment = SWT.RIGHT;
+      buttonArea.setLayoutData(gd);
+      
+      buttonAddFile = new Button(buttonArea, SWT.PUSH);
+      buttonAddFile.setText("&Add...");
+      gd = new GridData();
+      gd.widthHint = WidgetHelper.BUTTON_WIDTH_HINT;
+      buttonAddFile.setLayoutData(gd);
+      buttonAddFile.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e)
+         {
+            SelectServerFileDialog dlg = new SelectServerFileDialog(getShell(), true);
+            if (dlg.open() == Window.OK)
+            {
+               for(ServerFile f : dlg.getSelectedFiles())
+               {
+                  boolean found = false;
+                  for(ServerFile sf : serverFiles)
+                     if (sf.getName().equals(f.getName()))
+                     {
+                        found = true;
+                        break;
+                     }
+                  if (!found)
+                     serverFiles.add(f);
+               }
+               fileList.refresh();
+            }
+         }
+      });
+		
+      buttonRemoveFile = new Button(buttonArea, SWT.PUSH);
+      buttonRemoveFile.setText("&Remove");
+      gd = new GridData();
+      gd.widthHint = WidgetHelper.BUTTON_WIDTH_HINT;
+      buttonRemoveFile.setLayoutData(gd);
+      buttonRemoveFile.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e)
+         {
+            IStructuredSelection selection = (IStructuredSelection)fileList.getSelection();
+            for(Object o : selection.toList())
+               serverFiles.remove(o);
+            fileList.refresh();
+         }
+      });
+      
+		textRemoteFile = new LabeledText(dialogArea, SWT.NONE);
+		textRemoteFile.setLabel(i18n.tr("Remote file name (leave blank for upload to agent's file store)"));
+		gd = new GridData();
+		gd.horizontalAlignment = SWT.FILL;
+		gd.grabExcessHorizontalSpace = true;
+		textRemoteFile.setLayoutData(gd);
+		
+		if (canScheduleFileUpload)
+		{
+   		checkIsSchedule = new Button(dialogArea, SWT.CHECK);
+   		checkIsSchedule.setText(i18n.tr("Schedule task"));
+         checkIsSchedule.addSelectionListener(new SelectionListener() {
+            @Override
+            public void widgetSelected(SelectionEvent e)
+            {        
+               checkJobOnHold.setEnabled(!checkIsSchedule.getSelection());
+               scheduleEditor.setEnabled(checkIsSchedule.getSelection());
+            }
+            
+            @Override
+            public void widgetDefaultSelected(SelectionEvent e)
+            {
+               widgetSelected(e); 
+            }
+         });
+         
+         scheduleEditor = new ScheduleEditor(dialogArea, SWT.NONE);
+         scheduleEditor.setEnabled(false);
+		}
+		
+		return dialogArea;
+	}
+
+   /**
+    * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+    */
+	@Override
+	protected void okPressed()
+	{
+	   scheduledTask = checkIsSchedule.getSelection();
+	   if (scheduledTask)
+	   {
+	      schedule = scheduleEditor.getSchedule();
+	   }
+		if (serverFiles.isEmpty())
+		{
+			MessageDialogHelper.openWarning(getShell(), i18n.tr("Warning"), i18n.tr("Please select file for upload"));
+			return;
+		}
+		remoteFileName = textRemoteFile.getText().trim();
+      /* TODO: remove?
+		createJobOnHold = checkJobOnHold.getSelection();
+		*/
+		super.okPressed();
+	}
+
+	/**
+	 * @return selected server files
+	 */
+	public List<ServerFile> getServerFiles()
+	{
+		return serverFiles;
+	}
+
+	/**
+	 * @return the remoteFileName
+	 */
+	public String getRemoteFileName()
+	{
+		return remoteFileName;
+	}
+	
+	/**
+    * @return the scheduledTask
+    */
+	public boolean isScheduled()
+	{
+	   return scheduledTask;
+	}
+	
+	/**
+    * @return the schedule
+    */
+	public ScheduledTask getScheduledTask()
+	{
+	   return schedule;
+	}
+}
