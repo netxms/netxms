23808a6611aa2d03adc84cd5567c248fc7cb14a6
Victor Kirhenshtein <victor@netxms.org>
2024-01-24 08:43:20 +0200

    fixed exception in old web UI during two-factor authentication

--- webui/webapp/Core/src/org/netxms/ui/eclipse/jobs/LoginJob.java
+++ webui/webapp/Core/src/org/netxms/ui/eclipse/jobs/LoginJob.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2023 Raden Solutions
+ * Copyright (C) 2003-2024 Raden Solutions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -193,14 +193,14 @@ public class LoginJob implements IRunnableWithProgress
                         }
                      }
                   });
-               }
-               try
-               {
-                  response.wait();
-               }
-               catch(InterruptedException e)
-               {
-                  Activator.logError("Wait interrupted", e);
+                  try
+                  {
+                     response.wait();
+                  }
+                  catch(InterruptedException e)
+                  {
+                     Activator.logError("Wait interrupted", e);
+                  }
                }
                return response[0];
             }
