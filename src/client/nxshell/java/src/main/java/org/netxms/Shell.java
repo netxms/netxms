b5b35e5756d701f8d75918e379e9adc03d164be0
Victor Kirhenshtein <victor@netxms.org>
2024-03-04 09:40:56 +0200

    fixed imports in nxshell

--- src/client/nxshell/java/src/main/java/org/netxms/Shell.java
+++ src/client/nxshell/java/src/main/java/org/netxms/Shell.java
@@ -1,3 +1,21 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2003-2024 Raden Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.netxms;
 
 import java.io.Console;
@@ -238,6 +256,8 @@ public class Shell
       console.exec("from org.netxms.base import *");
       console.exec("from org.netxms.client import *");
       console.exec("from org.netxms.client.agent.config import *");
+      console.exec("from org.netxms.client.asset import *");
+      console.exec("from org.netxms.client.businessservices import *");
       console.exec("from org.netxms.client.constants import *");
       console.exec("from org.netxms.client.dashboards import *");
       console.exec("from org.netxms.client.datacollection import *");
@@ -246,14 +266,16 @@ public class Shell
       console.exec("from org.netxms.client.maps import *");
       console.exec("from org.netxms.client.maps.configs import *");
       console.exec("from org.netxms.client.maps.elements import *");
-      console.exec("from org.netxms.client.market import *");
       console.exec("from org.netxms.client.mt import *");
       console.exec("from org.netxms.client.objects import *");
       console.exec("from org.netxms.client.objects.configs import *");
+      console.exec("from org.netxms.client.objects.interfaces import *");
+      console.exec("from org.netxms.client.objects.queries import *");
       console.exec("from org.netxms.client.objecttools import *");
       console.exec("from org.netxms.client.packages import *");
       console.exec("from org.netxms.client.reporting import *");
-      console.exec("from org.netxms.client.sensor.configs import *");
+      console.exec("from org.netxms.client.search import *");
+      console.exec("from org.netxms.client.sensor import *");
       console.exec("from org.netxms.client.server import *");
       console.exec("from org.netxms.client.services import *");
       console.exec("from org.netxms.client.snmp import *");
