2ab05efc6bd8a3d081389ba1058da01f54834911
zev <zev@radensolutions.com>
2024-03-12 12:32:27 +0200

    Fixed scheduling agent to server upload

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/filemanager/dialogs/StartServerToAgentFileUploadDialog.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/filemanager/dialogs/StartServerToAgentFileUploadDialog.java
@@ -229,6 +229,7 @@ public class StartServerToAgentFileUploadDialog extends Dialog
 	   if (scheduledTask)
 	   {
 	      schedule = scheduleEditor.getSchedule();
+	      schedule.setTaskHandlerId("Upload.File");
 	   }
 		if (serverFiles.isEmpty())
 		{
