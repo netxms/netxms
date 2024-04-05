d7772b4d5c514a02209b39c1e2deb677debaaf39
Victor Kirhenshtein <victor@netxms.org>
2024-04-05 12:57:10 +0300

    cosmetic change

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/reporting/ReportingPerspective.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/reporting/ReportingPerspective.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2023 Victor Kirhenshtein
+ * Copyright (C) 2003-2024 Victor Kirhenshtein
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,12 +27,12 @@ import org.netxms.nxmc.modules.reporting.views.ReportView;
 import org.netxms.nxmc.resources.ResourceManager;
 
 /**
- * Alarm browser perspective
+ * Reporting perspective
  */
 public class ReportingPerspective extends Perspective
 {
    /**
-    * @param name
+    * Create reporting perspective
     */
    public ReportingPerspective()
    {
