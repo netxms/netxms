7c7fa28029ee793e4bff90fe2aa87ee2248a9145
Victor Kirhenshtein <victor@netxms.org>
2024-07-14 11:46:39 +0300

    fixed Windows build dependency issue

--- src/agent/tools/nxreload/nxreload.cpp
+++ src/agent/tools/nxreload/nxreload.cpp
@@ -1,6 +1,6 @@
 /* 
 ** nxreload - helper tool for restarting session agents
-** Copyright (C) 2006-2021 Raden Solutions
+** Copyright (C) 2006-2024 Raden Solutions
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
