30cff929e529a481cf059959b43b558304d75ca8
Victor Kirhenshtein <victor@netxms.org>
2024-10-11 20:27:03 +0300

    added env setup file for WIndows ARM64 build

---
+++ build/msvc_setenv_arm64.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+rem *** Windows 7 SDK with Visual Studio 2017 MSBuild
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin;C:\SDK\Windows 7 SDK\Bin;%PATH%
