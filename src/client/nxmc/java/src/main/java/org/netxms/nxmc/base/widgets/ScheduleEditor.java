c6a605adec625e8269191ff79b763d72da449afc
Victor Kirhenshtein <victor@netxms.org>
2024-11-09 12:40:19 +0200

    scheduled task processing performance optimization and bug fixes

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/base/widgets/ScheduleEditor.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/base/widgets/ScheduleEditor.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2022 Raden Solutions
+ * Copyright (C) 2003-2024 Raden Solutions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -100,15 +100,15 @@ public class ScheduleEditor extends Composite
 		super.setEnabled(enabled);
 	   radioOneTimeSchedule.setEnabled(enabled);
 	   radioRecurrent.setEnabled(enabled);
-	   if(enabled == true)
+      if (enabled)
 	   {
          execDateSelector.setEnabled(radioOneTimeSchedule.getSelection());
          textSchedule.setEnabled(radioRecurrent.getSelection());
 	   }
 	   else 
 	   {
-	      execDateSelector.setEnabled(enabled);
-	      textSchedule.setEnabled(enabled);
+         execDateSelector.setEnabled(false);
+         textSchedule.setEnabled(false);
 	   }
 	}
 
