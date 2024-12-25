fa7aa759266944d4be2d22be1f5254fb31cba941
Victor Kirhenshtein <victor@netxms.org>
2024-12-25 22:23:06 +0200

    fixed wfopen declaration; code cleanup

--- include/nms_util.h
+++ include/nms_util.h
@@ -219,15 +219,15 @@ static inline WCHAR towupper(WCHAR c)
 /**
  * nxlog_open() flags
  */
-#define NXLOG_USE_SYSLOG		  ((UINT32)0x00000001)  /* use syslog (Event Log on Windows) as log device */
-#define NXLOG_PRINT_TO_STDOUT	  ((UINT32)0x00000002)  /* print log copy to stdout */
-#define NXLOG_BACKGROUND_WRITER ((UINT32)0x00000004)  /* enable background log writer */
-#define NXLOG_DEBUG_MODE        ((UINT32)0x00000008)  /* log debug mode - pront additional debug infor about logger itself */
-#define NXLOG_USE_SYSTEMD       ((UINT32)0x00000010)  /* use stderr as log device and systemd output format */
-#define NXLOG_JSON_FORMAT       ((UINT32)0x00000020)  /* write log in JSON format */
-#define NXLOG_USE_STDOUT        ((UINT32)0x00000040)  /* use stdout as log device */
-#define NXLOG_ROTATION_ERROR    ((UINT32)0x40000000)  /* internal flag, should not be set by caller */
-#define NXLOG_IS_OPEN           ((UINT32)0x80000000)  /* internal flag, should not be set by caller */
+#define NXLOG_USE_SYSLOG        ((uint32_t)0x00000001)  /* use syslog (Event Log on Windows) as log device */
+#define NXLOG_PRINT_TO_STDOUT   ((uint32_t)0x00000002)  /* print log copy to stdout */
+#define NXLOG_BACKGROUND_WRITER ((uint32_t)0x00000004)  /* enable background log writer */
+#define NXLOG_DEBUG_MODE        ((uint32_t)0x00000008)  /* log debug mode - pront additional debug infor about logger itself */
+#define NXLOG_USE_SYSTEMD       ((uint32_t)0x00000010)  /* use stderr as log device and systemd output format */
+#define NXLOG_JSON_FORMAT       ((uint32_t)0x00000020)  /* write log in JSON format */
+#define NXLOG_USE_STDOUT        ((uint32_t)0x00000040)  /* use stdout as log device */
+#define NXLOG_ROTATION_ERROR    ((uint32_t)0x40000000)  /* internal flag, should not be set by caller */
+#define NXLOG_IS_OPEN           ((uint32_t)0x80000000)  /* internal flag, should not be set by caller */
 
 /**
  * nxlog rotation policy
@@ -3884,7 +3884,7 @@ public:
    void setStatus(int status) { m_status = status; }
 
    int getObjectId() const { return m_objectId; }
-   void setObjectId(UINT32 id) { m_objectId = id; }
+   void setObjectId(uint32_t id) { m_objectId = id; }
 };
 
 #ifdef _WIN32
@@ -5185,9 +5185,9 @@ static inline bool SocketCanWrite(SOCKET hSocket, uint32_t timeout)
 SOCKET LIBNETXMS_EXPORTABLE ConnectToHost(const InetAddress& addr, uint16_t port, uint32_t timeout);
 SOCKET LIBNETXMS_EXPORTABLE ConnectToHostUDP(const InetAddress& addr, uint16_t port);
 
-TCHAR LIBNETXMS_EXPORTABLE *IpToStr(UINT32 dwAddr, TCHAR *szBuffer);
+TCHAR LIBNETXMS_EXPORTABLE *IpToStr(uint32_t ipAddr, TCHAR *buffer);
 #ifdef UNICODE
-char LIBNETXMS_EXPORTABLE *IpToStrA(UINT32 dwAddr, char *szBuffer);
+char LIBNETXMS_EXPORTABLE *IpToStrA(uint32_t ipAddr, char *buffer);
 #else
 #define IpToStrA IpToStr
 #endif
@@ -5466,8 +5466,6 @@ int LIBNETXMS_EXPORTABLE __daemon(int nochdir, int noclose);
 #define daemon __daemon
 #endif
 
-UINT32 LIBNETXMS_EXPORTABLE inet_addr_w(const WCHAR *pszAddr);
-
 #ifndef _WIN32
 
 bool LIBNETXMS_EXPORTABLE SetDefaultCodepage(const char *cp);
@@ -5505,9 +5503,6 @@ int LIBNETXMS_EXPORTABLE wstat(const WCHAR *_path, struct stat *_sbuf);
 
 #if !HAVE_WPOPEN
 FILE LIBNETXMS_EXPORTABLE *wpopen(const WCHAR *_command, const WCHAR *_type);
-#endif
-#if !HAVE_WFOPEN
-FILE LIBNETXMS_EXPORTABLE *wfopen(const WCHAR *_name, const WCHAR *_type);
 #endif
 #if HAVE_FOPEN64 && !HAVE_WFOPEN64
 FILE LIBNETXMS_EXPORTABLE *wfopen64(const WCHAR *_name, const WCHAR *_type);
