5173db940acebae7a80b1b1e0bb5a4ff89527c2f
Victor Kirhenshtein <victor@netxms.org>
2024-12-27 17:00:03 +0200

    bump version

--- src/agent/subagents/winperf/winperf.cpp
+++ src/agent/subagents/winperf/winperf.cpp
@@ -1,6 +1,6 @@
 /*
 ** Windows Performance NetXMS subagent
-** Copyright (C) 2004-2016 Victor Kirhenshtein
+** Copyright (C) 2004-2024 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
