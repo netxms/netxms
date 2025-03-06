9f8ac12d619ff9af15f99a007230d4c37d6a2d11
Victor Kirhenshtein <victor@netxms.org>
2025-03-06 12:03:56 +0200

    system access rights included into user ACL report; updated object access right set in ACL report

--- src/java/netxms-eclipse/UserManager/src/org/netxms/ui/eclipse/usermanager/reports/acl/constants/UsersSheetCells.java
+++ src/java/netxms-eclipse/UserManager/src/org/netxms/ui/eclipse/usermanager/reports/acl/constants/UsersSheetCells.java
@@ -1,3 +1,21 @@
+/**
+ * NetXMS - open source network management system
+ * Copyright (C) 2020-2025 Raden Soultions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 package org.netxms.ui.eclipse.usermanager.reports.acl.constants;
 
 public enum UsersSheetCells
@@ -8,5 +26,55 @@ public enum UsersSheetCells
    FULL_NAME,
    DESCRIPTION,
    ORIGIN,
-   GROUPS
+   GROUPS,
+   ALL_SCHEDULED_TASKS,
+   AM_ATTRIBUTE_MANAGE,
+   CONFIGURE_TRAPS,
+   DELETE_ALARMS,
+   EDIT_EVENT_DB,
+   EPP,
+   EXTERNAL_INTEGRATION,
+   IMPORT_CONFIGURATION,
+   MANAGE_ACTIONS,
+   MANAGE_AGENT_CFG,
+   MANAGE_GEO_AREAS,
+   MANAGE_IMAGE_LIB,
+   MANAGE_MAPPING_TBLS,
+   MANAGE_OBJECT_QUERIES,
+   MANAGE_PACKAGES,
+   MANAGE_REPOSITORIES,
+   MANAGE_SCRIPTS,
+   MANAGE_SERVER_FILES,
+   MANAGE_SESSIONS,
+   MANAGE_SUMMARY_TBLS,
+   MANAGE_TOOLS,
+   MANAGE_2FA_METHODS,
+   MANAGE_USERS,
+   MOBILE_DEVICE_LOGIN,
+   OBJECT_CATEGORIES,
+   OWN_SCHEDULED_TASKS,
+   PERSISTENT_STORAGE,
+   READ_SERVER_FILES,
+   REGISTER_AGENTS,
+   REPORTING_SERVER,
+   SCHEDULE_FILE_UPLOAD,
+   SCHEDULE_MAINTENANCE,
+   SCHEDULE_SCRIPT,
+   SEND_NOTIFICATION,
+   SERVER_CONFIG,
+   SERVER_CONSOLE,
+   SETUP_TCP_PROXY,
+   SSH_KEY_CONFIGURATION,
+   UA_NOTIFICATIONS,
+   UNLINK_ISSUES,
+   USER_SCHEDULED_TASKS,
+   VIEW_ALL_ALARMS,
+   VIEW_ASSET_CHANGE_LOG,
+   VIEW_AUDIT_LOG,
+   VIEW_EVENT_DB,
+   VIEW_EVENT_LOG,
+   VIEW_REPOSITORIES,
+   VIEW_SYSLOG,
+   VIEW_TRAP_LOG,
+   WEB_SERVICE_DEFINITIONS
 }
