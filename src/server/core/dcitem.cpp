3577d57bff716b615e6a2266fbb5358475f60cce
Victor Kirhenshtein <victor@netxms.org>
2025-03-22 20:33:03 +0200

    fixed race condition when processing scripted thresholds (fixes NX-2712)

--- src/server/core/dcitem.cpp
+++ src/server/core/dcitem.cpp
@@ -431,9 +431,10 @@ bool DCItem::saveToDatabase(DB_HANDLE hdb)
 }
 
 /**
- * Check last value for threshold violations
+ * Check last value for threshold violations. Original DCI is a pointer to source DCI
+ * if threshold check is performed on shadow copy or nullptr otherwise.
  */
-void DCItem::checkThresholds(ItemValue &value)
+void DCItem::checkThresholds(ItemValue &value, const shared_ptr<DCObject>& originalDci)
 {
 	if (m_thresholds == nullptr)
 		return;
@@ -454,7 +455,7 @@ void DCItem::checkThresholds(ItemValue &value)
       {
          case ThresholdCheckResult::ACTIVATED:
             {
-               shared_ptr<DCObject> sharedThis(shared_from_this());
+               shared_ptr<DCObject> sharedThis((originalDci != nullptr) ? originalDci : shared_from_this());
                EventBuilder(t->getEventCode(), m_ownerId)
                   .dci(m_id)
                   .param(_T("dciName"), m_name)
@@ -478,7 +479,7 @@ void DCItem::checkThresholds(ItemValue &value)
             break;
          case ThresholdCheckResult::DEACTIVATED:
             {
-               shared_ptr<DCObject> sharedThis(shared_from_this());
+               shared_ptr<DCObject> sharedThis((originalDci != nullptr) ? originalDci : shared_from_this());
                EventBuilder(t->getRearmEventCode(), m_ownerId)
                   .dci(m_id)
                   .param(_T("dciName"), m_name)
@@ -509,7 +510,7 @@ void DCItem::checkThresholds(ItemValue &value)
 	            time_t repeatInterval = (t->getRepeatInterval() == -1) ? g_thresholdRepeatInterval : static_cast<time_t>(t->getRepeatInterval());
 				   if (resendActivationEvent || ((repeatInterval != 0) && (t->getLastEventTimestamp() + repeatInterval <= now)))
 				   {
-	               shared_ptr<DCObject> sharedThis(shared_from_this());
+	               shared_ptr<DCObject> sharedThis((originalDci != nullptr) ? originalDci : shared_from_this());
 		            EventBuilder(t->getEventCode(), m_ownerId)
 		               .dci(m_id)
 		               .param(_T("dciName"), m_name)
@@ -799,7 +800,7 @@ bool DCItem::processNewValue(time_t tmTimeStamp, const TCHAR *originalValue, boo
          // shared pointer to DCI for additional background processing
          shared_ptr<DCItem> shadowCopy = make_shared<DCItem>(this, true);
          unlock();
-         shadowCopy->checkThresholds(*pValue);
+         shadowCopy->checkThresholds(*pValue, shared_from_this());
          lock();
 
          // Reconcile threshold updates
@@ -815,7 +816,7 @@ bool DCItem::processNewValue(time_t tmTimeStamp, const TCHAR *originalValue, boo
       }
       else
       {
-         checkThresholds(*pValue);
+         checkThresholds(*pValue, nullptr);
       }
    }
 
