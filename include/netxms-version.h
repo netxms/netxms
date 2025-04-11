542aa971557350ee8545219e23e60353c0640025
Victor Kirhenshtein <victor@netxms.org>
2025-04-11 17:20:08 +0300

    base commit for version 5.3

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -29,8 +29,8 @@
  * Version constants 
  */
 #define NETXMS_VERSION_MAJOR        5
-#define NETXMS_VERSION_MINOR        2
-#define NETXMS_VERSION_RELEASE      1
+#define NETXMS_VERSION_MINOR        3
+#define NETXMS_VERSION_RELEASE      0
 
 #ifdef UNICODE
 #define IS_UNICODE_BUILD_STRING     _T(" (UNICODE)")
