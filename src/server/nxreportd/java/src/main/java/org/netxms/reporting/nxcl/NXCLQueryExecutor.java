8ab98bac64458dbcd99835b0d7779a2802513085
Victor Kirhenshtein <victor@netxms.org>
2025-05-08 11:23:02 +0300

    fixed bugs in reporting server NXCLQueryExecutor

--- src/server/nxreportd/java/src/main/java/org/netxms/reporting/nxcl/NXCLQueryExecutor.java
+++ src/server/nxreportd/java/src/main/java/org/netxms/reporting/nxcl/NXCLQueryExecutor.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2021 Raden Solutions
+ * Copyright (C) 2003-2025 Raden Solutions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRDataset;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JRQueryChunk;
 import net.sf.jasperreports.engine.JRValueParameter;
 import net.sf.jasperreports.engine.JasperReportsContext;
@@ -41,16 +42,18 @@ import net.sf.jasperreports.engine.query.JRQueryExecuter;
  */
 public class NXCLQueryExecutor implements JRQueryExecuter
 {
-   private static final Logger log = LoggerFactory.getLogger(NXCLQueryExecutor.class);
+   private static final Logger logger = LoggerFactory.getLogger(NXCLQueryExecutor.class);
 
    private JRDataset dataset;
    private Map<String, ? extends JRValueParameter> parametersMap;
+   private NXCLDataSource dataSource;
 
    protected NXCLQueryExecutor(JasperReportsContext jasperReportsContext, JRDataset dataset,
          Map<String, ? extends JRValueParameter> parametersMap)
    {
       this.dataset = dataset;
       this.parametersMap = parametersMap;
+      this.dataSource = null;
    }
 
    /**
@@ -69,25 +72,31 @@ public class NXCLQueryExecutor implements JRQueryExecuter
          ReportClassLoader classLoader = new ReportClassLoader(urls, getClass().getClassLoader());
          Class<NXCLDataSource> aClass = (Class<NXCLDataSource>)classLoader.loadClass("report.DataSource");
          Constructor<NXCLDataSource> constructor = aClass.getConstructor(JRDataset.class, Map.class);
-         NXCLDataSource dataSource = constructor.newInstance(dataset, parametersMap);
+         dataSource = constructor.newInstance(dataset, parametersMap);
 
          JRQueryChunk chunk = dataset.getQuery().getChunks()[0];
          dataSource.setQuery(chunk.getText().trim());
 
          Server server = ThreadLocalReportInfo.getServer();
+         String hostname = server.getConfigurationProperty("netxms.server.hostname", "localhost");
          String authToken = getAuthToken();
          if (authToken != null)
-            dataSource.connect(server.getConfigurationProperty("netxms.server.hostname", "localhost"), authToken);
+         {
+            logger.debug("Connecting to NetXMS server {} using authentication token", hostname);
+            dataSource.connect(hostname, authToken);
+         }
          else
-            dataSource.connect(server.getConfigurationProperty("netxms.server.hostname", "localhost"),
-                  server.getConfigurationProperty("netxms.server.login", "admin"),
-                  server.getConfigurationProperty("netxms.server.password", ""));
+         {
+            String login = server.getConfigurationProperty("netxms.server.login", "admin");
+            logger.debug("Connecting to NetXMS server {} as {} using password authentication", hostname, login);
+            dataSource.connect(hostname, login, server.getConfigurationProperty("netxms.server.password", ""));
+         }
 
          return dataSource;
       }
       catch(Exception e)
       {
-         log.error("Cannot load report data source", e);
+         logger.error("Cannot load report data source", e);
       }
       return null;
    }
@@ -97,12 +106,18 @@ public class NXCLQueryExecutor implements JRQueryExecuter
     *
     * @return authentication token or null
     */
+   @SuppressWarnings("unchecked")
    private String getAuthToken()
    {
-      JRValueParameter p = parametersMap.get(ReportManager.AUTH_TOKEN_KEY);
+      JRValueParameter p = parametersMap.get(JRParameter.REPORT_PARAMETERS_MAP);
       if (p == null)
          return null;
+
       Object v = p.getValue();
+      if (!(v instanceof Map<?, ?>))
+         return null;
+
+      v = ((Map<String, Object>)v).get(ReportManager.AUTH_TOKEN_KEY);
       return (v instanceof String) ? (String)v : null;
    }
 
@@ -112,6 +127,8 @@ public class NXCLQueryExecutor implements JRQueryExecuter
    @Override
    public void close()
    {
+      if (dataSource != null)
+         dataSource.disconnect();
    }
 
    /**
@@ -120,7 +137,7 @@ public class NXCLQueryExecutor implements JRQueryExecuter
    @Override
    public boolean cancelQuery() throws JRException
    {
-      log.warn("Query cancellation requested");
+      logger.warn("Query cancellation requested");
       return false;
    }
 }
