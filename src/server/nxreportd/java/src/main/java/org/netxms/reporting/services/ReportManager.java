ac611bba1208edefe05a4c441724ec8f01b3236a
Victor Kirhenshtein <victor@netxms.org>
2025-05-08 11:23:02 +0300

    fixed bugs in reporting server NXCLQueryExecutor

--- src/server/nxreportd/java/src/main/java/org/netxms/reporting/services/ReportManager.java
+++ src/server/nxreportd/java/src/main/java/org/netxms/reporting/services/ReportManager.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2023 Raden Solutions
+ * Copyright (C) 2003-2025 Raden Solutions
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -699,7 +699,7 @@ public class ReportManager
       hook.run(parameters, dbConnection);
       hook.disconnect();
    }
-   
+
    /**
     * Start report background worker, if any
     *
