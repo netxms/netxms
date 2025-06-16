64fdb7b515d9b659a5c7a787c183f21b7296e6af
Victor Kirhenshtein <victor@netxms.org>
2025-06-16 15:44:00 +0300

    added event for responsible user modification

--- include/nxevent.h
+++ include/nxevent.h
@@ -195,6 +195,7 @@
 #define EVENT_IF_SPEED_CHANGED                      141
 #define EVENT_RESPONSIBLE_USER_ADDED                145
 #define EVENT_RESPONSIBLE_USER_REMOVED              146
+#define EVENT_RESPONSIBLE_USER_MODIFIED             147
 
 #define EVENT_SNMP_UNMATCHED_TRAP                   500
 #define EVENT_SNMP_COLD_START                       501
