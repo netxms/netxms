523d1e8452e6771e13e09e655a7f4ecc1df9d251
Victor Kirhenshtein <victor@netxms.org>
2025-10-20 22:37:29 +0300

    outdated tools removed

--- build/prepare_release_build.sh
+++ build/prepare_release_build.sh
@@ -32,14 +32,6 @@ VERSION=`cat build/netxms-build-tag.properties | grep NETXMS_VERSION= | cut -d =
 BASE_VERSION=`cat build/netxms-build-tag.properties | grep NETXMS_BASE_VERSION= | cut -d = -f 2`
 QUALIFIER=`cat build/netxms-build-tag.properties | grep NETXMS_VERSION_QUALIFIER= | cut -d = -f 2`
 
-./build/update_plugin_versions.py . $BASE_VERSION $QUALIFIER
-
-cat src/java/netxms-eclipse/Core/plugin.xml | $SED -r "s,^(.*;Version) [0-9.]+(&#x0A.*)\$,\\1 $VERSION\\2," > ./plugin.xml
-mv ./plugin.xml src/java/netxms-eclipse/Core/plugin.xml
-
-cat src/java/netxms-eclipse/Product/nxmc.product | $SED -r "s,^Version [0-9.]+\$,Version $VERSION," > nxmc.product
-mv ./nxmc.product src/java/netxms-eclipse/Product/nxmc.product
-
 $SED -i -e "s/^AC_INIT.*/AC_INIT([NetXMS], [$VERSION], [bugs@netxms.org])/g" configure.ac
 
 if [ -x ./private/branding/build/prepare_release_build_hook.sh ]; then
