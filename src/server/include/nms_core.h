c684b08f122341068f4b2c2f2c40c1b7ab04e6cc
Victor Kirhenshtein <victor@netxms.org>
2025-10-31 12:43:55 +0200

    method ServerDownloadFileInfo::close marked as override

--- src/server/include/nms_core.h
+++ src/server/include/nms_core.h
@@ -403,7 +403,7 @@ public:
    }
    virtual ~ServerDownloadFileInfo();
 
-   virtual void close(bool success);
+   virtual void close(bool success) override;
 
    void setUploadData(uint32_t data) { m_uploadData = data; }
    void updatePackageDBInfo(const TCHAR *description, const TCHAR *pkgName, const TCHAR *pkgVersion, const TCHAR *pkgType, const TCHAR *platform, const TCHAR *cleanFileName, const TCHAR *command);
