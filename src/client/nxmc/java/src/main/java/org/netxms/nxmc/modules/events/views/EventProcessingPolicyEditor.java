netxms/netxms

commit 9024bb07801cb1e7820ab09678932126b612ab28

no-lock EPP

src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/events/views/EventProcessingPolicyEditor.java
@@ -46,6 +46,7 @@ import org.netxms.client.NXCSession;
 import org.netxms.client.ServerAction;
 import org.netxms.client.SessionListener;
 import org.netxms.client.SessionNotification;
+import org.netxms.client.events.EPPSaveResult;
 import org.netxms.client.events.EventProcessingPolicy;
 import org.netxms.client.events.EventProcessingPolicyRule;
 import org.netxms.client.events.EventTemplate;
@@ -54,6 +55,7 @@ import org.netxms.nxmc.base.jobs.Job;
 import org.netxms.nxmc.base.views.ConfigurationView;
 import org.netxms.nxmc.localization.LocalizationHelper;
 import org.netxms.nxmc.modules.actions.views.helpers.DecoratingActionLabelProvider;
+import org.netxms.nxmc.modules.events.dialogs.EPPConflictDialog;
 import org.netxms.nxmc.modules.events.views.helpers.RuleClipboard;
 import org.netxms.nxmc.modules.events.widgets.RuleEditor;
 import org.netxms.nxmc.modules.objects.widgets.helpers.BaseObjectLabelProvider;
@@ -72,7 +74,6 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    private final I18n i18n = LocalizationHelper.getI18n(EventProcessingPolicyEditor.class);
 
    private NXCSession session;
-   private boolean policyLocked = false;
    private EventProcessingPolicy policy;
    private SessionListener sessionListener;
    private Map<Long, ServerAction> actions = new HashMap<Long, ServerAction>();
@@ -195,7 +196,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    @Override
    protected void postContentCreate()
    {
-      openEventProcessingPolicy();
+      reloadPolicy();
       super.postContentCreate();
    }
 
@@ -230,7 +231,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
          @Override
          public void run()
          {
-            savePolicy(false);
+            savePolicy();
          }
       };
       actionSave.setEnabled(false);
@@ -378,44 +379,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    }
 
    /**
-    * Open event processing policy
-    */
-   private void openEventProcessingPolicy()
-   {
-      Job job = new Job(i18n.tr("Loading event processing policy"), this) {
-         @Override
-         protected void run(IProgressMonitor monitor) throws Exception
-         {
-            List<ServerAction> actions = session.getActions();
-            synchronized(EventProcessingPolicyEditor.this.actions)
-            {
-               for(ServerAction a : actions)
-               {
-                  EventProcessingPolicyEditor.this.actions.put(a.getId(), a);
-               }
-            }
-            policy = session.openEventProcessingPolicy();
-            policyLocked = true;
-            runInUIThread(new Runnable() {
-               @Override
-               public void run()
-               {
-                  initPolicyEditor();
-               }
-            });
-         }
-
-         @Override
-         protected String getErrorMessage()
-         {
-            return i18n.tr("Cannot load event processing policy");
-         }
-      };
-      job.start();
-   }
-
-   /**
-    * Init policy editor
+    * Initialize policy editor
     */
    private void initPolicyEditor()
    {
@@ -497,24 +461,26 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Save policy to server
-    * 
-    * @param unlockAfterSave if true, remove EPP lock after save
     */
-   public void savePolicy(final boolean unlockAfterSave)
+   public void savePolicy()
    {
       actionSave.setEnabled(false);
-      if (unlockAfterSave)
-         policyLocked = false;
       new Job(i18n.tr("Saving event processing policy"), this) {
          @Override
          protected void run(IProgressMonitor monitor) throws Exception
          {
-            session.saveEventProcessingPolicy(policy);
-            if (unlockAfterSave)
-               session.closeEventProcessingPolicy();
-            runInUIThread(() -> {
-               modified = false;
-            });
+            EPPSaveResult result = session.saveEventProcessingPolicy(policy);
+            if (result.isSuccess())
+            {
+               runInUIThread(() -> {
+                  modified = false;
+               });
+            }
+            else
+            {
+               // Conflicts detected - handle in UI thread
+               runInUIThread(() -> handleSaveConflicts(result));
+            }
          }
 
          @Override
@@ -532,6 +498,56 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    }
 
    /**
+    * Handle save conflicts by showing dialog and taking appropriate action.
+    *
+    * @param result save result containing conflict information
+    */
+   private void handleSaveConflicts(EPPSaveResult result)
+   {
+      EPPConflictDialog dialog = new EPPConflictDialog(getWindow().getShell(), result.getConflicts(), policy);
+      if (dialog.open() == EPPConflictDialog.RELOAD)
+      {
+         reloadPolicy();
+      }
+   }
+
+   /**
+    * Reload policy from server, discarding local changes.
+    */
+   private void reloadPolicy()
+   {
+      // Clear current editors
+      for(RuleEditor editor : ruleEditors)
+      {
+         if (!editor.isDisposed())
+            editor.dispose();
+      }
+      ruleEditors.clear();
+      selection.clear();
+      lastSelectedRule = -1;
+
+      new Job(i18n.tr("Reloading event processing policy"), this) {
+         @Override
+         protected void run(IProgressMonitor monitor) throws Exception
+         {
+            policy = session.getEventProcessingPolicy();
+            runInUIThread(() -> {
+               initPolicyEditor();
+               modified = false;
+               actionSave.setEnabled(false);
+               updateEditorAreaLayout();
+            });
+         }
+
+         @Override
+         protected String getErrorMessage()
+         {
+            return i18n.tr("Cannot reload event processing policy");
+         }
+      }.start();
+   }
+
+   /**
     * @see org.eclipse.ui.part.WorkbenchPart#dispose()
     */
    @Override
@@ -539,23 +555,6 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    {
       if (sessionListener != null)
          session.removeListener(sessionListener);
-
-      if (policyLocked)
-      {
-         new Job(i18n.tr("Closing event processing policy"), null) {
-            @Override
-            protected void run(IProgressMonitor monitor) throws Exception
-            {
-               session.closeEventProcessingPolicy();
-            }
-
-            @Override
-            protected String getErrorMessage()
-            {
-               return i18n.tr("Cannot close event processing policy");
-            }
-         }.start();
-      }
 
       imageStop.dispose();
       imageAlarm.dispose();
@@ -1143,8 +1142,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    @Override
    public void save()
    {
-      // This method should be called only when save on close is selected, so do unlock after save
-      savePolicy(true);
+      savePolicy();
    }
 
    /**
@@ -1153,12 +1151,6 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    @Override
    public void refresh()
    {
-      if (!policyLocked)
-      {
-         openEventProcessingPolicy();
-         return;
-      }
-      
       if (isModified())
       {
          if (!MessageDialogHelper.openConfirm(getWindow().getShell(), i18n.tr("Unsaved Changes"), 
@@ -1167,19 +1159,6 @@ public class EventProcessingPolicyEditor extends ConfigurationView
             return;
          }
       }
-      new Job(i18n.tr("Closing event processing policy"), null) {
-         @Override
-         protected void run(IProgressMonitor monitor) throws Exception
-         {
-            session.closeEventProcessingPolicy();
-            runInUIThread(() -> openEventProcessingPolicy());
-         }
-
-         @Override
-         protected String getErrorMessage()
-         {
-            return i18n.tr("Cannot close event processing policy");
-         }
-      }.start();
+      reloadPolicy();
    }
 }

include/netxmsdb.h
@@ -25,7 +25,7 @@
 
 #define DB_LEGACY_SCHEMA_VERSION       700
 #define DB_SCHEMA_VERSION_MAJOR        60
-#define DB_SCHEMA_VERSION_MINOR        28
+#define DB_SCHEMA_VERSION_MINOR        29
 
 #define DB_SCHEMA_VERSION_V60_MINOR    DB_SCHEMA_VERSION_MINOR
 

include/nms_cscp.h
@@ -217,10 +217,10 @@ typedef struct
 #define CMD_DELETE_CONFIG_VARIABLE        0x0011
 #define CMD_NOTIFY                        0x0012
 #define CMD_TRAP                          0x0013
-#define CMD_OPEN_EPP                      0x0014
-#define CMD_CLOSE_EPP                     0x0015
-#define CMD_SAVE_EPP                      0x0016
-#define CMD_EPP_RECORD                    0x0017
+#define CMD_GET_EPP                       0x0014
+#define CMD_SAVE_EPP                      0x0015
+#define CMD_EPP_RECORD                    0x0016
+#define CMD_EXPLAIN_EPP_RULE              0x0017
 #define CMD_EVENT_DB_UPDATE               0x0018
 #define CMD_TRAP_CFG_UPDATE               0x0019
 #define CMD_SET_EVENT_INFO                0x001A
@@ -680,7 +680,7 @@ typedef struct
 #define CMD_ADD_AI_AGENT_TASK             0x01E0
 #define CMD_CREATE_AI_ASSISTANT_CHAT      0x01E1
 #define CMD_DELETE_AI_ASSISTANT_CHAT      0x01E2
-#define CMD_EXPLAIN_EPP_RULE              0x01E3
+#define CMD_AI_FUNCTION_CALL              0x01E3
 #define CMD_CLEAR_NOTIFICATION_QUEUE      0x01E4
 #define CMD_DECOMMISSION_NODE             0x01E5
 #define CMD_GET_INCIDENTS                 0x01E6
@@ -706,7 +706,6 @@ typedef struct
 #define CMD_GET_LOG_QUERY_SQL             0x01FA
 #define CMD_GET_LIST_LIST                 0x01FB
 #define CMD_QUERY_LIST                    0x01FC
-#define CMD_AI_FUNCTION_CALL              0x01FD
 
 #define CMD_RS_LIST_REPORTS               0x1100
 #define CMD_RS_GET_REPORT_DEFINITION      0x1101
@@ -1637,6 +1636,19 @@ typedef struct
 #define VID_AI_FUNCTION_NAME        ((uint32_t)916)
 #define VID_AI_HINT                 ((uint32_t)917)
 #define VID_TEMPLATE_REMOVAL_GP     ((uint32_t)918)
+#define VID_EPP_VERSION             ((uint32_t)919)
+#define VID_RULE_VERSION            ((uint32_t)920)
+#define VID_BASE_VERSION            ((uint32_t)921)
+#define VID_MODIFIED_BY_GUID        ((uint32_t)922)
+#define VID_MODIFIED_BY_NAME        ((uint32_t)923)
+#define VID_CONFLICT_COUNT          ((uint32_t)924)
+#define VID_CONFLICT_TYPE           ((uint32_t)925)
+#define VID_RULE_MODIFIED           ((uint32_t)926)
+#define VID_DELETED_RULE_COUNT      ((uint32_t)927)
+
+// Base values for EPP optimistic concurrency
+#define VID_DELETED_RULE_LIST_BASE  ((uint32_t)0x7A000000)
+#define VID_CONFLICT_LIST_BASE      ((uint32_t)0x7B000000)
 
 // Base variabe for single threshold in message
 #define VID_THRESHOLD_BASE          ((uint32_t)0x00800000)

include/nxcldefs.h
@@ -791,6 +791,7 @@ enum SessionState
 #define RCC_INVALID_INCIDENT_STATE        ((uint32_t)183)
 #define RCC_COMMENT_REQUIRED              ((uint32_t)184)
 #define RCC_INVALID_MESSAGE_ID            ((uint32_t)185)
+#define RCC_EPP_CONFLICT                  ((uint32_t)186)
 
 /**
  * Mask bits for NXCModifyEventTemplate()

sql/Makefile.am
@@ -21,7 +21,7 @@ sql_DATA = dbinit_mysql.sql \
 
 ROOT_FILE = dbinit.in
 SOURCE = $(ROOT_FILE) database.in functions.in schema.in metadata.in setup.in events.in policy.in scripts.in images.in traps.in country_codes.in currency_codes.in devdb.in
-HEADER_FILES = ../include/netxmsdb.h ../include/nxevent.h ../src/server/include/nms_locks.h
+HEADER_FILES = ../include/netxmsdb.h ../include/nxevent.h
 COMMAND = $(CPP) @SQLFLAGS@ -D_NETXMS_DB_SCHEMA_ -I@top_srcdir@/include -I@top_srcdir@/src/server/include
 SQUEEZE = perl -n strip.pl
 

sql/Makefile.msvc.w32
@@ -8,7 +8,7 @@ sql_SCRIPTS = dbinit_mysql.sql dbinit_pgsql.sql dbinit_mssql.sql dbinit_oracle.s
 
 ROOT_FILE = dbinit.in
 SOURCE = $(ROOT_FILE) database.in schema.in setup.in events.in policy.in scripts.in images.in
-HEADERS = ../include/netxmsdb.h ../include/nxevent.h ../src/server/include/nms_locks.h
+HEADERS = ../include/netxmsdb.h ../include/nxevent.h
 COMMAND = $(CPP) /EP /D_NETXMS_DB_SCHEMA_ /I../include /I../src/server/include
 #SQUEEZE = perl -e "while (<>) { if (! /^[\s\t\n]*$$/) { print; } }"
 

sql/Makefile.w32
@@ -21,7 +21,7 @@ sql_SCRIPTS = dbinit_mysql.sql \
 
 ROOT_FILE = dbinit.in
 SOURCE = $(ROOT_FILE) database.in functions.in schema.in metadata.in setup.in events.in policy.in scripts.in images.in
-HEADERS = ../include/netxmsdb.h ../include/nxevent.h ../src/server/include/nms_locks.h
+HEADERS = ../include/netxmsdb.h ../include/nxevent.h
 COMMAND = $(CPP) -P -Wundef -D_NETXMS_DB_SCHEMA_ -I../include -I../src/server/include
 SQUEEZE = perl -e "while (<>) { if (! /^[\s\t\n]*$$/) { print; } }"
 

sql/dbinit.in
@@ -8,7 +8,6 @@
 
 // Common constants
 #include <nxevent.h>
-#include <nms_locks.h>
 #include <netxmsdb.h>
 #include <nxtools.h>
 

commit c502c137a7d0774a23e6b0e26a85c7748343bf65

fixed missing action list loading in EPP editor (regression from 9024bb0780)

src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/events/views/EventProcessingPolicyEditor.java
@@ -186,7 +186,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
             return arg0.getRuleNumber() - arg1.getRuleNumber();
          }
       });
-      
+
       createActions();
    }
 
@@ -340,7 +340,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Fill local pull-down menu
-    * 
+    *
     * @param manager Menu manager for pull-down menu
     */
    @Override
@@ -357,7 +357,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Fill local tool bar
-    * 
+    *
     * @param manager Menu manager for local toolbar
     */
    @Override
@@ -416,7 +416,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Process session notifications
-    * 
+    *
     * @param n notification
     */
    private void processSessionNotification(SessionNotification n)
@@ -446,7 +446,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Set all rules to collapsed or expanded state
-    * 
+    *
     * @param collapsed true to collapse all, false to expand
     */
    private void setAllRulesCollapsed(boolean collapsed)
@@ -531,6 +531,13 @@ public class EventProcessingPolicyEditor extends ConfigurationView
          protected void run(IProgressMonitor monitor) throws Exception
          {
             policy = session.getEventProcessingPolicy();
+            List<ServerAction> serverActions = session.getActions();
+            synchronized(actions)
+            {
+               actions.clear();
+               for(ServerAction a : serverActions)
+                  actions.put(a.getId(), a);
+            }
             runInUIThread(() -> {
                System.out.println("Policy version: " + policy.getVersion());
                initPolicyEditor();
@@ -586,7 +593,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Find server action by ID
-    * 
+    *
     * @param id action id
     * @return server action object or null
     */
@@ -597,7 +604,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Find server actions for list of Ids
-    * 
+    *
     * @param idList list of action identifiers
     * @return list of server actions
     */
@@ -615,7 +622,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Return complete actions list
-    * 
+    *
     * @return actions list
     */
    public Collection<ServerAction> getActions()
@@ -657,7 +664,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Get "cancel timer" image
-    * 
+    *
     * @return "cancel timer" image
     */
    public Image getImageCancelTimer()
@@ -752,12 +759,12 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Set selection to given rule
-    * 
+    *
     * @param e rule editor
     */
    public void setSelection(RuleEditor e)
    {
-      clearSelection();      
+      clearSelection();
       addToSelection(e, false);
    }
 
@@ -774,7 +781,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Add rule to selection
-    * 
+    *
     * @param e rule editor
     */
    public void addToSelection(RuleEditor e, boolean allFromPrevSelection)
@@ -792,13 +799,13 @@ public class EventProcessingPolicyEditor extends ConfigurationView
       selection.add(e);
       e.setSelected(true);
       lastSelectedRule = e.getRuleNumber();
-      
+
       onSelectionChange();
    }
-   
+
    /**
     * Remove rule from selection
-    * 
+    *
     * @param e rule editor
     */
    public void removeFromSelection(RuleEditor e)
@@ -806,7 +813,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
       selection.remove(e);
       e.setSelected(false);
       lastSelectedRule = -1;
-      
+
       onSelectionChange();
    }
 
@@ -862,7 +869,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Insert new rule at given position
-    * 
+    *
     * @param position
     */
    private void insertRule(int position)
@@ -980,17 +987,17 @@ public class EventProcessingPolicyEditor extends ConfigurationView
       updateEditorAreaLayout();
       setModified(true);
    }
-   
+
    /**
     * Moves rule selection
-    * 
+    *
     * @param anchor - where the selection is being moved
     */
    public void moveSelection(RuleEditor anchor)
    {
       if (selection.contains(anchor))
          return;
-      
+
       List<RuleEditor> movedRuleEditors = new ArrayList<RuleEditor>();
       for(RuleEditor e : ruleEditors)
       {
@@ -1035,7 +1042,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Fill context menu for rule
-    * 
+    *
     * @param manager menu manager
     */
    public void fillRuleContextMenu(IMenuManager manager)
@@ -1096,7 +1103,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
 
    /**
     * Check if given rule should be visible
-    * 
+    *
     * @param rule
     * @return
     */
@@ -1154,7 +1161,7 @@ public class EventProcessingPolicyEditor extends ConfigurationView
    {
       if (isModified())
       {
-         if (!MessageDialogHelper.openConfirm(getWindow().getShell(), i18n.tr("Unsaved Changes"), 
+         if (!MessageDialogHelper.openConfirm(getWindow().getShell(), i18n.tr("Unsaved Changes"),
                i18n.tr("Are you sure you want to refresh and lose all not saved changes?")))
          {
             return;