d462b30218e13d2df0f68ae288ac783c90703834
Victor Kirhenshtein <victor@netxms.org>
2026-02-20 16:28:37 +0200

    client protocol version updated

--- include/netxms-version.h
+++ include/netxms-version.h
@@ -1,6 +1,6 @@
 /* 
 ** NetXMS - Network Management System
-** Copyright (C) 2003-2025 Victor Kirhenshtein
+** Copyright (C) 2003-2026 Victor Kirhenshtein
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU Lesser General Public License as published by
@@ -53,7 +53,7 @@
 #define CLIENT_PROTOCOL_VERSION_MOBILE    1     /* All functionality relevant for mobile client */
 #define CLIENT_PROTOCOL_VERSION_TCPPROXY  1     /* All functionality relevant for TCP proxy sessions */
 #define CLIENT_PROTOCOL_VERSION_SCHEDULER 2     /* All functionality relevant for scheduler configuration */
-#define CLIENT_PROTOCOL_VERSION_FULL      54    /* All functionality */
+#define CLIENT_PROTOCOL_VERSION_FULL      55    /* All functionality */
 
 /**
  * Protocol version positions
--- src/client/java/netxms-client/src/main/java/org/netxms/client/ProtocolVersion.java
+++ src/client/java/netxms-client/src/main/java/org/netxms/client/ProtocolVersion.java
@@ -1,6 +1,6 @@
 /**
  * NetXMS - open source network management system
- * Copyright (C) 2003-2025 Victor Kirhenshtein
+ * Copyright (C) 2003-2026 Victor Kirhenshtein
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ public final class ProtocolVersion
    // Versions
    public static final int ALARMS    = 4;
    public static final int BASE      = 62;
-   public static final int FULL      = 54;
+   public static final int FULL      = 55;
    public static final int MOBILE    = 1;
    public static final int PUSH      = 1;
    public static final int SCHEDULER = 2;
