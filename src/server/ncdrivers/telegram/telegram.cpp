621e11d761f25d1bace578ac100df273f82a25cf
Victor Kirhenshtein <victor@netxms.org>
2026-07-22 14:38:41 +0300

    Fixed possible uninitialized variable access in Telegram driver (issue #3445)

--- src/server/ncdrivers/telegram/telegram.cpp
+++ src/server/ncdrivers/telegram/telegram.cpp
@@ -584,7 +584,7 @@ TelegramDriver *TelegramDriver::createInstance(Config *config, NCDriverStorageMa
    memset(&proxy, 0, sizeof(ProxyInfo));
    proxy.protocol = 0x7FFF;   // "unset" indicator
 
-   char authToken[64];
+   char authToken[64] = "";
    char protocol[8] = "http";
    uint32_t options = LONG_POLLING;
    uint32_t pollingInterval = 300;
@@ -609,6 +609,12 @@ TelegramDriver *TelegramDriver::createInstance(Config *config, NCDriverStorageMa
    if (!config->parseTemplate(_T("Telegram"), configTemplate))
    {
       nxlog_write_tag(NXLOG_ERROR, DEBUG_TAG, _T("Error parsing driver configuration"));
+      return nullptr;
+   }
+
+   if (authToken[0] == 0)
+   {
+      nxlog_write_tag(NXLOG_ERROR, DEBUG_TAG, _T("Authentication token not provided"));
       return nullptr;
    }
 
