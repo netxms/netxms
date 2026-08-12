d5b9e7e1a7a9176fd7b35d014631775cfb9c61d4
Victor Kirhenshtein <victor@netxms.org>
2026-08-12 09:49:50 +0300

    do not start data collection before server initialization completion (issue #3549)

--- src/server/core/datacoll.cpp
+++ src/server/core/datacoll.cpp
@@ -483,6 +483,9 @@ static void ItemPoller()
 
    nxlog_debug_tag(DEBUG_TAG_DC_POLLER, 1, _T("DCI poller thread started"));
 
+   WaitForServerStartupCompletion();
+   nxlog_debug_tag(DEBUG_TAG_DC_POLLER, 4, _T("DCI poller thread activated"));
+
    uint32_t watchdogId = WatchdogAddThread(_T("Item Poller"), 10);
    GaugeData<uint32_t> queuingTime(ITEM_POLLING_INTERVAL, 300);
 
@@ -490,9 +493,12 @@ static void ItemPoller()
    {
       if (SleepAndCheckForShutdown(ITEM_POLLING_INTERVAL))
          break;      // Shutdown has arrived
+
       WatchdogNotify(watchdogId);
+
       if (HACheckFence())
          break;   // node fenced - no further role-sensitive work
+
       nxlog_debug_tag(DEBUG_TAG_DC_POLLER, 8, _T("ItemPoller: wakeup"));
 
       int64_t startTime = GetCurrentTimeMs();
