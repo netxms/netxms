ad2be57a6e216215a5d55f3f1a738d2bc35da477
Victor Kirhenshtein <victor@netxms.org>
2026-08-31 18:46:12 +0300

    fixed map tooltip color issue

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/networkmaps/widgets/helpers/LinkTooltip.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/networkmaps/widgets/helpers/LinkTooltip.java
@@ -73,6 +73,7 @@ public class LinkTooltip extends Figure
 
       setOpaque(true);
       setBackgroundColor(ThemeEngine.getBackgroundColor("Map.ObjectTooltip"));
+      setForegroundColor(ThemeEngine.getForegroundColor("Map.ObjectTooltip"));
 
       buildContent();
    }
