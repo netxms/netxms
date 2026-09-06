9c83a82fe0cb93794bc42d43c909f541378a104c
Victor Kirhenshtein <victor@netxms.org>
2026-09-06 20:14:29 +0300

    fixed read-only restriction check in file manager subagent for merge files command (issue #3630)

--- src/agent/subagents/filemgr/filemgr.cpp
+++ src/agent/subagents/filemgr/filemgr.cpp
@@ -1606,7 +1606,7 @@ static void CH_MergeFiles(const NXCPMessage& request, NXCPMessage *response, Abs
    ConvertPathToHost(destinationFileName, request.getFieldAsBoolean(VID_ALLOW_PATH_EXPANSION), session->isMasterServer());
 
    TCHAR *destinationFullPath;
-   if (CheckFullPath(destinationFileName, &destinationFullPath, false))
+   if (CheckFullPath(destinationFileName, &destinationFullPath, false, true))
    {
       size_t size;
       const BYTE *md5 = request.getBinaryFieldPtr(VID_HASH_MD5, &size);
@@ -1623,7 +1623,7 @@ static void CH_MergeFiles(const NXCPMessage& request, NXCPMessage *response, Abs
                _tcslcpy(sourceFileName, partFiles.get(i), MAX_PATH);
                ConvertPathToHost(sourceFileName, request.getFieldAsBoolean(VID_ALLOW_PATH_EXPANSION), session->isMasterServer());
                TCHAR *sourceFullPath;
-               if (CheckFullPath(sourceFileName, &sourceFullPath, false))
+               if (CheckFullPath(sourceFileName, &sourceFullPath, false, true))
                {
                   if (!MergeFiles(sourceFullPath, destinationFullPath))
                   {
@@ -1647,7 +1647,7 @@ static void CH_MergeFiles(const NXCPMessage& request, NXCPMessage *response, Abs
                   _tcslcpy(sourceFileName, partFiles.get(i), MAX_PATH);
                   ConvertPathToHost(sourceFileName, request.getFieldAsBoolean(VID_ALLOW_PATH_EXPANSION), session->isMasterServer());
                   TCHAR *sourceFullPath;
-                  if (CheckFullPath(sourceFileName, &sourceFullPath, false))
+                  if (CheckFullPath(sourceFileName, &sourceFullPath, false, true))
                   {
                      Delete(sourceFullPath);
                      MemFree(sourceFullPath);
