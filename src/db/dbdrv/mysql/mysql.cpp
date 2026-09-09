65c3b0d31871223846725e63cfeca200efe7669f
Victor Kirhenshtein <victor@netxms.org>
2026-09-09 16:03:33 +0300

    fixed stack overflow in MySQL and MariaDB drivers (issue #3637)

--- src/db/dbdrv/mysql/mysql.cpp
+++ src/db/dbdrv/mysql/mysql.cpp
@@ -583,6 +583,67 @@ static int32_t GetFieldLength(DBDRV_RESULT hResult, int iRow, int iColumn)
 }
 
 /**
+ * Fetch column value from current row of prepared statement result into caller's buffer.
+ * UTF-8 values are fetched directly into caller's buffer (connector truncates the value
+ * to buffer size and reports full length). Wide character values go through intermediate
+ * UTF-8 buffer sized by actual field length, which is placed on heap for large fields.
+ * Returns false if column could not be fetched.
+ */
+static bool FetchPreparedStatementColumn(MYSQL_STMT *statement, int column, unsigned long fieldLength, void *buffer, int bufferSize, bool utf8)
+{
+   if (bufferSize <= 0)
+      return false;
+
+   MYSQL_BIND b;
+   unsigned long l = 0;
+#if HAVE_MY_BOOL
+   my_bool isNull;
+#else
+   bool isNull;
+#endif
+
+   memset(&b, 0, sizeof(MYSQL_BIND));
+   b.buffer_type = MYSQL_TYPE_STRING;
+   b.length = &l;
+   b.is_null = &isNull;
+
+   Buffer<char, 4096> utf8Buffer;
+   if (utf8)
+   {
+      b.buffer = buffer;
+      b.buffer_length = bufferSize - 1;
+   }
+   else
+   {
+      utf8Buffer.reserve(fieldLength + 1);
+      b.buffer = utf8Buffer.buffer();
+      b.buffer_length = fieldLength + 1;
+   }
+
+   if (mysql_stmt_fetch_column(statement, &b, column, 0) != 0)
+      return false;
+
+   if (isNull)
+   {
+      if (utf8)
+         *static_cast<char*>(buffer) = 0;
+      else
+         *static_cast<WCHAR*>(buffer) = 0;
+   }
+   else if (utf8)
+   {
+      static_cast<char*>(buffer)[std::min(l, static_cast<unsigned long>(bufferSize - 1))] = 0;
+   }
+   else
+   {
+      utf8Buffer[static_cast<size_t>(std::min(l, fieldLength))] = 0;
+      utf8_to_wchar(utf8Buffer.buffer(), -1, static_cast<WCHAR*>(buffer), bufferSize);
+      static_cast<WCHAR*>(buffer)[bufferSize - 1] = 0;
+   }
+   return true;
+}
+
+/**
  * Get field value from result - UNICODE and UTF8 implementation
  */
 static void *GetFieldInternal(MYSQL_RESULT *hResult, int iRow, int iColumn, void *pBuffer, int nBufSize, bool utf8)
@@ -603,47 +664,9 @@ static void *GetFieldInternal(MYSQL_RESULT *hResult, int iRow, int iColumn, void
 			hResult->currentRow = iRow;
 		}
 
-		MYSQL_BIND b;
-		unsigned long l = 0;
-#if HAVE_MY_BOOL
-		my_bool isNull;
-#else
-		bool isNull;
-#endif
-
-		memset(&b, 0, sizeof(MYSQL_BIND));
-		b.buffer = MemAllocLocal(hResult->lengthFields[iColumn] + 1);
-		b.buffer_length = hResult->lengthFields[iColumn] + 1;
-		b.buffer_type = MYSQL_TYPE_STRING;
-		b.length = &l;
-		b.is_null = &isNull;
-      int rc = mysql_stmt_fetch_column(hResult->statement, &b, iColumn, 0);
-      if (rc == 0)
-		{
-         if (!isNull)
-         {
-			   ((char *)b.buffer)[l] = 0;
-            if (utf8)
-            {
-			      strlcpy((char *)pBuffer, (char *)b.buffer, nBufSize);
-            }
-            else
-            {
-			      utf8_to_wchar((char *)b.buffer, -1, (WCHAR *)pBuffer, nBufSize);
-   			   ((WCHAR *)pBuffer)[nBufSize - 1] = 0;
-            }
-         }
-         else
-         {
-            if (utf8)
-               *((char *)pBuffer) = 0;
-            else
-               *((WCHAR *)pBuffer) = 0;
-         }
-			value = pBuffer;
-		}
+      if (FetchPreparedStatementColumn(hResult->statement, iColumn, hResult->lengthFields[iColumn], pBuffer, nBufSize, utf8))
+         value = pBuffer;
       hResult->connection->mutexQueryLock.unlock();
-		MemFreeLocal(b.buffer);
 	}
 	else
 	{
@@ -955,46 +978,8 @@ static void *GetFieldUnbufferedInternal(MYSQL_UNBUFFERED_RESULT *hResult, int iC
    void *value = nullptr;
    if (hResult->isPreparedStatement)
    {
-      MYSQL_BIND b;
-      unsigned long l = 0;
-#if HAVE_MY_BOOL
-      my_bool isNull;
-#else
-      bool isNull;
-#endif
-
-      memset(&b, 0, sizeof(MYSQL_BIND));
-      b.buffer = MemAllocLocal(hResult->lengthFields[iColumn] + 1);
-      b.buffer_length = hResult->lengthFields[iColumn] + 1;
-      b.buffer_type = MYSQL_TYPE_STRING;
-      b.length = &l;
-      b.is_null = &isNull;
-      int rc = mysql_stmt_fetch_column(hResult->statement, &b, iColumn, 0);
-      if (rc == 0)
-      {
-         if (!isNull)
-         {
-            ((char *)b.buffer)[l] = 0;
-            if (utf8)
-            {
-               strlcpy((char *)pBuffer, (char *)b.buffer, iBufSize);
-            }
-            else
-            {
-               utf8_to_wchar((char *)b.buffer, -1, (WCHAR *)pBuffer, iBufSize);
-               ((WCHAR *)pBuffer)[iBufSize - 1] = 0;
-            }
-         }
-         else
-         {
-            if (utf8)
-               *((char *)pBuffer) = 0;
-            else
-               *((WCHAR *)pBuffer) = 0;
-         }
+      if (FetchPreparedStatementColumn(hResult->statement, iColumn, hResult->lengthFields[iColumn], pBuffer, iBufSize, utf8))
          value = pBuffer;
-      }
-      MemFreeLocal(b.buffer);
    }
    else
    {
