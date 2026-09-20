544ed9383484d2d39a69a48f9bbb9c28783562fd
Victor Kirhenshtein <victor@netxms.org>
2026-09-20 14:56:35 +0300

    fixed connection leak in NXSL function SQLQuery (issue #3666)

--- src/server/core/nxslext.cpp
+++ src/server/core/nxslext.cpp
@@ -2492,7 +2492,7 @@ static int F_SQLQuery(int argc, NXSL_Value **argv, NXSL_Value **result, NXSL_VM 
          success = false;
       }
    }
-
+   DBConnectionPoolReleaseConnection(hdb);
    *result = vm->createValue(success);
    return NXSL_ERR_SUCCESS;
 }
